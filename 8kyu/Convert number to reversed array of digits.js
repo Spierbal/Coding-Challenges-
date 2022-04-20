d7d0b146d9200cc556918955476d245abf82c0e4
Spierbal <abdulkadir.f.osman@gmail.com>
2022-04-20 10:47:33 +0100

    T

---
+++ 8kyu/Convert number to reversed array of digits.js
@@ -0,0 +1,32 @@
+function digitize(n) {
+  const numString = String(n);
+
+  return numString
+    .split("")
+    .reverse()
+    .map((num) => {
+      return +num;
+    });
+}
+
+// Solution 2: in one line
+
+function digitize(n) {
+  return String(n).split("").map(Number).reverse();
+}
+
+// Pseudo code
+
+// Convert the number into a string (built in string)
+// and then split the number into a string e.g. split("")
+// 348597 => ["3", "4", "8", "5", "9", "7"]
+
+// then use map function to convert the array of strings into array of numbers
+// ["7", "9", "5", "8", "4", "3"] => [7, 9, 5, 8, 4, 3]
+
+// reverse the array of strings using reverse()
+// Expected input = [348597] => [7, 9, 5, 8, 4, 3]
+
+// also expected input 0 => [0]
+
+// Finally return it
---
+++ 8kyu/Sum of positive.js
@@ -0,0 +1,26 @@
+function positiveSum(arr) {
+  let sum = 0;
+
+  arr.forEach((item) => {
+    if (item > 0) {
+      sum += item;
+    }
+  });
+  return sum;
+}
+// 2nd solution
+// function positiveSum(arr) {
+//   return arr.filter(el => el > 0).reduce((a, b) => a + b, 0)
+// }
+// 3rd solution
+// function positiveSum(arr) {
+
+//   let sum = 0;
+
+//   for (let i = 0; i < arr.length; i++){
+//     if (arr[i] > 0){
+//       sum += arr[i];
+//     }
+//   }
+//    return sum;
+// }
