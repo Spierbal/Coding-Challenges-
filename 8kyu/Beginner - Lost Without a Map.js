71df30092cfc57defbaf600777c21a99a0bf84d8
Spierbal <abdulkadir.f.osman@gmail.com>
2022-05-28 00:49:03 +0100

    T

--- 8kyu/Beginner - Lost Without a Map.js
+++ 8kyu/Beginner - Lost Without a Map.js
@@ -1,1 +1,3 @@
 const maps = x => x.map(n => n * 2);
+
+console.log(maps([1, 2, 3])); // 2, 4, 6
--- 8kyu/Century From Year.js
+++ 8kyu/Century From Year.js
@@ -1,3 +1,9 @@
+// Solution:
 const century = year => Math.ceil(year / 100);
 
-//console.log(century(1705))
+// Task: Given a year, return the century it is in.
+
+//console.log(century(1705)) --> 18
+//console.log(century(1900)) --> 19
+//console.log(century(1601)) --> 17
+//console.log(century(2000)) --> 20
