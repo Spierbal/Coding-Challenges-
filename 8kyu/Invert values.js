95137eb2d8c7584be57dd7f699d283cc4a724e54
Spierbal <abdulkadir.f.osman@gmail.com>
2022-06-18 22:38:56 +0100

    T

--- 8kyu/Invert values.js
+++ 8kyu/Invert values.js
@@ -1,7 +1,7 @@
 const invert = array => array.map(n => -n);
 
-console.log(invert([1, 2, 3, 4, 5])); // --> [-1, -2, -3, -4, -5]
-console.log(invert([-1, -2, -3, -4, -5])); //--> [1, 2, 3, 4, 5]
+// console.log(invert([1, 2, 3, 4, 5])); // --> [-1, -2, -3, -4, -5]
+// console.log(invert([-1, -2, -3, -4, -5])); //--> [1, 2, 3, 4, 5]
 
 // 2nd Solution:
 // function invert(array) {
--- 8kyu/Square(n)Sum.js
+++ 8kyu/Square(n)Sum.js
@@ -1,11 +1,7 @@
-function squareSum(n) {
-  return n.map(x => x ** 2).reduce((a, b) => a + b, 0);
-}
+const squareSum = n => n.reduce((a, b) => a + b ** 2, 0);
 
-// Shorter solutions
+// 2nd Solution:
+// const squareSum = n => n.map(x => x ** 2).reduce((a, b) => a + b, 0);
 
-//const squareSum = n => n.reduce((a, b) => a + b ** 2, 0);
-
-//const squareSum = n => n.map(x => x ** 2).reduce((a, b) => a + b, 0);
-
-// console.log(squareSum([1, 2])); // 5
+// console.log(squareSum([1, 2])); // 5 (1^1 + 2^2 = 5)
+// console.log(squareSum([2, 4])) // --> 20
