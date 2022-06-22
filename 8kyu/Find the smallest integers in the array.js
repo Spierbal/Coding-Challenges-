1823f816b5270fb4587cbfc10367fab70f055c03
Spierbal <abdulkadir.f.osman@gmail.com>
2022-06-22 23:33:21 +0100

    T

--- 8kyu/Find the smallest integers in the array.js
+++ 8kyu/Find the smallest integers in the array.js
@@ -3,3 +3,5 @@ class SmallestIntegerFinder {
     return args.reduce((a, b) => Math.min(a, b));
   }
 }
+// let obj = new SmallestIntegerFinder();
+// console.log(obj.findSmallestInt([78,56,232,12,8])); --> 8
