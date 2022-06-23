68ca6000394cbc53cc6aa105e9ec6d305a670c7a
Spierbal <abdulkadir.f.osman@gmail.com>
2022-06-23 04:36:43 +0100

    T

--- 8kyu/Find Maximum and Minimum Values of a List.js
+++ 8kyu/Find Maximum and Minimum Values of a List.js
@@ -1,5 +1,10 @@
-const min = list => list.reduce((a, b) => Math.min(a, b), 0);
-const max = list => list.reduce((a, b) => Math.max(a, b), 0);
+// Solution 1:
+const min = list => list.reduce((a, b) => Math.min(a, b));
+const max = list => list.reduce((a, b) => Math.max(a, b));
+
+// Solution 2:
+// const min = list => Math.min(...list);
+// const max = list => Math.max(...list);
 
 // console.log(min([-52, 56, 30, 29, -54, 0, -110])); // --> -110
 // console.log(max([4, 6, 2, 1, 9, 63, -134, 566])); // --> 566
