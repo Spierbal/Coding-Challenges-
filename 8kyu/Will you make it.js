95ebf2909ff9ed315e54cccfbd46751ece9929e9
Spierbal <abdulkadir.f.osman@gmail.com>
2022-06-25 07:36:01 +0100

    T

--- 8kyu/Will you make it.js
+++ 8kyu/Will you make it.js
@@ -2,7 +2,7 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) =>
   mpg * fuelLeft >= distanceToPump;
 
 // solution 2:
-// const zeroFuelw = (distanceToPump, mpg, fuelLeft) =>
+// const zeroFuel = (distanceToPump, mpg, fuelLeft) =>
 //   distanceToPump / mpg <= fuelLeft;
 
 // console.log(zeroFuel(50, 25, 2)) --> true
