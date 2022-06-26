f2892e5498e56c62a2053946cef9c9e479e0ba13
Spierbal <abdulkadir.f.osman@gmail.com>
2022-06-27 00:30:37 +0100

    T

--- 8kyu/Count the monkey.js
+++ 8kyu/Count the monkey.js
@@ -1,4 +1,19 @@
 //Solution:
-const monkeyCount = n => [...new Array(n)].map((_, x) => x + 1);
+//const monkeyCount = n => [...Array(n)].map((_, x) => x + 1);
+
+// Solution 2:
+// const monkeyCount = n =>
+//   Array(n)
+//     .fill()
+//     .map((_, i) => i + 1);
+
+// Solution 3: Less Optimized
+
+// function monkeyCount(n) {
+//     let arr = [];
+//     for(let i = 1; i <= n; i++) arr.push(i);
+//
+//     return arr;
+//   }
 
 //console.log(monkeyCount(5)) --> [1, 2, 3, 4, 5]
