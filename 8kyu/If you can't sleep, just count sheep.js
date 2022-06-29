eb7b3299113426dcbe40fea7b08801c06358d988
Spierbal <abdulkadir.f.osman@gmail.com>
2022-06-29 08:40:52 +0100

    T

--- 8kyu/If you can't sleep, just count sheep.js
+++ 8kyu/If you can't sleep, just count sheep.js
@@ -2,10 +2,10 @@ const countSheep = n =>
   [...Array(n)].map((_, i) => `${i + 1} sheep...`).join("");
 
 // Solution 2: less optimized
-function countSheep(num) {
-  let str = "";
-for(let i = 1; i <= num; i++) str += `${i} sheep...` <-- string template literals
-  return str;
-}
+// function countSheep(num) {
+//   let str = "";
+// for(let i = 1; i <= num; i++) str += `${i} sheep...`
+//   return str;
+// }
 
 // console.log(countSheep(2)) --> 1 sheep...2 sheep...
