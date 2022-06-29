81a4259197fbcf3c653a48309894d4a7b6305ec7
Spierbal <abdulkadir.f.osman@gmail.com>
2022-06-29 04:18:35 +0100

    T

---
+++ 8kyu/Do I get bonus.js
@@ -0,0 +1,12 @@
+const bonusTime = (salary, bonus) => `£${bonus ? salary * 10 : salary}`;
+
+// Shorter Solution 2:
+//const bonusTime = (salary, bonus) => `£${salary * (bonus ? 10 : 1)}`;
+
+//Longer Solution 3:
+// function bonusTime(salary, bonus) {
+//     if (bonus) return `£${salary * 10}`
+//     return `£${salary}`
+//   }
+
+//console.log(bonusTime(100)); // --> £1000
