af13a3d8426b01e8af3bc41f414a50700e55b711
Spierbal <abdulkadir.f.osman@gmail.com>
2022-06-29 10:08:04 +0100

    T

---
+++ 8kyu/Count by X.js
@@ -0,0 +1,15 @@
+// const countBy = (x, n) => Array.from({ length: n }, (_, i) => x + i * x);
+
+//Solution 2:
+// const countBy = (x, n) => [...Array(n)].map((_, index) => ++index * x);
+
+//Longer Solution 3:
+// function countBy(x, n) {
+//   let z = [];
+
+//   for (let i = 1; i <= n; i++) z.push(x * i);
+
+//   return z;
+// }
+
+// console.log(countBy(2,5)) // --> [ 2, 4, 6, 8, 10 ]
