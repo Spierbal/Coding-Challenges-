b035a9b93a7e2dca0c1f6d76cf96a343e9db2fbe
Spierbal <abdulkadir.f.osman@gmail.com>
2022-07-02 03:54:46 +0100

    T

--- 8kyu/Can we divide it.js
+++ 8kyu/Can we divide it.js
@@ -1,4 +1,7 @@
 const isDivideBy = (number, a, b) => number % a === 0 && number % b === 0;
 
+//Solution 2:
+// const isDivideBy = (number, a, b) => !(number % a || num % b)
+
 //  console.log(isDivideBy(45, 5, 15)); // --> true
 //  console.log(isDivideBy(45, 1, 6)); // --> false
