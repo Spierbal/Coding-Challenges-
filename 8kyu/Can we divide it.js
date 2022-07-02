ffb87a7d4422062d5c09ef291401c32a1ec73a25
Spierbal <abdulkadir.f.osman@gmail.com>
2022-07-02 03:38:41 +0100

    T

---
+++ 8kyu/Can we divide it.js
@@ -0,0 +1,4 @@
+const isDivideBy = (number, a, b) => number % a === 0 && number % b === 0;
+
+//  console.log(isDivideBy(45, 5, 15)); // --> true
+//  console.log(isDivideBy(45, 1, 6)); // --> false
--- 8kyu/Sum Mixed Array.js
+++ 8kyu/Sum Mixed Array.js
@@ -1,3 +1,15 @@
-const sumMix = x => x.reduce((a, b) => a + parseFloat(b), 0);
+const sumMix = x => x.reduce((a, b) => +b + a, 0);
+
+//Solution2:
+//const sumMix = x => x.map(Number).reduce((a, b) => a + b);
+
+//Solution2:
+// function sumMix(x) {
+//     let sum = 0;
+//     x.forEach(num => {
+//       sum += Number(num);
+//     });
+//     return sum;
+//   }
 
 // console.log(sumMix([8, 0, 0, 8, 5, 7, 2, 3, 7, 8, 6, 7])) // --> 61
