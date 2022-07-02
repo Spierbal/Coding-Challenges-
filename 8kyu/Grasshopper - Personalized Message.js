4e3c722dda86388ba201534d4b11ceafc5d38e81
Spierbal <abdulkadir.f.osman@gmail.com>
2022-07-02 23:31:59 +0100

    T

---
+++ 8kyu/Grasshopper - Personalized Message.js
@@ -0,0 +1,4 @@
+const greet = (name, owner) => (name === owner ? "Hello boss" : "Hello guest");
+
+// console.log(greet('Daniel', 'Daniel')) // --> Hello boss
+// console.log(greet('Greg', 'Daniel')) // --> Hello guest
