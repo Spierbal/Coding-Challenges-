8e80dbea3d5c3c84d41357b5cb936ead811ef9ce
Spierbal <abdulkadir.f.osman@gmail.com>
2022-07-03 02:01:05 +0100

    T

--- 8kyu/Reversed Strings.js
+++ 8kyu/Reversed Strings.js
@@ -1,18 +1,13 @@
-// Solution:
-
 const solution = str => str.split("").reverse().join("");
 
-// old Solution:
-function solution(str) {
-  reverse = "";
+// Solution 2:
+// function solution(str) {
+//   reverse = "";
 
-  for (let i = str.length - 1; i >= 0; i--) {
-    reverse += str[i];
-  }
-  return reverse;
-}
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     reverse += str[i];
+//   }
+//   return reverse;
+// }
 
-// input => output
-
-// 'world'  =>  'dlrow'
-// 'word'   =>  'drow'
+// console.log(solution("world")) // --> "dlrow"
