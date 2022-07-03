c1776bc6caed674f52430bb45494457e5ff1da06
Spierbal <abdulkadir.f.osman@gmail.com>
2022-07-03 05:57:18 +0100

    T

--- 8kyu/Remove exclamation marks.js
+++ 8kyu/Remove exclamation marks.js
@@ -1,3 +1,9 @@
 const removeExclamationMarks = s => s.replace(/!/g, "");
 
+//Solution 2:
+//const  removeExclamationMarks = s => [...s].filter(el => el !== "!").join("");
+
+//Solution 3:
+//const removeExclamationMarks = (s) => s.split("!").join("")
+
 //console.log(removeExclamationMarks("Hello World!!!!!")) // --> "Hello World"
