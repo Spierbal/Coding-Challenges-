3a26130225a5940ddd4d42745ddda6a87ad11402
Spierbal <abdulkadir.f.osman@gmail.com>
2022-07-03 01:48:47 +0100

    T

---
+++ 8kyu/Transportation on vacation.js
@@ -0,0 +1,11 @@
+function rentalCarCost(d) {
+  if (d >= 7) return d * 40 - 50;
+  if (d >= 3) return d * 40 - 20;
+
+  return d * 40;
+}
+
+//Solution2 :
+//const rentalCarCost = d => d * 40 - (d >= 7 ? 50 : (d >= 30 ? 20 : 0))
+
+console.log(rentalCarCost(8)); // -270
