51f2138d04cbdbaf64f7ad7043365d2a4f724354
Spierbal <abdulkadir.f.osman@gmail.com>
2022-07-05 05:25:32 +0100

    T

--- 8kyu/Find the remainder.js
+++ 8kyu/Find the remainder.js
@@ -1,26 +1,22 @@
-// Shortest solution:
 const remainder = (a, b) => (a > b ? a % b : b % a);
 
-// More readable than solution 1:
-function remainder(a, b) {
-  return a > b ? a % b : b % a;
-}
+// Solution 2:
+//const remainder = (a, b) => Math.max(a, b) % Math.min(a, b);
 
-// solution 3, less optimized:
+// solution 3, more readable
 // function remainder(a, b) {
-//   if (a > b) {
-//     return a % b;
-//   } else {
-//     return b % a;
-//   }
-// }
-// console.log(remainder(17, 5));
+//   if (a > b) return a % b;
 
-// Solution 4  , with NaN:
-//   function remainder(a, b){
-//     if ( a > b && b === 0 || b < a && a === 0){
-//       return NaN
-//       }else{
-//         return a > b ? a % b : b % a
-//       }
-//   }
+//   return b % a;
+// }
+
+// Solution 3, with NaN:
+// function remainder(a, b){
+//   if ( a > b && b === 0 || b < a && a === 0){
+//     return NaN
+//     }else{
+//       return a > b ? a % b : b % a
+//     }
+// }
+
+// console.log(remainder(17, 5)); // --> 2(remainder)
