2fcd1a8666c5402a75a931e536eb2c02b101112f
Spierbal <abdulkadir.f.osman@gmail.com>
2022-07-07 06:19:14 +0100

    T

--- 8kyu/Double Char.js
+++ 8kyu/Double Char.js
@@ -1,8 +1,16 @@
-function doubleChar(str) {
-  let result = "";
+const doubleChar = str => [...str].map(x => x + x).join("");
 
-  for (let i = 0; i < str.length; i++) {
-    result += str[i].repeat(2);
-  }
-  return result;
-}
+//Solution 2:
+//const doubleChar = str => Array.from(str).map(x => x + x).join("")
+
+//Solution: 3
+// function doubleChar(str) {
+//   let result = "";
+
+//   for (let i = 0; i < str.length; i++) {
+//     result += str[i].repeat(2);
+//   }
+//   return result;
+// }
+
+// console.log(doubleChar("abcd")); // --> "aabbccdd"
--- 8kyu/String repeat.js
+++ 8kyu/String repeat.js
@@ -1,24 +1,20 @@
-// solution 1:
-//shortest solution and most optimized:
 const repeatStr = (n, s) => s.repeat(n);
 
-// 2nd solution:
+//Solution 2:
+//const repeatStr = (n, s) => [...Array(n)].map(i => s).join("");
+
+// 3rd solution:
 //const repeatStr = (n, s) => Array(n).fill(s).join("");
 
 // for Loop solution:
 // function repeatStr (n, s) {
 //   let str = ""
-  
+
 //   for (let i = 0; i< n; i++){
 //     str+=s;
 //   }
 //   return str;
 // }
 
-
-//console.log(repeatStr(3, "ha"))
-
-//Write a function called repeatStr which repeats the given string exactly n times.
-// Examples:
-// repeatStr(6, "I") // "IIIIII"
-// repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+//console.log(repeatStr(3, "ha")) // --> "hahaha"
+//console.log(repeatStr(6, "I")) // --> // "IIIIII"
