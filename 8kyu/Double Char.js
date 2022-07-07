70b0b44c265fbf3a448ca1d794573885110e2a8d
Spierbal <abdulkadir.f.osman@gmail.com>
2022-07-07 03:42:06 +0100

    T

---
+++ 8kyu/Double Char.js
@@ -0,0 +1,8 @@
+function doubleChar(str) {
+  let result = "";
+
+  for (let i = 0; i < str.length; i++) {
+    result += str[i].repeat(2);
+  }
+  return result;
+}
--- 8kyu/Grasshopper-Summation.js
+++ 8kyu/Grasshopper-Summation.js
@@ -1,42 +1,21 @@
-function summation (num) {
-    let sum = 0;
-    
-    for (let i = 1; i <= num; i++){
-      sum += i;
-    }
-    return sum;
-  }
+const summation = num => [...Array(num)].reduce((s, _, i) => s + i + 1, 0);
 
+//Soluton 2:
+// const summation = num =>
+//   Array.from({ length: num }, (_, i) => i + 1).reduce((a, b) => a + b, 0);
 
-// Description:
+// Solution 3:
+//const summation = num => Array(num).fill(0).reduce((s,_,i)=> s + i + 1, 0);
 
-// summation(2) -> 3
-// 1 + 2
+//Solution 3:
+// function summation(num) {
+//   let sum = 0;
 
-// summation(8) -> 36
-// 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+//   for (let i = 1; i <= num; i++) {
+//     sum += i;
+//   }
+//   return sum;
+// }
 
-// Test Cases:
-
-// describe('summation', function () {
-//     it('should return the correct total', function () {
-//       Test.assertEquals(summation(1), 1)
-//       Test.assertEquals(summation(8), 36)
-//       Test.assertEquals(summation(22), 253)
-//       Test.assertEquals(summation(100), 5050)
-//       Test.assertEquals(summation(213), 22791)
-//     })
-//     it('random numbers test', function () {
-//       for (let i = 0; i < 100; i++) {
-//         let rand = Test.randomNumber() + 1;
-//         Test.assertEquals(summation(rand), solution(rand))
-//       }
-//     })
-//     function solution (num) {
-//         let total = 0;
-//         for (let i = 1; i <= num; i++) {
-//           total += i
-//         }
-//         return total
-//       }
-//     })
+// console.log(summation(8)) // --> 36(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+// console.log(summation(5)) // --> 15(1 + 2 + 3 + 4 + 5)
