17b9016778f5c6bf529f1fccd09646f224b6b1bf
Spierbal <abdulkadir.f.osman@gmail.com>
2022-07-14 12:05:33 +0100

    T

---
+++ 8kyu/Correct the mistakes of the character recognition software.js
@@ -0,0 +1,15 @@
+const correct = s => s.replace(/5/g, "S").replace(/0/g, "O").replace(/1/g, "I");
+
+//Solution 2:
+
+// const makeCorrections = {
+//   5: "S",
+//   0: "O",
+//   1: "I",
+// };
+
+//const correct = s => s.replace(/[501]/g, char => makeCorrections[char]);
+
+console.log(correct("L0ND0N")); // --> LONDON
+console.log(correct("DUBL1N")); // --> DUBLIN
+console.log(correct("BUDAPE5T")); // --> BUDAPEST
