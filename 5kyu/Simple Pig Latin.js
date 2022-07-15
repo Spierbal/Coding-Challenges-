b4ae373b294f06344fb8672b73813fa62d8bc1ad
Spierbal <abdulkadir.f.osman@gmail.com>
2022-07-15 23:10:46 +0100

    T

---
+++ 5kyu/Simple Pig Latin.js
@@ -0,0 +1,10 @@
+function pigIt(str) {
+  return str
+    .split(" ")
+    .map(w => (w.match(/\w/) ? `${w.slice(1)}${w.slice(0, 1)}ay` : w))
+    .join(" ");
+}
+//Solution 2:
+const pigIt = str => str.replace(/(\w)(\w*)/g, "$2$1ay");
+
+// console.log(pigIt("Pig latin is cool")); // --> igPay atinlay siay oolcay
