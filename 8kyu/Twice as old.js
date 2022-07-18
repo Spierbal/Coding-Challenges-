6d5b3417caa62f79c6bf866d0c46d6b6e76229d5
Spierbal <abdulkadir.f.osman@gmail.com>
2022-07-18 17:07:56 +0100

    T

--- 8kyu/Twice as old.js
+++ 8kyu/Twice as old.js
@@ -1,3 +1,9 @@
 const twiceAsOld = (dadAge, sonAge) => Math.abs(dadAge - sonAge * 2);
 
+//Solution2:
+function twiceAsOld(dadAge, sonAge) {
+  return dadAge - sonAge * 2 < 0
+    ? dadAge - sonAge * 2 * -1
+    : dadAge - sonAge * 2;
+}
 console.log(twiceAsOld(36, 7)); // --> 22
