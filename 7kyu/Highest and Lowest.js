34d84d877fa22aceffe84d780b02b70df2bd1fc8
Spierbal <abdulkadir.f.osman@gmail.com>
2022-07-19 20:11:05 +0100

    T

---
+++ 7kyu/Highest and Lowest.js
@@ -0,0 +1,10 @@
+function highAndLow(numbers) {
+  const n = numbers.split(" ");
+  return `${Math.max(...n)} ${Math.min(...n)}`;
+}
+
+//Solution 2:
+const highAndLow = numbers =>
+  `${Math.max(...numbers.split(" "))} ${Math.min(...numbers.split(" "))}`;
+
+console.log(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4")); // --> 42 -9
