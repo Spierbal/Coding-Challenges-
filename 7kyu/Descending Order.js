addba35c296717995055c4abb14c41da4dc73e80
Spierbal <abdulkadir.f.osman@gmail.com>
2022-07-20 02:18:51 +0100

    T

---
+++ 7kyu/Descending Order.js
@@ -0,0 +1,19 @@
+const descendingOrder = n => Number([...String(n)].sort().reverse().join(""));
+
+//Solution 2:
+const descendingOrder = n => +Array.from(String(n)).sort().reverse().join("");
+
+//Solution 3
+// const descendingOrder = n =>
+//   Number(String(n).split("").sort().reverse().join(""));
+
+// Solution 4
+// function descendingOrder(n) {
+//   return Number(
+//     String(n)
+//       .split("")
+//       .sort((a, b) => b - a)
+//       .join("")
+//   );
+// }
+console.log(descendingOrder(1021)); // 2110
