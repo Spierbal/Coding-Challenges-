45630db1fe45122f36cd3366537da4709df085db
Spierbal <abdulkadir.f.osman@gmail.com>
2022-07-21 00:41:01 +0100

    T

--- 6kyu/Find the odd int.js
+++ 6kyu/Find the odd int.js
@@ -1,14 +1,14 @@
-function findOdd(A) {
-  let counter = 0;
+// function findOdd(A) {
+//   let counter = 0;
 
-  for (let i = 0; i < A.length; i++) {
-    let Arr = A[i];
-    for (let j = 0; j < A.length; j++) {
-      if (A[i] === A[j]) counter++;
-    }
-    if (counter % 2 !== 0) return Arr;
-  }
-}
+//   for (let i = 0; i < A.length; i++) {
+//     let Arr = A[i];
+//     for (let j = 0; j < A.length; j++) {
+//       if (A[i] === A[j]) counter++;
+//     }
+//     if (counter % 2 !== 0) return Arr;
+//   }
+// }
 
 // Shortest Solution:
 
---
+++ 7kyu/Get the Middle Character.js
@@ -0,0 +1,13 @@
+function getMiddle(s) {
+  const middle = Math.floor(s.length / 2);
+
+  return s.length % 2 ? s.charAt(middle) : s.slice(middle - 1, middle + 1);
+}
+
+function getMiddle(s) {
+  const middle = Math.floor(s.length / 2);
+
+  return s.length % 2 === 0 ? s[middle - 1] + s[middle] : s[middle];
+}
+// console.log(getMiddle("test")); // --> es
+// console.log(getMiddle("testing")); // --> t
