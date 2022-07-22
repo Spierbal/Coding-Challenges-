13028778c95fdea79b26f7100b6c28f0bccd6a2f
Spierbal <abdulkadir.f.osman@gmail.com>
2022-07-22 23:27:35 +0100

    T

--- 7kyu/Square Every Digit.js
+++ 7kyu/Square Every Digit.js
@@ -1,20 +1,18 @@
 const squareDigits = num => Number([...String(num)].map(x => x ** 2).join(""));
 
 //Solution 2:
+//const squareDigits = num => +[...String(num)].map(x => Math.pow(x, 2)).join("");
+
 // const squareDigits = n =>
 //   +Array.from(String(n))
 //     .map(x => x**2)
 //     .join("");
 
-//Solution 3:
+//Solution 4:
 // const squareDigits = num =>
 //   Number(String(num).split("").map(x => x ** 2).join(""));
 
 //Solution 4:
-const squareDigits = num => +[...String(num)].map(x => Math.pow(x, 2)).join("");
+
 // console.log(squareDigits(3212)) // => 9414
 // console.log(squareDigits(2112)) // => 4114
-
-// function squareDigits(num){
-//     return +String(num).split("").map(x => x**2).join("")
-//   }
