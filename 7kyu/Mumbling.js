57325a2f3b7c2b444e372e823fbe044ab063d8ee
Spierbal <abdulkadir.f.osman@gmail.com>
2022-07-23 20:54:52 +0100

    T

--- 7kyu/Mumbling.js
+++ 7kyu/Mumbling.js
@@ -1,4 +1,4 @@
 const accum = s =>
   [...s].map((c, i) => c.toUpperCase() + c.toLowerCase().repeat(i)).join("-");
 
-console.log(accum("ZpglnRxqenU")); // Z-Pp-Ggg-Llll-Nnnnn
+// console.log(accum("ZpglnRxqenU")); // Z-Pp-Ggg-Llll-Nnnnn
