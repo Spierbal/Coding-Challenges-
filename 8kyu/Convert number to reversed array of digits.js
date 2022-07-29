091abffc1d27cbb2025ce0ff1a0df7db126414c4
Spierbal <abdulkadir.f.osman@gmail.com>
2022-07-30 00:54:37 +0100

    T

--- 8kyu/Convert number to reversed array of digits.js
+++ 8kyu/Convert number to reversed array of digits.js
@@ -1,9 +1,9 @@
 digitize = n => [...String(n)].map(Number).reverse();
 
-// Solution 2
-const digitize = n => String(n).split("").map(Number).reverse();
-
-// Solution 3:
+// Solution 2:
 //const digitize = n => Array.from(String(n), Number).reverse()
+
+// Solution 3
+// const digitize = n => String(n).split("").map(Number).reverse();
 
 // console.log(digitize(35231)); // --> [1,3,2,5,3]
