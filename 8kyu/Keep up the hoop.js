07e5e190eaadfb5334e732f991e3a897ab1dab4b
Spierbal <abdulkadir.f.osman@gmail.com>
2022-07-29 23:22:20 +0100

    T

---
+++ 8kyu/Keep up the hoop.js
@@ -0,0 +1,5 @@
+const hoopCount = n =>
+  n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it";
+
+console.log(hoopCount(34)); // Great, now move on to tricks
+console.log(hoopCount(3)); // Keep at it until you get it
