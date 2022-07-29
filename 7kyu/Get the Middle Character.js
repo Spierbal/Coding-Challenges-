a16d13aff8611f4c66401d210da2d7d282aea5cb
Spierbal <abdulkadir.f.osman@gmail.com>
2022-07-30 00:51:21 +0100

    T

--- 7kyu/Get the Middle Character.js
+++ 7kyu/Get the Middle Character.js
@@ -4,10 +4,11 @@ function getMiddle(s) {
   return s.length % 2 ? s.charAt(middle) : s.slice(middle - 1, middle + 1);
 }
 
-function getMiddle(s) {
-  const middle = Math.floor(s.length / 2);
+// Solution 2:
+// function getMiddle(s) {
+//   const middle = Math.floor(s.length / 2);
 
-  return s.length % 2 === 0 ? s[middle - 1] + s[middle] : s[middle];
-}
+//   return s.length % 2 === 0 ? s[middle - 1] + s[middle] : s[middle];
+// }
 // console.log(getMiddle("test")); // --> es
 // console.log(getMiddle("testing")); // --> t
--- 8kyu/Squash the bugs.js
+++ 8kyu/Squash the bugs.js
@@ -11,7 +11,7 @@ function findLongest(str) {
 }
 
 // Solution 2:
-//const findLongest = (str) => Math.max(...str.split(' ').map((word) => word.length));
+//const findLongest = (str) => Math.max(...str.split(' ').map(word => word.length));
 
 //console.log(findLongest("The quick white fox jumped around the massive dog")) // --> 7
 //console.log(findLongest("Take me to tinseltown with you")) // --> 10
