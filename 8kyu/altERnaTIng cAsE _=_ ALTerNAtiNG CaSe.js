0cc8cb5dac16043b075168c1430a03e5a19d1fb9
Spierbal <abdulkadir.f.osman@gmail.com>
2022-08-10 23:57:47 +0100

    T

--- 8kyu/altERnaTIng cAsE _=_ ALTerNAtiNG CaSe.js
+++ 8kyu/altERnaTIng cAsE _=_ ALTerNAtiNG CaSe.js
@@ -3,3 +3,8 @@ String.prototype.toAlternatingCase = function () {
     /[a-z]/.test(char) ? char.toUpperCase() : char.toLowerCase()
   );
 };
+
+//  console.log("hello world".toAlternatingCase()) // --> HELLO WORLD
+//  console.log("HELLO WORLD".toAlternatingCase()) // --> hello world
+//  console.log("hello WORLD".toAlternatingCase()) // --> HELLO world
+//  console.log("HeLLo WoRLD".toAlternatingCase()) // --> hEllO wOrld
