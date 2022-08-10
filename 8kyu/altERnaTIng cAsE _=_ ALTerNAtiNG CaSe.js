e5b089fb7ae640c81e092d8ea1835bcfaeda9fbb
Spierbal <abdulkadir.f.osman@gmail.com>
2022-08-11 00:38:38 +0100

    T

--- 8kyu/altERnaTIng cAsE _=_ ALTerNAtiNG CaSe.js
+++ 8kyu/altERnaTIng cAsE _=_ ALTerNAtiNG CaSe.js
@@ -1,14 +1,14 @@
 String.prototype.toAlternatingCase = function () {
-  return this.replace(/[A-z]/g, char =>
-    /[a-z]/.test(char) ? char.toUpperCase() : char.toLowerCase()
-  );
+  return [...this]
+    .map(c => (c === c.toUpperCase() ? c.toLowerCase() : c.toUpperCase()))
+    .join("");
 };
 
 // Solution 2:
 // String.prototype.toAlternatingCase = function () {
-//   return [...this]
-//     .map(c => (c === c.toUpperCase() ? c.toLowerCase() : c.toUpperCase()))
-//     .join("");
+//   return this.replace(/[A-z]/g, char =>
+//     /[a-z]/.test(char) ? char.toUpperCase() : char.toLowerCase()
+//   );
 // };
 
 //  console.log("hello world".toAlternatingCase()) // --> HELLO WORLD
