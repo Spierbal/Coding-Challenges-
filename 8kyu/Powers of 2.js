488ba6cde41cd974437f69fe5a71190f05d3f427
Spierbal <abdulkadir.f.osman@gmail.com>
2022-08-15 01:17:16 +0100

    T

--- 8kyu/Powers of 2.js
+++ 8kyu/Powers of 2.js
@@ -1,3 +1,6 @@
-const powersOfTwo = n => Array.from({ length: n + 1 }, (_, i) => 2 ** i);
+const powersOfTwo = n => [...Array(n + 1)].map((_, x) => 2 ** x);
+
+//Solution 2:
+//const powersOfTwo = n => Array.from({ length: n + 1 }, (_, i) => 2 ** i);
 
 // console.log(powersOfTwo(1)); // --> [ 1, 2 ]
