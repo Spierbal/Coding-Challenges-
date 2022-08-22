c5100c8f248786620e90a7a60173ebea37fa01dc
Spierbal <abdulkadir.f.osman@gmail.com>
2022-08-22 04:25:00 +0100

    T

--- 8kyu/Third Angle of a Triangle.js
+++ 8kyu/Third Angle of a Triangle.js
@@ -1,16 +1,3 @@
 const otherAngle = (a, b) => 180 - (a + b);
 
 //console.log(otherAngle(60, 30)); // --> 90;
-
-const shape = {
-  radius: 10,
-  diameter() {
-    return this.radius * 2;
-  },
-  perimeter() {
-    return 2 * Math.PI * this.radius;
-  },
-};
-
-console.log(shape.diameter());
-console.log(shape.perimeter());
