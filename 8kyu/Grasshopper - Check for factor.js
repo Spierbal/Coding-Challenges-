617630d731615e81faece0a9dadba367cf22ef1d
Spierbal <abdulkadir.f.osman@gmail.com>
2022-09-02 23:27:31 +0100

    T

--- 8kyu/Grasshopper - Check for factor.js
+++ 8kyu/Grasshopper - Check for factor.js
@@ -1,4 +1,4 @@
 const checkForFactor = (base, factor) => base % factor === 0;
 
-console.log(checkForFactor(63, 7)); // true;
-console.log(checkForFactor(9, 2)); // false;
+// console.log(checkForFactor(63, 7)); // true;
+// console.log(checkForFactor(9, 2)); // false;
