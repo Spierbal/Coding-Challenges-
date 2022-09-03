8faaaba2963b3ce5bb1adc8c9d81234045e797a2
Spierbal <abdulkadir.f.osman@gmail.com>
2022-09-03 01:03:58 +0100

    T

--- 8kyu/Sort and Star.js
+++ 8kyu/Sort and Star.js
@@ -1,4 +1,11 @@
-function twoSort(s) {
-  return s.sort()[0].split("").join("***");
-}
+const twoSort = s => [...s.sort()[0]].join("***");
+
+// Solution 2:
+// const twoSort = s => Array.from((s).sort()[0]).join("***");
+
+// Solution 3:
+// function twoSort(s) {
+//   return s.sort()[0].split("").join("***");
+// }
+
 // console.log(twoSort(["Hello", "World"])); // --> H***e***l***l***o
