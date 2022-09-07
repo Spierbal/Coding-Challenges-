9770563d2d2b4d431ec95723ad39d765bb70b765
Spierbal <abdulkadir.f.osman@gmail.com>
2022-09-07 01:41:16 +0100

    T

--- 8kyu/Expressions Matter.js
+++ 8kyu/Expressions Matter.js
@@ -1,5 +1,18 @@
-function expressionMatter(a, b, c) {
-  const add = a + b + c;
-  const cross = a * b * c;
-  return Math.max(add, cross, (a + b) * c, a * (b + c));
-}
+const expressionMatter = (a, b, c) =
+  Math.max(a + b + c, a * b * c, (a + b) * c, a * (b + c));
+
+
+// function expressionMatter(a, b, c) {
+//   const values = [a + b + c, a * b * c, a * (b + c), (a + b) * c];
+//   return Math.max(...values);
+// }
+
+
+// function expressionMatter(a, b, c) {
+//   const values1 = a + b + c;
+//   const values2 = a * b * c;
+//   const values3 = a * (b + c);
+//   const values4 = (a + b) * c;
+
+//   return Math.max(values1, values2, values3, values4);
+// }
