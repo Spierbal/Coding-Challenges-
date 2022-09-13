d5e79d545f035df793d4815d53d1797c7d151326
Spierbal <abdulkadir.f.osman@gmail.com>
2022-09-13 18:45:21 +0100

    T

--- 8kyu/Sum The Strings.js
+++ 8kyu/Sum The Strings.js
@@ -1,3 +1,9 @@
 const sumStr = (a, b) => +a + +b + "";
 
+// Solution 2:
+// const sumStr = (a, b) => String(+a + +b)
+
+// Solution 3:
+// const sumStr = (a, b) => String(Number(a)+ Number(b));
+
 console.log(sumStr("4", "5")); // --> 9;
