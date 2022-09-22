7356e591817ce85af1fd4f6a3c550510020f5e98
Spierbal <abdulkadir.f.osman@gmail.com>
2022-09-23 00:22:58 +0100

    T

---
+++ 8kyu/filter out the geese.js
@@ -0,0 +1,19 @@
+const geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+
+const gooseFilter = birds => birds.filter(bird => !geese.includes(bird));
+
+// Solution 2:
+function gooseFilter(birds) {
+  const geese = [
+    "African",
+    "Roman Tufted",
+    "Toulouse",
+    "Pilgrim",
+    "Steinbacher",
+  ];
+
+  return birds.filter(bird => !geese.includes(bird));
+}
+
+//console.log(gooseFilter(["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"])) --> []
+// console.log(gooseFilter(["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"])) // --> ["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
