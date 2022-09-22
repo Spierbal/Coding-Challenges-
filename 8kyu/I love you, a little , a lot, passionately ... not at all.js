e6c50dbc8f7b191735b6b05f06f4ae8dc12c127f
Spierbal <abdulkadir.f.osman@gmail.com>
2022-09-22 23:45:04 +0100

    T

--- 8kyu/I love you, a little , a lot, passionately ... not at all.js
+++ 8kyu/I love you, a little , a lot, passionately ... not at all.js
@@ -1,26 +1,25 @@
-function howMuchILoveYou(nbPetals) {
-  const phrases = [
-    "I love you",
-    "a little",
-    "a lot",
-    "passionately",
-    "madly",
-    "not at all",
-  ];
-  return phrases[(nbPetals - 1) % 6];
-}
+const phrases = [
+  "I love you",
+  "a little",
+  "a lot",
+  "passionately",
+  "madly",
+  "not at all",
+];
+const howMuchILoveYou = n => phrases[(n - 1) % 6];
 
-// Solution 2:
+// solution 2:
+// function howMuchILoveYou(nbPetals) {
+//   const phrases = [
+//     "I love you",
+//     "a little",
+//     "a lot",
+//     "passionately",
+//     "madly",
+//     "not at all",
+//   ];
+//   return phrases[(nbPetals - 1) % 6];
+// }
 
-// const phrases = [
-//   "I love you",
-//   "a little",
-//   "a lot",
-//   "passionately",
-//   "madly",
-//   "not at all",
-// ];
-// const howMuchILoveYou = n => phrases[(n - 1) % 6];
-
-console.log(howMuchILoveYou(8)); // --> "a little"
-console.log(howMuchILoveYou(3)); // --> "a lot"
+// console.log(howMuchILoveYou(8)); // --> "a little"
+// console.log(howMuchILoveYou(3)); // --> "a lot"
