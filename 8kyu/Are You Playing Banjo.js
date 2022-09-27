722432cbcbb91155182f99cd2abc18ca2367105d
Spierbal <abdulkadir.f.osman@gmail.com>
2022-09-27 01:59:18 +0100

    T

--- 8kyu/Are You Playing Banjo.js
+++ 8kyu/Are You Playing Banjo.js
@@ -1,24 +1,11 @@
 const areYouPlayingBanjo = name =>
-  name.startsWith("R") || name.startsWith("r")
+  name[0].toLowerCase() === "r"
     ? `${name} plays banjo`
     : `${name} does not play banjo`;
 
 // Solution 2:
-// function areYouPlayingBanjo(name) {
-//   return name[0] === "R" || name[0] === "r"
-//     ? `${name} plays banjo`
-//     : `${name} does not play banjo`;
-// }
-
-// Solution3:
 // const areYouPlayingBanjo = name =>
-//   name[0].toLowerCase() === 'r'
-// 	? `${name} plays banjo`
-// 	: `${name} does not play banjo`;
-
-// Solution 4:
-// const areYouPlayingBanjo = name =>
-//   name[0].toUpperCase() == 'R'
+//   name.startsWith("R") || name.startsWith("r")
 //     ? `${name} plays banjo`
 //     : `${name} does not play banjo`;
 
