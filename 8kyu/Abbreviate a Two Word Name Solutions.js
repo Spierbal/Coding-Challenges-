d549b4d945c4da9969a6bed29479136605068e93
Spierbal <abdulkadir.f.osman@gmail.com>
2022-09-27 01:29:02 +0100

    T

--- 8kyu/Abbreviate a Two Word Name Solutions.js
+++ 8kyu/Abbreviate a Two Word Name Solutions.js
@@ -4,4 +4,10 @@ const abbrevName = name =>
     .map(x => x[0].toUpperCase())
     .join(".");
 
+// Solution 2:
+// function abbrevName(name) {
+//   const initial = name.toUpperCase().split(" ");
+//   return `${initial[0][0]}.${initial[1][0]}`;
+// }
+
 // console.log(abbrevName("Ben Gorden")); // B.G
