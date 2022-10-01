2dde5c75a3513c94d8e5de012ae8785c3e6f2ab5
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-01 18:35:31 +0100

    T

--- 8kyu/Well of Ideas - Easy Version.js
+++ 8kyu/Well of Ideas - Easy Version.js
@@ -1,3 +1,24 @@
+const well = x => {
+  const goodIdeas = x.filter(idea => idea === "good").length;
+
+  if (goodIdeas > 2) return "I smell a series!";
+  if (goodIdeas > 0) return "Publish!";
+
+  return "Fail!";
+};
+
+// Solution 2: Nested Ternary Operator
+// function well(x) {
+//   const goodIdeas = x.filter(idea => idea === "good").length;
+
+//   return goodIdeas > 2
+//     ? "I smell a series!"
+//     : goodIdeas > 0
+//     ? "Publish!"
+//     : "Fail!";
+// }
+
+// Solution 3: Switch Statement
 function well(x) {
   const goodIdeas = x.filter(idea => idea === "good").length;
   return goodIdeas > 2
