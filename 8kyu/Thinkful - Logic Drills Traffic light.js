bf3b79aa2751ab04813d575771d205b43857d57b
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-01 16:39:38 +0100

    T

--- 8kyu/Thinkful - Logic Drills Traffic light.js
+++ 8kyu/Thinkful - Logic Drills Traffic light.js
@@ -1,13 +1,13 @@
-const updateLight = current =>
-  current === "green" ? "yellow" : current === "yellow" ? "red" : "green";
+function updateLight(current) {
+  if (current === "green") return "yellow";
+  if (current === "yellow") return "red";
+
+  return "green";
+}
 
 //Solution 2:
-
-//   function updateLight(current) {
-//     if (current === "green") return "yellow"
-//     if (current === "yellow") return "red"
-//     return "green"
-//    }
+// const updateLight = current =>
+//   current === "green" ? "yellow" : current === "yellow" ? "red" : "green";
 
 // console.log(updateLight("green")); // --> yellow
 // console.log(updateLight("yellow")); // --> red
