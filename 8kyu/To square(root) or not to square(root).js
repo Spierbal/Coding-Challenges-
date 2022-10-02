a295814e0b3d545dc160454317319103ead00f2a
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-02 05:57:55 +0100

    T

--- 8kyu/To square(root) or not to square(root).js
+++ 8kyu/To square(root) or not to square(root).js
@@ -1,7 +1,16 @@
 const squareOrSquareRoot = arr =>
-  arr.map(num => (Math.sqrt(num) % 1 === 0 ? Math.sqrt(num) : num ** 2));
+  arr.map(num => (Math.sqrt(x) % 1 === 0 ? Math.sqrt(num) : num * num));
 
 // Solution 2:
+// const squareOrSquareRoot = arr => {
+//   return arr.map(x => {
+//     if (Math.sqrt(x) % 1 === 0) return Math.sqrt(x);
+
+//     return x * x;
+//   });
+// };
+
+// Solution 3:
 // function squareOrSquareRoot(array) {
 //   for (let i = 0; i < array.length; i++) {
 //     if (Math.sqrt(array[i]) % 1 === 0) {
