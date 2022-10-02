da7c7d115a9db68be991f913d4f206ee6d2d7e8c
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-02 04:36:36 +0100

    T

---
+++ 8kyu/Convert a String to a Number.js
@@ -0,0 +1,9 @@
+const stringToNumber = str => parseInt(str);
+
+// const stringToNumber = str => Number(str);
+
+// const stringToNumber = str => +str;
+
+// const stringToNumber = str => str -"";
+
+// console.log(stringToNumber("1234")) // --> 1234
