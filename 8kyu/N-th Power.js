2f303c3d8a8cecd243806c2ec1d576fe26deeb5d
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-04 00:58:51 +0100

    T

--- 8kyu/N-th Power.js
+++ 8kyu/N-th Power.js
@@ -9,3 +9,10 @@ const index = (array, n) => (array.length > n ? Math.pow(array[n], n) : -1);
 
   return Math.pow(array[n], n);
 } */
+
+// Examples:
+// array = [1, 2, 3, 4] and N = 2, then the result is 3^2 == 9;
+// array = [1, 2, 3] and N = 3, but N is outside of the array, so the result is -1.
+
+//console.log(index([1, 2, 3, 4], 2)) // --> 9
+//console.log(index(index([1,1,1,1,1,1,1,1,1,2], 9)) --> 512
