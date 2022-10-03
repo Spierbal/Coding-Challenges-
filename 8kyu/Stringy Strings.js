c9f879cc0c314ea6d4b33dd6a243f5cf92469a46
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-03 17:30:55 +0100

    T

---
+++ 8kyu/Stringy Strings.js
@@ -0,0 +1,18 @@
+const stringy = size =>
+  [...Array(size)].map((_, i) => (i % 2 === 0 ? 1 : 0)).join("");
+
+// Solution 2:
+/*   const stringy = size => 
+  Array.from(Array(size), (_, i) => (i % 2 === 0 ? 1 : 0)).join("");
+
+// Solution 3:
+/* const stringy = size => {
+  let str = "";
+  for (let i = 1; i <= size; i++) {
+    str += i % 2;
+  }
+  return str;
+}; */
+
+//console.log(stringy(3)); //--> "101"
+//console.log(stringy(4)) // --> "1010"
