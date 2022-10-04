4ed2f6843c6fc232929b9916953cdf49f679748d
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-04 20:36:10 +0100

    T

---
+++ 8kyu/Grasshopper - if else syntax debug.js
@@ -0,0 +1,13 @@
+function checkAlive(health) {
+  if (health <= 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+// Short solution:
+const checkAlive = health => health > 0;
+
+// console.log(checkAlive(5)); // --> true;
+// console.log(checkAlive(0)); // --> false;
