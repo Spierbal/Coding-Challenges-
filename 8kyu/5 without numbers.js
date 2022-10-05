bdca933db34a4f9f4af1464dfa4f7e2d9ab98720
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-05 02:04:55 +0100

    T

---
+++ 8kyu/5 without numbers.js
@@ -0,0 +1,13 @@
+const unusualFive = (str = "hello") => str.split("").length;
+
+// Solution 1b
+/* function unusualFive() {
+  const str = "Hello";
+  return str.split("").length;
+}
+ */
+
+// solution 2
+// const unusualFive = () => ["h", "e", "l", "l", "o"].length;
+
+// console.log(unusualFive()) // --> 5
