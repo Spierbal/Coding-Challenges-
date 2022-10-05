ba6f93d3e688cdea23eec8aa3402218635ddf507
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-05 23:53:06 +0100

    T

---
+++ 8kyu/Grasshopper - Combine Strings.js
@@ -0,0 +1,6 @@
+const combineNames = (first, last) => `${first} ${last}`;
+
+// Solution 2:
+// const combineNames = (first, last) => first + " " + last;
+
+//console.log(combineNames("James", "Stevens")); // "James Stevens"
