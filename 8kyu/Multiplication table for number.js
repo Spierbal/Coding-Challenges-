466adb4a36ae1df7c48e767279b84f2ee2789dcd
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-06 23:59:55 +0100

    T

--- 8kyu/Multiplication table for number.js
+++ 8kyu/Multiplication table for number.js
@@ -1,7 +1,8 @@
 const multiTable = n =>
-  Array.from(Array(10), (_, i) => `${i + 1} * ${n} = ${(i + 1) * n}`).join(
-    "\n"
-  );
+  Array.from(
+    Array(10),
+    (_, i) => `${i + 1} * ${number} = ${(i + 1) * number}`
+  ).join("\n");
 
 // Solution 2:
 // const multiTable = n =>
