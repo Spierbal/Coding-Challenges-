2f95745475613a9d971a3710e5d885f815e00279
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-06 23:59:32 +0100

    T

---
+++ 8kyu/Multiplication table for number.js
@@ -0,0 +1,8 @@
+const multiTable = n =>
+  Array.from(Array(10), (_, i) => `${i + 1} * ${n} = ${(i + 1) * n}`).join(
+    "\n"
+  );
+
+// Solution 2:
+// const multiTable = n =>
+//   [...Array(10)].map((_, i) => `${i + 1} * ${n} = ${(i + 1) * n}`).join("\n");
