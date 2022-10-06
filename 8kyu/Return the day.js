19963b30167882c8a4ffa17e1931a72ccab1ec4f
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-06 14:41:52 +0100

    T

---
+++ 8kyu/Return the day.js
@@ -0,0 +1,22 @@
+function whatday(num) {
+  switch (num) {
+    case 0:
+      return "Sunday";
+    case 1:
+      return "Monday";
+    case 2:
+      return "Tuesday";
+    case 3:
+      return "Wednesday";
+    case 4:
+      return "Thursday";
+    case 5:
+      return "Friday";
+    case 6:
+      return "Saturday";
+    default:
+      return "Wrong, please enter a number between 1 and 7";
+  }
+}
+
+// console.log(whatday(1)); //--> "Sunday"
--- 8kyu/Well of Ideas - Easy Version.js
+++ 8kyu/Well of Ideas - Easy Version.js
@@ -19,13 +19,13 @@ const well = x => {
 // }
 
 // Solution 3: Switch Statement
-function well(x) {
+/* function well(x) {
   const goodIdeas = x.filter(idea => idea === "good").length;
   return goodIdeas > 2
     ? "I smell a series!"
     : goodIdeas > 0
     ? "Publish!"
     : "Fail!";
-}
+} */
 
 //console.log(well(['good', 'bad', 'bad', 'bad', 'bad'])) // --> "Publish"
