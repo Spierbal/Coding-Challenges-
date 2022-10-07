a2dec9ff4067c6d3d767a43c79de5cd06b2e6ff7
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-07 14:13:09 +0100

    T

--- 8kyu/Multiplication table for number.js
+++ 8kyu/Multiplication table for number.js
@@ -1,9 +1,31 @@
-const multiTable = number =>
-  Array.from(
-    Array(10),
-    (_, i) => `${i + 1} * ${number} = ${(i + 1) * number}`
-  ).join("\n");
+const multiTable = n =>
+  [...Array(10)].map((_, i) => `${i + 1} * ${n} = ${(i + 1) * n}`).join("\n");
 
 // Solution 2:
-// const multiTable = n =>
-//   [...Array(10)].map((_, i) => `${i + 1} * ${n} = ${(i + 1) * n}`).join("\n");
+/* const multiTable = n =>
+  Array.from(Array(10), (_, i) => `${i + 1} * ${n} = ${(i + 1) * n}`).join(
+    "\n"
+  ); */
+
+// Solution 3:
+/* function multiTable(num) {
+  let result = "";
+  for (let i = 1; i <= 10; i++) {
+    result += `${i} * ${num} = ${i * num}\n`;
+  }
+  return result.slice(0, -1);
+}
+ */
+
+// console.log(multiTable(5)); // -->
+/* output:
+1 * 5 = 5
+2 * 5 = 10
+3 * 5 = 15
+4 * 5 = 20
+5 * 5 = 25
+6 * 5 = 30
+7 * 5 = 35
+8 * 5 = 40
+9 * 5 = 45
+10 * 5 = 50  */
