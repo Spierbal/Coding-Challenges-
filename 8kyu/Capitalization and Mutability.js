51b345d815546eac0c511c4d005e9e44726e378b
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-07 20:53:28 +0100

    T

---
+++ 8kyu/Capitalization and Mutability.js
@@ -0,0 +1,4 @@
+const capitalizeWord = word => word[0].toUpperCase() + word.slice(1);
+
+// console.log(capitalizeWord('word')) // --> "Word"
+// console.log(capitalizeWord('glassWear')) // --> "Glasswear"
