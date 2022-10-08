3487e55a1cffb89957f78685912bc7a4e5c7778d
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-08 22:45:12 +0100

    T

--- 8kyu/Get Nth Even Number.js
+++ 8kyu/Get Nth Even Number.js
@@ -1,4 +1,7 @@
 const nthEven = n => n * 2 - 2;
 
+// Solution 2:
+// const nthEven = n => --n * 2; // '--2 * 2' is the same as '(n - 1) * 2'
+
 // console.log(nthEven(1)); //--> 0
 // console.log(nthEven(100)); //--> 198
