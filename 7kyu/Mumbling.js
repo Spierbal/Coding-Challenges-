37b2326ad9426e7b43f101583b19833e17192dd5
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-08 03:04:51 +0100

    T

--- 7kyu/Mumbling.js
+++ 7kyu/Mumbling.js
@@ -1,4 +1,21 @@
 const accum = s =>
   [...s].map((c, i) => c.toUpperCase() + c.toLowerCase().repeat(i)).join("-");
 
-// console.log(accum("ZpglnRxqenU")); // Z-Pp-Ggg-Llll-Nnnnn
+// Solution 2:
+/* const accum = s => {
+  return s
+    .split("")
+    .map((c, i) => c.toUpperCase() + c.toLowerCase().repeat(i))
+    .join("-");
+} */
+
+// Solution 2:
+const accum = s =>
+  Array.from(
+    { length: s },
+    (c, i) => c.toUpperCase() + c.toLowerCase().repeat(i)
+  ).join("-");
+
+// console.log(accum("ZpglnRxqenU")); // "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu")
+
+//const countBy = (x, n) => Array.from({ length: n }, (_, i) => x + i * x);
