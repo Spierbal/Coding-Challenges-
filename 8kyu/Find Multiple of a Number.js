813a97d1bff56b10ca087bec4d7f4b6092f6e8a1
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-08 23:59:33 +0100

    T

--- 8kyu/Find Multiple of a Number.js
+++ 8kyu/Find Multiple of a Number.js
@@ -7,3 +7,5 @@ function findMultiples(integer, limit) {
 }
 
 // console.log(findMultiples(5, 25)) // --> [5, 10, 15, 20, 25]
+// console.log(findMultiples(5, 7)) // --> [5]
+// console.log(findMultiples(4, 27)) // --> [4, 8, 12, 16, 20, 24]
