40b5809f236447f998feba4bec719efbf6718b65
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-08 03:09:11 +0100

    T

--- 7kyu/Mumbling.js
+++ 7kyu/Mumbling.js
@@ -9,13 +9,4 @@ const accum = s =>
     .join("-");
 } */
 
-// Solution 2:
-const accum = s =>
-  Array.from(
-    { length: s },
-    (c, i) => c.toUpperCase() + c.toLowerCase().repeat(i)
-  ).join("-");
-
 // console.log(accum("ZpglnRxqenU")); // "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu")
-
-//const countBy = (x, n) => Array.from({ length: n }, (_, i) => x + i * x);
