52ab81a865c57a9e598d8645a6064d94bfcd1280
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-09 04:06:34 +0100

    T

--- 6kyu/Multiples of 3 or 5.js
+++ 6kyu/Multiples of 3 or 5.js
@@ -1,10 +1,37 @@
 function solution(number) {
   let result = 0;
 
-  for (let i = 0; i < number; i++) {
+  for (let i = 3; i < number; i++) {
     if (i % 3 === 0 || i % 5 === 0) result += i;
   }
   return result;
 }
 
-// console.log(solution(10)); --> 23
+// Solution 2:
+/* function solution(n) {
+  return n < 1
+    ? 0
+    : Array.from(Array(n))
+        .map((_, i) => i)
+        .reduce((x, y) => x + (y % 3 === 0 || y % 5 === 0 ? y : 0));
+} */
+
+// Solution 3:
+/* function solution(n) {
+  if (n < 0) return 0;
+
+  return Array.from(Array(n).keys())
+    .filter(n => n % 3 === 0 || n % 5 === 0)
+    .reduce((a, b) => a + b, 0);
+}
+ */
+
+// Solution 4:
+/* function solution(number) {
+  return number < 1
+    ? 0
+    : [...Array(number).keys()]
+        .filter(n => n % 3 == 0 || n % 5 == 0)
+        .reduce((a, b) => a + b);
+}
+ */
--- 8kyu/Sum of Multiples.js
+++ 8kyu/Sum of Multiples.js
@@ -1,13 +1,13 @@
 function sumMul(n, m) {
-  if (n > m || n <= 0 || m <= 0) return "INVALID";
-
-  // make a total
+  if (n <= 0 || m <= 0) return "INVALID";
   let total = 0;
-
   for (let i = n; i < m; i += n) {
-    // in every iteration get a multiple of n
-    // add it to the total
     total += i;
   }
   return total;
 }
+
+console.log(sumMul(2, 9)); // --> 20 (2 + 4 + 6 + 8 = 20)
+console.log(sumMul(4, -7)); //-->"INVALID"
+console.log(sumMul(0, 0)); // --> "INVALID"
+console.log(sumMul(3, 13)); // -->30 (3+6+9+12 = 30)
