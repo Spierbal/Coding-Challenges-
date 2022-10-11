102a51f8c307524a67fd333b589e17785e63f763
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-11 08:37:13 +0100

    T

--- 8kyu/Removing Elements.js
+++ 8kyu/Removing Elements.js
@@ -3,16 +3,15 @@ const removeEveryOther = arr => arr.filter((_, index) => index % 2 === 0);
 //const removeEveryOther = arr => arr.filter((_, i) => (i + 1) % 2 !== 0);
 //const removeEveryOther = arr => arr.filter((_, i) => (i + 2) % 2 === 0);
 
-// Solution 2: for...in loop
-// function removeEveryOther(arr) {
-//   let r = [];
+// Solution 2:
+function removeEveryOther(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (i % 2 === 0) result.push(arr[i]);
+  }
+  return result;
+}
 
-//   for (let i in arr) {
-//     if (i % 2 === 0) r.push(arr[i]);
-//   }
-//   return r;
-// }
+// console.log(removeEveryOther(["Hello", "Goodbye", "Hello Again"])); // --> [ 'Hello', 'Hello Again' ]
 
-// console.log(removeEveryOther(['Hello', 'Goodbye', 'Hello Again'])) // --> [ 'Hello', 'Hello Again' ]
-
-//console.log(removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])) // --> [ 1, 3, 5, 7, 9 ]
+// console.log(removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])); // --> [ 1, 3, 5, 7, 9 ]
