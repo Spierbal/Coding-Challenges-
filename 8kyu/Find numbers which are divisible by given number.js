9de2a19f664671d0185a50dbd6f515fbbe6f7402
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-11 07:11:26 +0100

    T

--- 8kyu/Find numbers which are divisible by given number.js
+++ 8kyu/Find numbers which are divisible by given number.js
@@ -1,22 +1,22 @@
-const divisibleBy = (n, divisor) => n.map(num => num % divisor === 0);
+const divisibleBy = (n, divisor) => n.filter(num => num % divisor === 0);
 
 //Solution 2:
-// function divisibleBy(n, divisor) {
-//   let newArr = [];
-//   n.map(x => {
-//     if (x % divisor === 0) newArr.push(x);
-//   });
-//   return newArr;
-// }
-
-//console.log(divisibleBy([1, 2, 3, 4, 5, 6], 2)); // --> [ 2, 4, 6 ]
+/* function divisibleBy(n, divisor) {
+  let newArr = [];
+  n.map(x => {
+    if (x % divisor === 0) newArr.push(x);
+  });
+  return newArr;
+} */
 
 //Solution 3:
-// function divisibleBy(n, divisor) {
-//     let newArr = [];
+/* function divisibleBy(n, divisor) {
+  let newArr = [];
+  
+  for (let i = 0; i < n.length; i++) {
+      if (n[i] % divisor === 0) newArr.push(n[i]);
+    }
+  return newArr;
+} */
 
-//     for (let i = 0; i < n.length; i++) {
-//         if(n[i] % divisor === 0) newArr.push(n[i])
-//    }
-//    return newArr
-//   }
+//console.log(divisibleBy([1, 2, 3, 4, 5, 6], 2)); // --> [ 2, 4, 6 ]
