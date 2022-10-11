6fa8172fba902281c1e2f588ea38d8820905f002
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-11 05:42:57 +0100

    T

--- 6kyu/Find the odd int.js
+++ 6kyu/Find the odd int.js
@@ -1,19 +1,24 @@
-// function findOdd(A) {
-//   let counter = 0;
+const findOdd = arr =>
+  arr.find(item => arr.filter(el => el === item).length % 2);
 
-//   for (let i = 0; i < A.length; i++) {
-//     let Arr = A[i];
-//     for (let j = 0; j < A.length; j++) {
-//       if (A[i] === A[j]) counter++;
-//     }
-//     if (counter % 2 !== 0) return Arr;
-//   }
-// }
+// Solution 2:
+/* function findOdd(A) {
+  for (let i = 0; i < A.length; i++) {
+    if (A.filter(item => item === A[i]).length % 2 !== 0) {
+      return A[i];
+    }
+  }
+} */
 
-// Shortest Solution:
+// Solution 2b:
+/* function findOdd(A) {
+  for (let int of A) {
+    if (A.filter(el => el === int).length % 2) {
+      return int;
+    }
+  }
+}
+ */
 
-// const findOdd = arr =>
-//   arr.find((item) => arr.filter(el => el === item).length % 2);
-
-// console.log(findOdd([20,1,1,2,2,3,3,3,5,5,4,20,4], 5)); --> 5 appears three times which is odd.
-// console.log(findOdd([1,1,2,-2,5,2,4,4,-1,-2,5], -1)) --> -1 appears one time which is odd.
+// console.log(findOdd([1,1,2])); // --> return 2, because it occurs 1 time(odd)
+// console.log(findOdd([0, 1, 0, 1, 0])); // --> returns 0, because it occurs 3 times(which is odd)
--- 8kyu/Find the first non-consecutive number.js
+++ 8kyu/Find the first non-consecutive number.js
@@ -1,60 +1,25 @@
 function firstNonConsecutive(arr) {
   for (let i = 0; i < arr.length - 1; i++) {
-    const consecutive = arr[i + 1];
-    if (arr[i] + 1 !== consecutive) return consecutive;
+    if (arr[i] + 1 !== arr[i + 1]) return arr[i + 1];
   }
   return null;
 }
 
-// Solution 2: 21 steps
-const firstNonConsecutive = arr =>
-  [...arr.filter(n => !arr.includes(n - 1)), null][1];
+// Solution 2a:
+/* firstNonConsecutive = arr =>
+  (arr = arr.filter((e, i) => e !== arr[0] + i)[0]) === undefined ? null : arr; */
 
-console.log(firstNonConsecutive([1, 2, 3, 4, 6, 7, 8])); // --> 6
+// Solution 2b:
+/* firstNonConsecutive = a =>
+  (a = a.filter((e, i) => e - 1 !== a[i - 1])[1]) === undefined ? null : a; */
 
-// 18 steps
-const firstNonConsecutive = arr => {
-  return arr
-    .slice(1)
-    .filter((a, i) => a != arr[i] + 1)
-    .concat(null)[0];
-};
-
-// Great 13 steps
-function firstNonConsecutive(arr) {
-  const index = arr.findIndex((e, i) => arr[i + 1] - e !== 1) + 1;
-  return index === -1 || index === arr.length ? null : arr[index];
-}
-
-// 18 steps
-function firstNonConsecutive(arr) {
-  return arr.reduce(
-    (a, i, ind) =>
-      ind === 0 || i === arr[ind - 1] + 1 || a !== null ? a : (a = i),
-    null
-  );
-}
-
-// 16 steps
-const firstNonConsecutive = arr => {
-  let num = arr.find((e, i, a) => (i > 0 ? e - 1 !== a[i - 1] : false));
-  return num === 0 ? 0 : num || null;
-};
-
-// 19 steps
-const firstNonConsecutive = a =>
-  (a = a.filter((e, i) => e != a[0] + i)[0]) === undefined ? null : a;
-
-// 19 steps
-function firstNonConsecutive(arr) {
-  const result = arr.filter((el, i) => el - 1 != arr[i - 1])[1];
-
+// Solution 2c:
+/* function firstNonConsecutive(arr) {
+  const result = arr.filter((el, i) => {
+    return el - 1 !== arr[i - 1];
+  })[1];
   return result == undefined ? null : result;
-}
+} */
 
-// 19 steps
-firstNonConsecutive = arr =>
-  (arr = arr.filter((t, i) => t != arr[0] + i)[0]) == undefined ? null : arr;
-
-firstNonConsecutive = a =>
-  (a = a.filter((e, i) => e - a[i - 1] !== 1)[1]) === undefined ? null : a;
+//console.log(firstNonConsecutive([1, 2, 3, 4, 6, 7, 8])); // --> 6
+//console.log(firstNonConsecutive([1, 2, 3, 4])); // --> null
