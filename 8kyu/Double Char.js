1a9861fd57d812b777e7f36f19a1de60c7af9702
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-11 06:20:31 +0100

    T

--- 8kyu/Double Char.js
+++ 8kyu/Double Char.js
@@ -4,13 +4,13 @@ const doubleChar = str => [...str].map(x => x + x).join("");
 //const doubleChar = str => Array.from(str).map(x => x + x).join("")
 
 //Solution: 3
-// function doubleChar(str) {
-//   let result = "";
-
-//   for (let i = 0; i < str.length; i++) {
-//     result += str[i].repeat(2);
-//   }
-//   return result;
-// }
+/* function doubleChar(str) {
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    result += str[i].repeat(2);
+  }
+  return result;
+}
+ */
 
 // console.log(doubleChar("abcd")); // --> "aabbccdd"
