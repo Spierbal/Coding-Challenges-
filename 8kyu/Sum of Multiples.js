336d4dcfee53b10d8b0fd02914dacb32c44a0ca8
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-12 06:28:31 +0100

    T

--- 8kyu/Sum of Multiples.js
+++ 8kyu/Sum of Multiples.js
@@ -1,13 +1,25 @@
-function sumMul(n, m) {
+// Solution 2: 5 steps
+const sumMul = (n, m) =>
+  (n * (Math.floor(m / n) * (Math.floor(m / n) + 1))) / 2 || "INVALID";
+
+// Solution 2b:
+/* function sumMul(n, m) {
+  if (n <= 0 || m <= 0) return "INVALID";
+
+  return (n * Math.floor(m / n) * (Math.floor(m / n) + 1)) / 2;
+} */
+
+// Solution 3:
+/* function sumMul(n, m) {
   if (n <= 0 || m <= 0) return "INVALID";
   let total = 0;
   for (let i = n; i < m; i += n) {
     total += i;
   }
   return total;
-}
+} */
 
-console.log(sumMul(2, 9)); // --> 20 (2 + 4 + 6 + 8 = 20)
-console.log(sumMul(4, -7)); //-->"INVALID"
-console.log(sumMul(0, 0)); // --> "INVALID"
-console.log(sumMul(3, 13)); // -->30 (3+6+9+12 = 30)
+// console.log(sumMul(2, 9)); // --> 20 (2 + 4 + 6 + 8 = 20)
+// console.log(sumMul(4, -7)); //-->"INVALID"
+// console.log(sumMul(0, 0)); // --> "INVALID"
+// console.log(sumMul(3, 13)); // -->30 (3+6+9+12 = 30)
