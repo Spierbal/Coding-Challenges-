0d23f4611bbecfb5a38acf97d0f15adee479af51
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-12 03:29:04 +0100

    T

--- 8kyu/String repeat.js
+++ 8kyu/String repeat.js
@@ -1,20 +1,23 @@
 const repeatStr = (n, s) => s.repeat(n);
 
 //Solution 2:
+// const repeatStr = (n, s) => Array(n + 1).join(s);
+
+//Solution 3:
+// const repeatStr = (n, s) => Array(n).fill(s).join("");
+
+// Solution 4:
 //const repeatStr = (n, s) => [...Array(n)].map(i => s).join("");
 
-// 3rd solution:
-//const repeatStr = (n, s) => Array(n).fill(s).join("");
+// for loop solution:
+/* const repeatStr = (n, s) => {
+  let str = "";
+  for (let i = 0; i < n; i++) {
+    str += s;
+  }
+  return str;
+};
+*/
 
-// for Loop solution:
-// function repeatStr (n, s) {
-//   let str = ""
-
-//   for (let i = 0; i< n; i++){
-//     str+=s;
-//   }
-//   return str;
-// }
-
-//console.log(repeatStr(3, "ha")) // --> "hahaha"
-//console.log(repeatStr(6, "I")) // --> // "IIIIII"
+// console.log(repeatStr(3, "ha")); // --> "hahaha"
+// console.log(repeatStr(6, "I")); // --> // "IIIIII"
