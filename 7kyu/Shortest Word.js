235777bb58cb428e9ec9c40c2f9a00ddfbaac252
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-12 02:38:26 +0100

    T

--- 7kyu/Shortest Word.js
+++ 7kyu/Shortest Word.js
@@ -1,15 +1,15 @@
 const findShort = s => Math.min(...s.split(" ").map(word => word.length));
 
 // Solution 2:
-const findShort = s => {
+/* const findShort = s => {
   return s
     .split(" ")
     .reduce((min, word) => Math.min(min, word.length), Infinity);
 };
+ */
 
-// 34 steps
-// Solution 2:
-function findShort(str) {
+// Solution 3:
+/* function findShort(str) {
   const arr = str.split(" ");
   let smallest = arr[0].length;
   for (let i = 1; i < arr.length; i++) {
@@ -18,24 +18,6 @@ function findShort(str) {
     }
   }
   return smallest;
-}
+} */
 
 // console.log(findShort("bitcoin take over the world maybe who knows perhaps")); // --> 3
-
-// 39 steps
-/* function findShort(s){
-    return s
-      .split(' ')
-      .map(a => a.length)
-      .reduce((a, b) => Math.min(a, b))
-  } */
-
-// 43 steps
-// const findShort = s => s.split(" ").sort((a,b) => a.length-b.length)[0].length;
-
-// 23 steps
-// function findShort(s){
-//     return Math.min.apply(null, s.split(' ').map(w => w.length));
-//   }
-
-// 22 steps
