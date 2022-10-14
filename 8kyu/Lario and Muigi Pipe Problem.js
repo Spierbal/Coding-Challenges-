caacf955a8c0fc95f3b8e5df4faf9f52db484d2c
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-14 02:21:08 +0100

    T

--- 8kyu/Lario and Muigi Pipe Problem.js
+++ 8kyu/Lario and Muigi Pipe Problem.js
@@ -1,7 +1,21 @@
-const pipeFix = numbers =>
-  [...Array(numbers[numbers.length - 1] - numbers[0] + 1)].map(
-    (_, i) => numbers[0] + i
-  );
+const pipeFix = nums =>
+  [...Array(nums[nums.length - 1] - nums[0] + 1)].map((_, i) => nums[0] + i);
+
+// Solution 1b:
+// const pipeFix = n =>
+//   Array.from({ length: n[n.length - 1] - n[0] + 1 }, (_, i) => n[0] + i);
+
+// Solution 1c:
+// const pipeFix = n =>
+//   Array.from({ length: n.slice(-1) - n[0] + 1 }, (_, i) => n[0] + i);
+
+/* function pipeFix(nums) {
+  let arr = [];
+  for (let i = nums[0]; i <= nums[nums.length - 1]; i++) {
+    arr.push(i);
+  }
+  return arr;
+} */
 
 // console.log(pipeFix([6, 9])); // --> [ 6, 7, 8, 9 ]
 // console.log(pipeFix([1, 2, 3, 5, 6, 8, 9])); // --> [1, 2, 3, 4, 5, 6, 7, 8, 9]
