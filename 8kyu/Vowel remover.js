e417c3c17adc2aff5f808377a87649a5ed799c65
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-17 08:26:04 +0100

    T

--- 8kyu/Vowel remover.js
+++ 8kyu/Vowel remover.js
@@ -3,5 +3,5 @@ const shortcut = str => str.replace(/[aeiou]/g, "");
 // Solution 2:
 //const shortcut = str => str.split(/[aeiou]/).join("");
 
-// console.log(shortcut("hello")); hll
-// console.log(shortcut("goodbye")); gdby
+// console.log(shortcut("hello")); // --> hll
+// console.log(shortcut("goodbye")); // --> gdby
