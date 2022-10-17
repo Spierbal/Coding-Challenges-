d073fd8f89fed6eb0ec8b038b66d4fcb1e8a41a5
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-17 09:05:23 +0100

    T

---
+++ 8kyu/Reversing Words in a String.js
@@ -0,0 +1,4 @@
+const reverse = str => str.split(" ").reverse().join(" ");
+
+// console.log(reverse('I am an expert at this')) // --> 'this at expert an am I'
+// console.log(reverse('no one cares')) --> // 'cares one no'
