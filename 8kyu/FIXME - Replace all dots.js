3e612adfce29e0756c861c0fe8be78a57dba0f33
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-17 09:52:07 +0100

    T

---
+++ 8kyu/FIXME - Replace all dots.js
@@ -0,0 +1,6 @@
+const replaceDots = str => str.replace(/[.]/g, "-");
+
+// Solution 2:
+//const replaceDots = str => str.replace(/\./g, "-");
+
+// console.log(replaceDots("one.two.three")); // --> "one-two-three"
