604e4fdc079f88cf63e54cb6469b5a630ebe477e
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-17 08:28:47 +0100

    T

--- 7kyu/Vowel Count.js
+++ 7kyu/Vowel Count.js
@@ -1,30 +1,24 @@
 // Solution 1:
-
 const getCount2 = str => (str.match(/[aeiou]/g) || []).length;
 
 // Solution 2:
-
 // const getCount = str =>
 //   str.split("").reduce((total, item) => total + "aeiou".includes(item), 0);
 
 // Solution 3:
-
 // const getCount = str => str.split("").filter(el => "aeiou".includes(el)).length;
 
 // Solution 4:
-
-// function getCount(str) {
-//   let vowelsCount = 0;
-
-//   const vowels = ["a", "e", "i", "o", "u"];
-
-//   for (const char of str) {
-//     if (vowels.includes(char)) {
-//       vowelsCount++;
-//     }
-//   }
-//   return vowelsCount;
-// }
+/* function getCount(str) {
+  let vowelsCount = 0;
+  const vowels = ["a", "e", "i", "o", "u"];
+  for (const char of str) {
+    if (vowels.includes(char)) {
+      vowelsCount++;
+    }
+  }
+  return vowelsCount;
+} */
 
 // console.log(getCount("pyc")); // --> 0
 // console.log(getCount("pear tree")) // --> 4
