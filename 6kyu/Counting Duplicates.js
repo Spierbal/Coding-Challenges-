6f508c59f00e000da986785d6026106ffe62f347
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-18 21:39:10 +0100

    T

---
+++ 6kyu/Counting Duplicates.js
@@ -0,0 +1,33 @@
+function duplicateCount(text) {
+  return (
+    [...text.toLowerCase()]
+      .sort()
+      .join("")
+      .match(/([^])\1+/g) || []
+  ).length;
+}
+
+// Solution 2:
+const duplicateCount = text =>
+  [...new Set([...text.toLowerCase()].filter((e, i) => text.indexOf(e) !== i))]
+    .length;
+
+// Solution 3:
+/* function duplicateCount(text) {
+  const arr = [...text.toLowerCase()];
+
+  let result = [];
+
+  for (let i = 0; i <= arr.length; i++) {
+    if (arr.slice(i + 1).includes(arr[i])) {
+      result.push(arr[i]);
+    }
+  }
+  return [...new Set(result)].length;
+}
+ */
+
+// console.log(duplicateCount("abcde")) // --> 0
+// console.log(duplicateCount("aabbbcd")) // ---> 2
+// console.log(duplicateCount("indivisibility")) // --> 1
+// console.log(duplicateCount("Indivisibility")) // --> 2
