180a34dfb646fe72da98a64fd23f1115fea864d7
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-20 13:40:46 +0100

    T

--- 8kyu/How good are you really.js
+++ 8kyu/How good are you really.js
@@ -8,3 +8,6 @@ const betterThanAverage = (classPoints, yourPoints) =>
 
 //   return yourPoints > average;
 // }
+
+// console.log(betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75)) // --> true
+// console.log(betterThanAverage([41, 75, 72, 56, 80, 82, 81, 33], 50)) // -->  false
--- 8kyu/If you can't sleep, just count sheep.js
+++ 8kyu/If you can't sleep, just count sheep.js
@@ -14,7 +14,5 @@ const countSheep = n =>
   return str;
 } */
 
-// console.log(countSheep(2)) --> 1 sheep...2 sheep...
-
-//"1 sheep...2 sheep...3 sheep...4 sheep...5 sheep...6 sheep...7 sheep...8 sheep..."
-//"1 sheep...2 sheep...3 sheep...4 sheep...5 sheep...6 sheep...7 sheep...8 sheep..."
+// console.log(countSheep(2)) // --> "1 sheep...2 sheep..."
+// console.log(countSheep(5)); // --> "1 sheep...2 sheep...3 sheep...4 sheep...5 sheep..."
