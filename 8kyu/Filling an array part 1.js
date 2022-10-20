9e3a551dd18d62b986d290270336f2bb05633923
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-20 10:54:56 +0100

    T

--- 8kyu/Filling an array part 1.js
+++ 8kyu/Filling an array part 1.js
@@ -6,4 +6,4 @@ const arr = N => Array.from(Array(N), (_, i) => i);
 // Solution 3: same as solution 1 but includes ternary operator
 // const arr = N => (N ? Array.from({ length: N }, (_, i) => i) : []);
 
-// console.log(arr()); // --> [ 0, 1, 2, 3, 4 ]
+// console.log(arr(4)); // --> [ 0, 1, 2, 3, 4 ]
--- 8kyu/Find Multiple of a Number.js
+++ 8kyu/Find Multiple of a Number.js
@@ -1,18 +1,18 @@
-function findMultiples(integer, limit) {
+const findMultiples = (int, limit) =>
+  [...Array(Math.floor(limit / int))].map((_, i) => (i + 1) * int);
+
+// Solution 2:
+// const findMultiples = (int, limit) =>
+//   Array.from({ length: Math.floor(limit / int) }, (_, i) => (i + 1) * int);
+
+// Solution 3:
+/* function findMultiples(integer, limit) {
   const multiples = [];
   for (let i = integer; i <= limit; i += integer) {
     multiples.push(i);
   }
   return multiples;
-}
-
-// Solution 2:
-// const findMultiples = (int, limit) =>
-//   [...Array(Math.floor(limit / int))].map((_, i) => (i + 1) * int);
-
-// Solution 3:
-// const findMultiples = (int, limit) =>
-//   Array.from({ length: Math.floor(limit / int) }, (_, i) => (i + 1) * int);
+} */
 
 // console.log(findMultiples(5, 25)) // --> [5, 10, 15, 20, 25]
 // console.log(findMultiples(5, 7)) // --> [5]
