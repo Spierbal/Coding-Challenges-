d1dcf7828c99c30dae94d77b1b08b80d45c547a2
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-20 06:58:18 +0100

    T

--- 8kyu/Convert a Number to a string!.js
+++ 8kyu/Convert a Number to a string!.js
@@ -1,6 +1,9 @@
-const numberToString = num => num + "";
+const numberToString = num => String(num);
 
-//***past solution:
+// Solution 2
+// const numberToString = num => num + "";
+
+// Solution 3:
 //const numberToString = (num) => num.toString();
 
 //console.log(numberToString(100)); // --> "100"
