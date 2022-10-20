aeb67ebfcab1ce6ef89ce72a1680490fbda67991
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-20 04:37:38 +0100

    T

--- 8kyu/Remove First and Last Character.js
+++ 8kyu/Remove First and Last Character.js
@@ -3,4 +3,4 @@ const removeChar = str => str.slice(1, -1);
 //Solution 2:
 //const removeChar = str => str.split("").slice(1, -1).join("");
 
-//console.log(removeChar("eloquent")) //loquen
+//console.log(removeChar("eloquent")) // --> "loquen"
