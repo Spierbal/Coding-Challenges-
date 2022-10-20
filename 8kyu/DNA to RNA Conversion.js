30ba6f8c7b15b26bc61e8b65ee078e92284a1445
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-20 09:55:47 +0100

    T

--- 8kyu/DNA to RNA Conversion.js
+++ 8kyu/DNA to RNA Conversion.js
@@ -1,5 +1,5 @@
 const DNAtoRNA = dna => dna.replace(/T/g, "U");
 
-// console.log(DNAtoRNA("TTT")) // --> UUU
+// console.log(DNAtoRNA("TTT")) // --> "UUU"
 
-// console.log(DNAtoRNA("GCAT")) // --> GCAU
+// console.log(DNAtoRNA("GCAT")) // --> "GCAU"
--- 8kyu/Do I get bonus.js
+++ 8kyu/Do I get bonus.js
@@ -1,7 +1,7 @@
 const bonusTime = (salary, bonus) => `£${bonus ? salary * 10 : salary}`;
 
 // Shorter Solution 2:
-//const bonusTime = (salary, bonus) => `£${salary * (bonus ? 10 : 1)}`;
+// const bonusTime = (salary, bonus) => `£${salary * (bonus ? 10 : 1)}`;
 
 //Longer Solution 3:
 // function bonusTime(salary, bonus) {
@@ -9,4 +9,5 @@ const bonusTime = (salary, bonus) => `£${bonus ? salary * 10 : salary}`;
 //     return `£${salary}`
 //   }
 
-//console.log(bonusTime(100)); // --> £1000
+//console.log(bonusTime(100, true)); // --> "£1000"
+//console.log(bonusTime(100, false)); // --> "£100"
