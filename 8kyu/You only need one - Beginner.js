8bf9c6b3df52795eb32772a91145b9d4563a02fa
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-21 11:32:06 +0100

    T

--- 8kyu/You only need one - Beginner.js
+++ 8kyu/You only need one - Beginner.js
@@ -1,10 +1,13 @@
 const check = (a, x) => a.includes(x);
 
 // Solution 2:
-//const check = (a, x) => a.indexOf(x) > -1;
+// const check = (a, x) => a.indexOf(x) > -1;
+
 // const check = (a, x) => a.indexOf(x) >= 0;
-//const check = (a, x) => a.indexOf(x) in a
-//const check = (a, x) => a.indexOf(x) !== -1
+
+// const check = (a, x) => a.indexOf(x) in a
+
+// const check = (a, x) => a.indexOf(x) !== -1
 
 // console.log(check([66, 101], 66)); // --> true
 // console.log(check(['t', 'e', 's', 't'], 'z')) --> false
