a9e642d313d7086e47804fba37588cddaf83e9a5
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-21 04:57:54 +0100

    T

--- 8kyu/Simple multiplication.js
+++ 8kyu/Simple multiplication.js
@@ -1,7 +1,7 @@
-const simpleMultiplication = num => (num % 2 === 0 ? num * 8 : num * 9);
-
-// Shorter Solution
 const simpleMultiplication2 = n => n * (n % 2 ? 9 : 8);
+
+// Solution 2:
+// const simpleMultiplication = num => (num % 2 === 0 ? num * 8 : num * 9);
 
 // console.log(simpleMultiplication(3)) // --> 27 (multiply 3 * 9)
 // console.log(simpleMultiplication(2)) // --> 16 (multiply 2 * 8)
