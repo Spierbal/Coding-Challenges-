0211aec5e3932636de951bde2f24ebeb0654fb04
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-22 21:20:43 +0100

    T

--- 7kyu/Reverse words.js
+++ 7kyu/Reverse words.js
@@ -1,12 +1,20 @@
-function reverseWords(str) {
+const reverseWords = str =>
+  str.split` `.map(w => w.split``.reverse().join``).join` `;
+
+//Solution 1b:
+/* const reverseWords = str =>
+  str.split` `.map(w => [...w].reverse().join``).join` `; */
+
+//Solution 1c:
+/* const reverseWords = str =>
+  str.split``.reverse().join``.split` `.reverse().join` `; */
+
+// Solution 1d:
+/*   function reverseWords(str) {
   return str
     .split(" ")
     .map(w => w.split("").reverse().join(""))
     .join(" ");
-}
-
-// Shorter solution
-const reverseWords = str =>
-  str.split` `.map(w => w.split``.reverse().join``).join` `;
+} */
 
 //console.log(reverseWords("This is an example!")); //"sihT si na !elpmaxe"
