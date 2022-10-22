f4d6970aba0b484600ea2390fc04058a29394786
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-22 18:55:00 +0100

    T

--- 7kyu/Reverse a Number.js
+++ 7kyu/Reverse a Number.js
@@ -1,5 +1,5 @@
 const reverseNumber = n =>
-  Math.sign(n) * String(Math.abs(n)).split("").reverse().join("");
+  Math.sign(n) * [...`${Math.abs(n)}`].reverse().join("");
 
 /* const reverseNumber = n =>
   (n >= 0 ? 1 : -1) * String(Math.abs(n)).split("").reverse().join("");
@@ -11,3 +11,7 @@ const reverseNumber = n =>
       : Number(String(n).split("").reverse().join(""));
   }
  */
+
+// console.log(reverseNumber(123)); // --> 321
+// console.log(reverseNumber(-456)); // --> -654
+// console.log(reverseNumber(1000)); // 1
