23202119810bda31a239146e4407d594949290c2
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-23 07:57:21 +0100

    T

--- 7kyu/Array.prototype.reverse().js
+++ 7kyu/Array.prototype.reverse().js
@@ -7,4 +7,12 @@ Array.prototype.reverse = function () {
   return this;
 };
 
+// Solution 2:
+/* Array.prototype.reverse = function () {
+  return (
+    [...this].forEach((_, i, arr) => (this[i] = arr[arr.length - i - 1])), this
+  );
+};
+ */
+
 console.log([1, 2, 3, 4].reverse()); // [4,3,2,1]);
