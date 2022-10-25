83c912d0605d5595fb056d66521db21bd7d711f6
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-25 19:13:05 +0100

    T

---
+++ 8kyu/Exclamation marks series #2 - Remove  all exclamation marks from the end of sentence.js
@@ -0,0 +1,13 @@
+const remove = str => str.replace(/!*$/g, "");
+
+// Solution 2:
+/* function remove(str) {
+    while (str[str.length - 1] === "!") {
+        str = str.slice(0, -1);
+    }
+    return str;
+} */
+
+// console.log(remove("Hi!")); // --> "Hi"
+// console.log(remove("Hi!!!")); // --> "Hi"
+// console.log(remove("!Hi!")); // --> "!Hi"
