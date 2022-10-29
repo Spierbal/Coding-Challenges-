232f13e3df975376dc711a7dc9f20b897bc30c17
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-29 17:35:30 +0100

    T

--- 7kyu/List Filtering.js
+++ 7kyu/List Filtering.js
@@ -20,4 +20,16 @@ const filter_list = l => l.filter(x => typeof x !== "string");
   return res;
 } */
 
+// Solution 3b:
+/* function filter_list(l) {
+  let res = [];
+
+  for (let i = 0; i < l.length; i++) {
+    if (typeof l[i] === "number") {
+      res.push(l[i]);
+    }
+  }
+  return res;
+} */
+
 //console.log(filter_list([1,2,'a','b'])) // --> [1, 2]
