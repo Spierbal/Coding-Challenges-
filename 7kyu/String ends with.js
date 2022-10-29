a312410f7f533b40124ae83cb098f86bd240eec4
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-29 12:12:08 +0100

    T

--- 7kyu/String ends with.js
+++ 7kyu/String ends with.js
@@ -1,9 +1,8 @@
 const solution = (str, ending) => str.endsWith(ending);
 
 // Solution 2:
-/* function solution(str, ending) {
-  return str.slice(str.length - ending.length) === ending;
-} */
+/* const solution = (str, ending) =>
+  str.slice(str.length - ending.length) === ending; */
 
 // console.log(solution("abcde", "cde")); // --> true;
 // console.log(solution('abcde', 'abc')); // --> false;
