d7f670cbc51dd094a8b3131aa06e24a494e24e0e
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-30 17:50:43 +0000

    T

--- 7kyu/Isograms.js
+++ 7kyu/Isograms.js
@@ -1,19 +1,13 @@
-const isIsogram = str => new Set([...str.toLowerCase()]).size === str.length;
+const isIsogram = str => new Set(str.toLowerCase()).size === str.length;
 
-// Solution 1b:
-// const isIsogram = str =>
-//   str.length == new Set(str.toLowerCase().split("")).size;
+// Solution 2:
+// const isIsogram = str => !str.match(/([a-z]).*\1/i);
+// const isIsogram = str => !str.match(/(\w).*\1/i);
 
-// Solution 2
-// function isIsogram(str) {
-//   return (
-//     str
-//       .toLowerCase()
-//       .split("")
-//       .filter((item, pos, arr) => arr.indexOf(item) === pos).length ===
-//     str.length
-//   );
-// }
+// Solution 3
+/* const isIsogram = str =>
+  [...str.toLowerCase()].filter((item, pos, arr) => arr.indexOf(item) === pos)
+    .length === str.length; */
 
 // console.log(isIsogram("thomas")); // true
 // console.log(isIsogram("aba")); // false
