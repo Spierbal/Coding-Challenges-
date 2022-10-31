11a614f7cc364ec7dfa183775166f885910b97b0
Spierbal <abdulkadir.f.osman@gmail.com>
2022-10-31 22:33:10 +0000

    T

---
+++ 7kyu/Exes and Ohs.js
@@ -0,0 +1,24 @@
+const XO = str =>
+  (str.match(/x/gi) || []).length === (str.match(/o/gi) || []).length;
+
+// Solution 1b:
+/* const XO = str => {
+  const x = str.match(/x/gi) || [];
+  const o = str.match(/o/gi) || [];
+  return x.length === o.length;
+}; */
+
+// Solution 2:
+/* 
+function XO(str) {
+  let equal = 0;
+
+  for (let char of str.split("")) {
+    if ("x" === char.toLowerCase()) equal += 1;
+    if ("o" === char.toLowerCase()) equal -= 1;
+  }
+  return equal === 0;
+} */
+
+// console.log(XO("xo")); // --> true;
+// console.log(XO("xoxx")); // --> false;
