a8b2c1d095c01fd8d5c6e1e8b99ce1c038fbda9e
Spierbal <abdulkadir.f.osman@gmail.com>
2022-11-02 21:09:33 +0000

    T

--- 7kyu/Sum of two lowest positive integers.js
+++ 7kyu/Sum of two lowest positive integers.js
@@ -4,4 +4,11 @@ function sumTwoSmallestNumbers(numbers) {
   return arr[0] + arr[1];
 }
 
+// Solution 2:
+const sumTwoSmallestNumbers = numbers =>
+  [...numbers]
+    .sort((x, y) => x - y)
+    .slice(0, 2)
+    .reduce((x, y) => x + y);
+
 // console.log(sumTwoSmallestNumbers([5, 8, 12, 19, 22])); // --> 13
