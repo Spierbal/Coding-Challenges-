b012360b3835c9c12c71609caf6b2b9525382738
Spierbal <abdulkadir.f.osman@gmail.com>
2022-11-02 23:56:36 +0000

    T

---
+++ 8kyu/Tripple Trouble.js
@@ -0,0 +1,22 @@
+const tripleTrouble = (one, two, three) =>
+  [...one].map((_, i) => one[i] + two[i] + three[i]).join("");
+
+// Solution 1b:
+// const tripleTrouble = (one, two, three) =>
+//   [...one].map((a, i) => `${a}${two[i]}${three[i]}`).join("");
+
+// Solution 2:
+// const tripleTrouble = (one, two, three) =>
+//   [...one].reduce((a, _, i) => a + one[i] + two[i] + three[i], "");
+
+// Solution 3:
+/* function tripleTrouble(one, two, three) {
+  let str = "";
+
+  for (let i = 0; i < one.length; i++) {
+    str += one[i] + two[i] + three[i];
+  }
+  return str;
+} */
+// console.log(tripleTrouble("aaa", "bbb", "ccc")); // -->  "abcabcabc"
+// console.log(tripleTrouble("aaaaaa", "bbbbbb", "cccccc")); // --> "abcabcabcabcabcabc"
