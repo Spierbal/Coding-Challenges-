27906f0d3619825f1dba0f852e0a452366274fae
Spierbal <abdulkadir.f.osman@gmail.com>
2022-11-06 03:39:05 +0000

    T

---
+++ 8kyu/No Zeroes for Heroes.js
@@ -0,0 +1,16 @@
+const noBoringZeros = n => +`${n}`.replace(/0+$/, "");
+
+// Solution 1b:
+// const noBoringZeros = n => Number(`${n}`.replace(/0+$/, ""));
+
+// Solution 1c:
+// const noBoringZeros = n => Number(String(n).replace(/0+$/g, ""));
+
+// Solution 2:
+// const noBoringZeros = n => !n || n % 10 ? n : noBoringZeros(n / 10)
+
+// Solution 2b:
+// const noBoringZeros = n => (n % 10 || n === 0 ? n : noBoringZeros(n / 10));
+
+// console.log(noBoringZeros(14500)); // 145
+// console.log(noBoringZeros(-500)); // -5
