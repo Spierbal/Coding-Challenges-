6f0023d157530d4c04bd9f45ee09e641093920ba
Spierbal <abdulkadir.f.osman@gmail.com>
2022-11-06 20:11:58 +0000

    T

---
+++ 5kyu/Rot13.js
@@ -0,0 +1,23 @@
+function rot13(message) {
+  const alpha =
+    "abcdefghijklmnopqrstuvwxyzabcdefghijklmABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLM";
+  return message.replace(/[a-z]/gi, char => alpha[alpha.indexOf(char) + 13]);
+}
+
+// Solution 1b:
+// function rot13(message) {
+//   let alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+//   let rot13Alpha = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+//   return message.replace(/[a-z]/gi, char => rot13Alpha[alpha.indexOf(char)]);
+// }
+
+// Solution 2:
+/* const rot13 = message =>
+  message.replace(/[a-z]/gi, char =>
+    String.fromCharCode(
+      char.charCodeAt(0) + (char.toLowerCase() <= "m" ? 13 : -13)
+    )
+  ); */
+
+// console.log(rot13("test")); // --> grfg
