bc2c9804243768deaf6e7edebc524568c80f0a36
Spierbal <abdulkadir.f.osman@gmail.com>
2022-11-06 03:49:53 +0000

    T

--- 8kyu/No Zeroes for Heroes.js
+++ 8kyu/No Zeroes for Heroes.js
@@ -12,5 +12,13 @@ const noBoringZeros = n => +`${n}`.replace(/0+$/, "");
 // Solution 2b:
 // const noBoringZeros = n => (n % 10 || n === 0 ? n : noBoringZeros(n / 10));
 
+// Solution 3:
+/* function noBoringZeros(n) {
+  while (n % 10 === 0 && n !== 0) {
+    n /= 10;
+  }
+  return n;
+} */
+
 // console.log(noBoringZeros(14500)); // 145
 // console.log(noBoringZeros(-500)); // -5
