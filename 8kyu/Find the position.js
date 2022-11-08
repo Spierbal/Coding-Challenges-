51a0d5bf4fa961e1de3992298082e2d6453c5049
Spierbal <abdulkadir.f.osman@gmail.com>
2022-11-08 01:28:19 +0000

    T

---
+++ 8kyu/Find the position.js
@@ -0,0 +1,22 @@
+function position(letter) {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  return `Position of alphabet: ${alphabet.indexOf(letter) + 1}`;
+}
+
+// Solution 2:
+// const position = letter => `Position of alphabet: ${letter.charCodeAt() - 96}`
+
+// Solution 3:
+/* function position(letter) {
+  let alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+  for (let i = 0; i < alphabet.length; i++) {
+    if (alphabet[i] === letter) {
+      return `Position of alphabet: ${i + 1}`;
+    }
+  }
+} */
+
+// console.log(position("a")) // --> "Position of alphabet: 1
+// console.log(position("z")) // --> "Position of alphabet: 26
+// console.log(position("e")) // --> "Position of alphabet: 1"
