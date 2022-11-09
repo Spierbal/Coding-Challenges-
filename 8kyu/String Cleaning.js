7fac347f30558b962209b96dac391d36d02652b2
Spierbal <abdulkadir.f.osman@gmail.com>
2022-11-09 15:28:51 +0000

    T

---
+++ 8kyu/String Cleaning.js
@@ -0,0 +1,5 @@
+const stringClean = s => s.replace(/\d/g, "");
+
+// console.log(stringClean("E3at m2e2!!")); // ---> Eat me!!
+// console.log(stringClean("123344")); // ---> // ""
+// console.log(stringClean("This looks5 grea8t")); // --> This looks great
