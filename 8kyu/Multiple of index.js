4d802372bf7725c3819eabfbe4343d6984c6f9d9
Spierbal <abdulkadir.f.osman@gmail.com>
2022-11-09 16:55:33 +0000

    T

---
+++ 8kyu/Multiple of index.js
@@ -0,0 +1,5 @@
+const multipleOfIndex = arr => arr.filter((n, i) => n % i === 0);
+
+// console.log(multipleOfIndex([22, -6, 32, 82, 9, 25])); // ---> [-6, 32, 25]);
+// console.log(multipleOfIndex([68, -1, 1, -7, 10, 10])); // --> [-1, 10];
+// console.log(multipleOfIndex([11, -11])) // ---> [-11]);
