d97bef5facc88dc1978362abd632e972b590e8a7
Spierbal <abdulkadir.f.osman@gmail.com>
2022-11-10 23:12:01 +0000

    T

--- 8kyu/L1 - Bartender, drinks!.js
+++ 8kyu/L1 - Bartender, drinks!.js
@@ -29,11 +29,29 @@ function getDrinkByProfession(param) {
 
 const getDrinkByProfession = param => drinks[param.toLowerCase()] || "Beer"; */
 
-console.log(getDrinkByProfession("jabrOni")); // --> "Patron Tequila"
-console.log(getDrinkByProfession("scHOOl counselor")); // "Anything with Alcohol"
-console.log(getDrinkByProfession("prOgramMer")); // --> "Hipster Craft Beer"
-console.log(getDrinkByProfession("bike ganG member")); // --> "Moonshine"
-console.log(getDrinkByProfession("poLiTiCian")); // --> "Your tax dollars"
-console.log(getDrinkByProfession("rapper")); // --> "Cristal", "'Rapper'
-console.log(getDrinkByProfession("pundit")); // --> "Beer"
-console.log(getDrinkByProfession("Pug")); // --> "Beer"
+// Solution 3:
+/* function getDrinkByProfession(param) {
+  let res = param.toLowerCase();
+  return res === "jabroni"
+    ? "Patron Tequila"
+    : res === "school counselor"
+    ? "Anything with Alcohol"
+    : res === "programmer"
+    ? "Hipster Craft Beer"
+    : res === "bike gang member"
+    ? "Moonshine"
+    : res === "politician"
+    ? "Your tax dollars"
+    : res === "rapper"
+    ? "Cristal"
+    : "Beer";
+} */
+
+// console.log(getDrinkByProfession("jabrOni")); // --> "Patron Tequila"
+// console.log(getDrinkByProfession("scHOOl counselor")); // "Anything with Alcohol"
+// console.log(getDrinkByProfession("prOgramMer")); // --> "Hipster Craft Beer"
+// console.log(getDrinkByProfession("bike ganG member")); // --> "Moonshine"
+// console.log(getDrinkByProfession("poLiTiCian")); // --> "Your tax dollars"
+// console.log(getDrinkByProfession("rapper")); // --> "Cristal", "'Rapper'
+// console.log(getDrinkByProfession("pundit")); // --> "Beer"
+// console.log(getDrinkByProfession("Pug")); // --> "Beer"
