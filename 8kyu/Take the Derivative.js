b6e6acf83c7e44c43e9d5f441473431c9bc68cea
Spierbal <abdulkadir.f.osman@gmail.com>
2022-11-10 15:59:38 +0000

    T

--- 8kyu/Take the Derivative.js
+++ 8kyu/Take the Derivative.js
@@ -2,5 +2,5 @@ const derive = (coefficient, exponent) => {
   return `${coefficient * exponent}x^${exponent - 1}`;
 };
 
-console.log(derive(7, 8)); // --> "56x^7"
-console.log(derive(5, 9)); // --> "45^8"
+// console.log(derive(7, 8)); // --> "56x^7"
+// console.log(derive(5, 9)); // --> "45^8"
