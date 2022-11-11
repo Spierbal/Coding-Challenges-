5e930212cd1b9a4ac284de9a30035e46ca01adb4
Spierbal <abdulkadir.f.osman@gmail.com>
2022-11-11 03:07:25 +0000

    T

--- 8kyu/Grasshopper - Array Mean.js
+++ 8kyu/Grasshopper - Array Mean.js
@@ -1,5 +1,4 @@
 const findAverage = nums => nums.reduce((a, b) => a + b, 0) / nums.length;
 
-console.log(findAverage([1])); // --> 1
-console.log(findAverage([1, 3, 5, 7])); // --> 4;
-console.log(findAverage([1, 1, 1, 2])); // --> 4;
+// console.log(findAverage([1])); // --> 1
+// console.log(findAverage([1, 3, 5, 7])); // --> 4;
