54f904ad243ca653688344dc50e04b7c05ff26a3
Spierbal <abdulkadir.f.osman@gmail.com>
2022-11-11 00:00:56 +0000

    T

---
+++ 8kyu/Alan Partridge II - Apple Turnover.js
@@ -0,0 +1,15 @@
+function apple(x) {
+  if (x ** 2 > 1000) return "It's hotter than the sun!!";
+
+  return "Help yourself to a honeycomb Yorkie for the glovebox.";
+}
+
+// Solution 2:
+/* function apple(x) {
+  return Math.pow(x, 2) > 1000
+    ? "It's hotter than the sun!!"
+    : "Help yourself to a honeycomb Yorkie for the glovebox.";
+} */
+
+// console.log(apple("50")); // --> 'It's hotter than the sun!!'
+// console.log(apple(4)); // --> 'Help yourself to a honeycomb Yorkie for the glovebox.
