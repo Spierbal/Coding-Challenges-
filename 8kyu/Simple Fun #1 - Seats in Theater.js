42a8885390162513977caf0fd978cfc7446c979e
Spierbal <abdulkadir.f.osman@gmail.com>
2022-11-15 14:35:44 +0000

    T

--- 8kyu/Simple Fun #1 - Seats in Theater.js
+++ 8kyu/Simple Fun #1 - Seats in Theater.js
@@ -7,4 +7,4 @@ function seatsInTheater(nCols, nRows, col, row) {
 //   return (nCols - (col - 1)) * (nRows - row);
 // }
 
-console.log(seatsInTheater(16, 11, 5, 3)); // --> 96;
+// console.log(seatsInTheater(16, 11, 5, 3)); // --> 96;
