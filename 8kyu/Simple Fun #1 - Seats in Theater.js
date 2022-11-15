64ccb844884486c266481c8401360030b7e9b07e
Spierbal <abdulkadir.f.osman@gmail.com>
2022-11-15 14:34:49 +0000

    T

---
+++ 8kyu/Simple Fun #1 - Seats in Theater.js
@@ -0,0 +1,10 @@
+function seatsInTheater(nCols, nRows, col, row) {
+  return (nCols - col + 1) * (nRows - row);
+}
+
+// Solution 1b:
+// function seatsInTheater(nCols, nRows, col, row) {
+//   return (nCols - (col - 1)) * (nRows - row);
+// }
+
+console.log(seatsInTheater(16, 11, 5, 3)); // --> 96;
