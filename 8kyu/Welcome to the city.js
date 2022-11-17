adefa3242a46ddcbf4e8d36ea12bddfda9ffd094
Spierbal <abdulkadir.f.osman@gmail.com>
2022-11-17 01:37:21 +0000

    T

--- 8kyu/Welcome to the city.js
+++ 8kyu/Welcome to the city.js
@@ -1,4 +1,8 @@
 const sayHello = (name, city, state) =>
   `Hello, ${name.join(" ")}! Welcome to ${city}, ${state}!`;
 
+// Solution 2:
+/* const sayHello = (name, city, state) =>
+  "Hello, " + name.join(" ") + "! Welcome to " + city + ", " + state + "!"; */
+
 // console.log(sayHello(["John", "Smith"], "Phoenix", "Arizona")); // --> 'Hello, John Smith! Welcome to Phoenix, Arizona!'
