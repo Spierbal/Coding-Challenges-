f8c17ab2c842a96b1d979104459d3b70534500dc
Spierbal <abdulkadir.f.osman@gmail.com>
2022-11-17 22:18:35 +0000

    T

--- 8kyu/Find the position.js
+++ 8kyu/Find the position.js
@@ -17,6 +17,6 @@ function position(letter) {
   }
 } */
 
-// console.log(position("a")) // --> "Position of alphabet: 1
-// console.log(position("z")) // --> "Position of alphabet: 26
-// console.log(position("e")) // --> "Position of alphabet: 1"
+// console.log(position("a")) // --> "Position of alphabet: 1"
+// console.log(position("z")) // --> "Position of alphabet: 26"
+// console.log(position("e")) // --> "Position of alphabet: 5"
