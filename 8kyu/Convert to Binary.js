7c2c1f2e5ff0b50c424366ace447ac6e308ebaa6
Spierbal <abdulkadir.f.osman@gmail.com>
2022-11-17 22:41:08 +0000

    T

--- 8kyu/Convert to Binary.js
+++ 8kyu/Convert to Binary.js
@@ -1,9 +1,9 @@
 const toBinary = n => Number(n.toString(2));
 
 // Solution 1b:
-// const toBinary = n => parseInt(n.toString(2));
+// const toBinary = n => +n.toString(2);
 
 // Solution 1c:
-// const toBinary = n => +n.toString(2);
+// const toBinary = n => parseInt(n.toString(2));
 
 // console.log(toBinary(25)); // --> 11001
