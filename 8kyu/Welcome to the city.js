aed50f66af209190758a4baf825736b983dfebf4
Spierbal <abdulkadir.f.osman@gmail.com>
2022-11-17 01:28:41 +0000

    T

---
+++ 8kyu/Welcome to the city.js
@@ -0,0 +1,4 @@
+const sayHello = (name, city, state) =>
+  `Hello, ${name.join(" ")}! Welcome to ${city}, ${state}!`;
+
+// console.log(sayHello(["John", "Smith"], "Phoenix", "Arizona")); // --> 'Hello, John Smith! Welcome to Phoenix, Arizona!'
