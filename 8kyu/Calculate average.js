677bbd7ff26bcb329da280effb2ba45967725efc
Spierbal <abdulkadir.f.osman@gmail.com>
2022-11-18 06:51:55 +0000

    T

--- 8kyu/Calculate average.js
+++ 8kyu/Calculate average.js
@@ -2,3 +2,4 @@ const find_average = arr =>
   arr.length ? arr.reduce((a, b) => a + b, 0) / arr.length : 0;
 
 // console.log(find_average([10, 20])) // --> 15
+// console.log(find_average([])) // --> 0
