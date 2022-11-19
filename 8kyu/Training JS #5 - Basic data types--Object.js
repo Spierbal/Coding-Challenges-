81b5fcbb70b585f4a4c83da77324fb0fe41e9035
Spierbal <abdulkadir.f.osman@gmail.com>
2022-11-19 23:41:45 +0000

    T

--- 8kyu/Training JS #5 - Basic data types--Object.js
+++ 8kyu/Training JS #5 - Basic data types--Object.js
@@ -1,7 +1,12 @@
 const animal = obj => `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
 
-// Solution 1b:
+// Solution 1b: destructuring
 /* const animal = ({ name, legs, color }) =>
   `This ${color} ${name} has ${legs} legs.`; */
+
+// Solution 1c:
+/* function animal(obj) {
+  return "This " + obj.color + " " + obj.name + " has " + obj.legs + " legs.";
+} */
 
 // console.log(animal({ name: "dog", legs: 4, color: "white" })); // "This white dog has 4 legs."
