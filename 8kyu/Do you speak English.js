831b416746bc456f4b056fa0c2d9ba6075d2a8a6
Spierbal <abdulkadir.f.osman@gmail.com>
2022-11-20 23:31:32 +0000

    T

--- 8kyu/Do you speak English.js
+++ 8kyu/Do you speak English.js
@@ -3,5 +3,8 @@ const spEng = sentence => sentence.toLowerCase().includes("english");
 // Solution 2:
 // const spEng = sentence => /english/i.test(sentence);
 
+// Solution 3:
+// const spEng = sentence => sentence.toLowerCase().indexOf("english") !== -1;
+
 // console.log(spEng("english")); // true
 // console.log(spEng("egnlish")); // false
