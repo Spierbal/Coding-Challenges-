2d811390d045ef064c993fd0dc5258f042bf3b0e
Spierbal <abdulkadir.f.osman@gmail.com>
2022-11-24 16:53:08 +0000

    T

---
+++ 8kyu/Training JS #8 - Conditional statement - switch.js
@@ -0,0 +1,32 @@
+function howManydays(month) {
+  switch (month) {
+    case 1:
+      return 31;
+    case 2:
+      return 28;
+    case 3:
+      return 31;
+    case 4:
+      return 30;
+    case 5:
+      return 31;
+    case 6:
+      return 30;
+    case 7:
+      return 31;
+    case 8:
+      return 31;
+    case 9:
+      return 30;
+    case 10:
+      return 31;
+    case 11:
+      return 30;
+    case 12:
+      return 31;
+  }
+}
+
+// console.log(howManydays(2)); // 28
+// console.log(howManydays(4)); // 30
+// console.log(howManydays(3)); // 31
