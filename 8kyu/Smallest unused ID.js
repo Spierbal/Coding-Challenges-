465d2af64e857c92c695d8ae1300f1ae3fb9dfd8
Spierbal <abdulkadir.f.osman@gmail.com>
2022-11-25 19:09:14 +0000

    T

--- 8kyu/Smallest unused ID.js
+++ 8kyu/Smallest unused ID.js
@@ -27,4 +27,5 @@ function nextId(ids) {
   }
 } */
 
-// console.log(nextId([0, 1, 2, 3, 5]));
+// console.log(nextId([0, 1, 2, 3, 5])); //--> 4
+// console.log(nextId([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])); //--> 11
