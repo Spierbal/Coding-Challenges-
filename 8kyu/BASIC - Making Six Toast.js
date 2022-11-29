a4ae939927471f98d702481707dc1680f9caa46d
Spierbal <abdulkadir.f.osman@gmail.com>
2022-11-29 01:30:20 +0000

    T

---
+++ 8kyu/BASIC - Making Six Toast.js
@@ -0,0 +1,8 @@
+const sixToast = num => Math.abs(num - 6);
+
+// Solution 2:
+// const sixToast = num => (num < 6 ? num - 6 : 6 - num);
+
+// console.log(sixToast(6)); // 0
+// console.log(sixToast(17)); // 11
+// console.log(sixToast(3)); // 3
