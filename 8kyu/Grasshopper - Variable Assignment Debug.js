ed421c270fe916f044e0471bdf872677905515a2
Spierbal <abdulkadir.f.osman@gmail.com>
2022-12-01 00:35:36 +0000

    T

---
+++ 8kyu/Grasshopper - Variable Assignment Debug.js
@@ -0,0 +1,10 @@
+const a = "dev";
+const b = "Lab";
+const name = a + b;
+
+// Solution 1b:
+/* const a = `dev`;
+const b = `Lab`;
+const name = `${a}${b}`; */
+
+// console.log(name) // --> "devLab"
