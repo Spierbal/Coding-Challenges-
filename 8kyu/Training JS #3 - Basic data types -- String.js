93de10f8e1ce18af104bdd2ed5e9338585c8d0c2
Spierbal <abdulkadir.f.osman@gmail.com>
2022-12-02 09:34:44 +0000

    T

--- 8kyu/Training JS #3 - Basic data types -- String.js
+++ 8kyu/Training JS #3 - Basic data types -- String.js
@@ -23,15 +23,3 @@ const answer3 = () => "yes";
 console.log(Bee()); // --> "Dad"
 console.log(banana()); // --> "banana"
  */
-let a1 = "A",
-  a2 = "a",
-  b1 = "B",
-  b2 = "b",
-  c1 = "C",
-  c2 = "c",
-  d1 = "D",
-  d2 = "d",
-  e1 = "E",
-  e2 = "e",
-  n1 = "N",
-  n2 = "n";
