96728dbbcb23b0e58f994e28fc86ff8311089cd2
Spierbal <abdulkadir.f.osman@gmail.com>
2022-12-03 16:23:34 +0000

    T

---
+++ 8kyu/Contamination #1 - String-.js
@@ -0,0 +1,22 @@
+const contamination = (text, char) => char.repeat(text.length);
+
+// Solution 2
+/* function contamination(text, char) {
+  let result = "";
+
+  for (const el of text) {
+    result += char;
+  }
+  return result;
+} */
+
+// Solution 3
+// const contamination = (text, char) => text.replace(/[\w\D]/g, char);
+
+// Solution 4
+// const contamination = (text, char) => text.split``.map(e => e = char).join``
+
+/* console.log(contamination("abc", "z"));
+console.log(contamination("", "z")); // ""
+console.log(contamination("abc", "")); // ""
+ */
