bcdcda840d91f7eb6eca748c366495c41cffd48b
Spierbal <abdulkadir.f.osman@gmail.com>
2022-12-08 06:57:53 +0000

    T

---
+++ 7kyu/Friend or Foe.js
@@ -0,0 +1,4 @@
+const friend = friends => friends.filter(friend => friend.length === 4);
+
+// console.log(friend(["Ryan", "Kieran", "Mark"])); // --> ["Ryan", "Mark"]
+// console.log(friend(["Ryan", "Jimmy", "123", "4", "Cool Man"])); // --> ["Ryan"]
