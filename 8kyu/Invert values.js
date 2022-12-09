ebd2ad8b57e0291987f5e2384e239be0a7a01f72
Spierbal <abdulkadir.f.osman@gmail.com>
2022-12-09 18:35:37 +0000

    T

--- 8kyu/Invert values.js
+++ 8kyu/Invert values.js
@@ -1,13 +1,13 @@
 const invert = array => array.map(n => -n);
 
 // 2nd Solution:
-function invert(array) {
+/* function invert(array) {
   let arr = [];
   for (let i = 0; i < array.length; i++) {
     arr.push(-array[i]);
   }
   return arr;
-}
+} */
 
 // console.log(invert([1, 2, 3, 4, 5])); // --> [-1, -2, -3, -4, -5]
 // console.log(invert([-1, -2, -3, -4, -5])); // --> [1, 2, 3, 4, 5]
