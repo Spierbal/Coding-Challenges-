841b6f980980c3ed3ab4cfb5bacb8aceec197d9e
Spierbal <abdulkadir.f.osman@gmail.com>
2022-12-09 17:55:55 +0000

    T

--- 8kyu/Counting sheep.js
+++ 8kyu/Counting sheep.js
@@ -5,13 +5,13 @@ const countSheeps = arrayOfSheep =>
 //const countSheeps = arrayOfSheep => arrayOfSheep.filter(Boolean).length;
 
 // Solution 2:
-function countSheeps(arrayOfSheep) {
+/* function countSheeps(arrayOfSheep) {
   let count = 0;
 
   for (i = 0; i < arrayOfSheep.length; i++) {
     if (arrayOfSheep[i]) count++;
   }
   return count;
-}
+} */
 
 // console.log(countSheeps([true, true, true, false, false, false, true, true])); // --> 5
