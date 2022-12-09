1220e1108d526cf7f384102969a561c705fc8ba8
Spierbal <abdulkadir.f.osman@gmail.com>
2022-12-09 18:22:29 +0000

    T

--- 8kyu/Exclamation marks series #6 - Remove n exclamation marks in the sentence from left to right.js
+++ 8kyu/Exclamation marks series #6 - Remove n exclamation marks in the sentence from left to right.js
@@ -7,15 +7,25 @@ function remove(s, n) {
   return res;
 }
 
+// Solution 1b: While Loop
+/* function remove(s,n){
+    while(n) {
+      s = s.replace("!", "");
+      n--;
+    }
+    return s;
+  }
+ */
+
 // Solution 1b: For Of Loop
-function remove(s, n) {
+/* function remove(s, n) {
   let res = s;
 
   for (let item of s) {
     res = res.replace("!", "");
   }
   return res;
-}
+} */
 
 // Solution 2: recursion
 // const remove = (s, n) => (n ? remove(s.replace("!", ""), n - 1) : s);
