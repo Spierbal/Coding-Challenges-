6e3885e3489dabe62ddd7c2fc42a55a0c7f0ccd2
Spierbal <abdulkadir.f.osman@gmail.com>
2022-12-09 11:42:48 +0000

    T

---
+++ 7kyu/Categorize New Number.js
@@ -0,0 +1,13 @@
+const openOrSenior = data =>
+  data.map(([age, handicap]) => (age > 54 && handicap > 7 ? "Senior" : "Open"));
+
+// console.log(
+//   openOrSenior([
+//     [45, 12],
+//     [55, 21],
+//     [19, -2],
+//     [104, 20],
+//   ])
+// );
+
+// Output : ["Open", "Senior", "Open", "Senior"]
