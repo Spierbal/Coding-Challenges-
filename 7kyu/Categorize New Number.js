aab8373084b651d94b33cd4cacd1feabd8323c4e
Spierbal <abdulkadir.f.osman@gmail.com>
2022-12-09 15:52:31 +0000

    T

--- 7kyu/Categorize New Number.js
+++ 7kyu/Categorize New Number.js
@@ -16,17 +16,16 @@ const openOrSenior = data =>
 } */
 
 // Solution 3: For of loop
-// function openOrSenior(data) {
-//   const res = [];
+/* function openOrSenior(data) {
+  const res = [];
 
-//   for (const member of data) {
-//     const [age, handicap] = member;
-//     if (age > 54 && handicap > 7) res.push("Senior");
-//     else res.push("Open");
-//   }
-//   return res;
-// }
+  for (const member of data) {
+    const [age, handicap] = member;
+    if (age > 54 && handicap > 7) res.push("Senior");
+    else res.push("Open");
+  }
+  return res;
+} */
 
-/* console.log(openOrSenior([[45, 12],[55, 21],[19, -2],[104, 20]]));  */
-
+// console.log(openOrSenior([[45, 12],[55, 21],[19, -2],[104, 20]]));
 // Output : ["Open", "Senior", "Open", "Senior"]
