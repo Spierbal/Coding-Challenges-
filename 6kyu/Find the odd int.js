df04f3dabee2d1dfdf9ceaa5ca4ead2c57c513f6
Spierbal <abdulkadir.f.osman@gmail.com>
2022-12-09 17:23:04 +0000

    T

--- 6kyu/Find the odd int.js
+++ 6kyu/Find the odd int.js
@@ -2,23 +2,22 @@ const findOdd = arr =>
   arr.find(item => arr.filter(el => el === item).length % 2);
 
 // Solution 2:
-/* function findOdd(A) {
+function findOdd(A) {
   for (let i = 0; i < A.length; i++) {
     if (A.filter(item => item === A[i]).length % 2 !== 0) {
       return A[i];
     }
   }
-} */
+}
 
-// Solution 2b:
-/* function findOdd(A) {
+// Solution 2b: For Of Loop
+function findOdd(A) {
   for (let int of A) {
     if (A.filter(el => el === int).length % 2) {
       return int;
     }
   }
 }
- */
 
 // console.log(findOdd([1,1,2])); // --> return 2, because it occurs 1 time(odd)
 // console.log(findOdd([0, 1, 0, 1, 0])); // --> returns 0, because it occurs 3 times(which is odd)
