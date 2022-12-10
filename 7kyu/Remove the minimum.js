22a1043ed80a1c654419c176f9e40f66a1dcb7f1
Spierbal <abdulkadir.f.osman@gmail.com>
2022-12-10 19:48:10 +0000

    T

--- 7kyu/Remove the minimum.js
+++ 7kyu/Remove the minimum.js
@@ -6,7 +6,12 @@ function removeSmallest(numbers) {
   return result;
 }
 
-/* console.log(removeSmallest([1, 2, 3, 4, 5])); // --> [2, 3, 4, 5]
+// Solution 2:
+/* function removeSmallest2(numbers) {
+  let minIdx = numbers.indexOf(Math.min(...numbers));
+  return numbers.filter((_, i) => i !== minIdx);
+} */
+
+console.log(removeSmallest([1, 2, 3, 4, 5])); // --> [2, 3, 4, 5]
 console.log(removeSmallest([2, 2, 1, 2, 1])); // --> [2, 2, 2, 1]
 console.log(removeSmallest([])); // --> [];
- */
