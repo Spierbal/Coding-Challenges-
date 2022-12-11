45b5ccb6a20ca4870cc35e1d5fb069c26d14c2fc
Spierbal <abdulkadir.f.osman@gmail.com>
2022-12-11 21:48:31 +0000

    T

---
+++ 7kyu/Sum of odd numbers.js
@@ -0,0 +1,9 @@
+const rowSumOddNumbers = n => Math.pow(n, 3);
+
+// Solution 1b:
+// const rowSumOddNumbers = (n) => n * n * n;
+
+/* console.log(rowSumOddNumbers(1)); // --> 1;
+console.log(rowSumOddNumbers(2)); // --> 8;
+console.log(rowSumOddNumbers(42)); // --> 74088
+ */
