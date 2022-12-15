ee836756bf2e4a0042940aa4e84c06da577d33f6
Spierbal <abdulkadir.f.osman@gmail.com>
2022-12-15 16:19:46 +0000

    T

---
+++ 8kyu/Sleigh Authentication.js
@@ -0,0 +1,10 @@
+function Sleigh() {}
+
+Sleigh.prototype.authenticate = function (name, password) {
+  return name === "Santa Claus" && password === "Ho Ho Ho!";
+};
+
+const sleigh = new Sleigh();
+
+// console.log(sleigh.authenticate("Santa Claus", "Ho Ho Ho!")); // --> true
+// console.log(sleigh.authenticate("Santa", "Ho Ho Ho!")); // --> false
