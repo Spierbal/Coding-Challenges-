165dd965103401f393012d8e0e3b67aa7fc2b849
Spierbal <abdulkadir.f.osman@gmail.com>
2022-12-27 17:58:29 +0000

    T

---
+++ 7kyu/Ones and Zeros.js
@@ -0,0 +1,12 @@
+const binaryArrayToNumber = arr => parseInt(arr.join(""), 2);
+
+// Solution 1b:
+/* const binaryArrayToNumber = arr => {
+  const bin = arr.join("");
+  return parseInt(bin, 2);
+}; */
+
+/* console.log(binaryArrayToNumber([0, 0, 0, 1])); // 1
+console.log(binaryArrayToNumber([0, 0, 1, 0])); // 2);
+console.log(binaryArrayToNumber([1, 1, 1, 1])); //  15);
+console.log(binaryArrayToNumber([0, 1, 1, 0])); // 6); */
