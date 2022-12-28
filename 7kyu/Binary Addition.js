b17f2864491f06ff635a0c3728cd325a602bb825
Spierbal <abdulkadir.f.osman@gmail.com>
2022-12-28 21:37:23 +0000

    T

---
+++ 7kyu/Binary Addition.js
@@ -0,0 +1,10 @@
+const addBinary = (a, b) => (a + b).toString(2);
+
+// Solution 1b:
+/* function addBinary(a, b) {
+  let bin = a + b;
+  return bin.toString(2);
+} */
+
+/* console.log(addBinary(1, 2)); // "11"
+console.log(addBinary(5, 9)); // "1110" */
