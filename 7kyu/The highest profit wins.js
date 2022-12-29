47f5717a1d47a7d41ebff11c1c0889539a66e4ce
Spierbal <abdulkadir.f.osman@gmail.com>
2022-12-29 23:23:12 +0000

    T

---
+++ 7kyu/The highest profit wins.js
@@ -0,0 +1,12 @@
+const minMax = arr => [Math.max(...arr), Math.min(...arr)];
+
+// Solution 1b:
+/* function minMax(arr) {
+  const max = Math.max(...arr);
+  const min = Math.min(...arr);
+
+  return [min, max];
+} */
+
+/* console.log(minMax([1, 2, 3, 4, 5])); //[1, 5]],
+console.log(minMax([2334454, 5])); //[5, 2334454], */
