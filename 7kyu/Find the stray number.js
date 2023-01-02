e38a842514ad9f570ba03369d3ab308ae263dddf
Spierbal <abdulkadir.f.osman@gmail.com>
2023-01-02 06:31:03 +0000

    T

--- 7kyu/Find the stray number.js
+++ 7kyu/Find the stray number.js
@@ -1,13 +1,16 @@
-const stray = n => n.find(num => n.indexOf(num) === n.lastIndexOf(num));
-
-// Solution 1b:
-/* const stray2 = n => n.find(num => n.filter(v => num === v).length === 1); */
-
-/* function stray(numbers) {
+function stray(numbers) {
   const arr = [...numbers].sort((a, b) => a - b);
   return arr[0] === arr[1] ? arr[arr.length - 1] : arr[0];
 }
- */
+
+// Solution 2
+/* const stray = n => n.find(el => n.indexOf(el) === n.lastIndexOf(el)); */
+
+// Solution 2b:
+/* const stray = n => n.filter(el => n.indexOf(el) === n.lastIndexOf(el))[0]; */
+
+// Solution 3:
+/* const stray = numbers => numbers.reduce((pre, val) => pre ^ val); */
 
 /* console.log(stray([1, 1, 2])); // 2
 console.log(stray([17, 17, 3, 17, 17, 17, 17])); // 3 */
