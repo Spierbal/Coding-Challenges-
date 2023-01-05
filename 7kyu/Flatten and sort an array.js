42c6220eddd38dc347bee615a02810f8ffb0c36c
Spierbal <abdulkadir.f.osman@gmail.com>
2023-01-05 09:36:37 +0000

    T

--- 7kyu/Flatten and sort an array.js
+++ 7kyu/Flatten and sort an array.js
@@ -9,9 +9,31 @@ array.length ? array.reduce((a, b) => a.concat(b)).sort((a, b) => a - b) : []; *
 array.reduce((a, b) => a.concat(b)).sort((a, b) => a - b); */
 
 // Solution 3:
-// const flattenAndSort = array => array.flat().sort((a, b) => a - b);
+// const flattenAndSort = array => array.flat().sort((a, b) => a - b);#
+
+// Solution 4:
+/* function flattenAndSort(array) {
+  let arr = [];
+
+  for (let i = 0; i < array.length; i++) {
+    arr.push(...array[i]);
+  }
+  return arr.sort((a, b) => a - b);
+}
+ */
+
+// Solution 4b:
+/* function flattenAndSort(array) {
+  let arr = [];
+
+  for (const char of array) {
+    arr.push(...char);
+  }
+  return arr.sort((a, b) => a - b);
+} */
 
 /* console.log(flattenAndSort([[1, 3, 5], [100], [2, 4, 6]])); //[1, 2, 3, 4, 5, 6, 100]
 console.log(flattenAndSort([])); // [];
 console.log(flattenAndSort([[], []])); // [];
-console.log(flattenAndSort([[], [1]])); // [1]; */
+console.log(flattenAndSort([[], [1]])); // [1];
+ */
