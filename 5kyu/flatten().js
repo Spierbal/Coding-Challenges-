df31bd0fad9795fab5897d6fafdb3cc88128ca18
Spierbal <abdulkadir.f.osman@gmail.com>
2023-01-06 18:48:15 +0000

    T

---
+++ 5kyu/flatten().js
@@ -0,0 +1,44 @@
+const flatten = (...arr) =>
+  arr.reduce((a, b) => a.concat(Array.isArray(b) ? flatten(...b) : b), []);
+
+// Solution 2:
+/* function flatten(...array) {
+  let arr = [];
+
+  for (let char of array) {
+    if (Array.isArray(char)) {
+      arr = arr.concat(flatten(...char));
+    } else {
+      arr.push(char);
+    }
+  }
+  return arr;
+} */
+
+// Solution 2b:
+/* function flatten(...array) {
+  let arr = [];
+
+  for (let char of array) {
+    Array.isArray(char) ? (arr = arr.concat(flatten(...char))) : arr.push(char);
+  }
+  return arr;
+} */
+
+// Solution 2c
+/* function flatten(...array) {
+  let res = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      res = res.concat(flatten(...array[i]));
+    } else {
+      res.push(array[i]);
+    }
+  }
+  return res;
+} */
+
+/* console.log(flatten(1, [2, 3], 4, 5, [6, [7, [8]]])); // [1,2,3,4,5,6,7,8]
+console.log(flatten([[[[[1]]]]], ["a", [3, ["b"]]], null, 5, "c")); // [1,'a',3,'b',null,5,'c']
+ */
