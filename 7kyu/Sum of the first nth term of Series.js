0d97c173b757344b167d1cd9c9884d501e081eb9
Spierbal <abdulkadir.f.osman@gmail.com>
2023-01-08 10:16:31 +0000

    T

--- 7kyu/Sum of the first nth term of Series.js
+++ 7kyu/Sum of the first nth term of Series.js
@@ -1,14 +1,15 @@
 function SeriesSum(n) {
   return [...Array(n)]
     .map((_, i) => 1 / (1 + 3 * i))
-    .reduce((a, b) => a + b)
+    .reduce((a, b) => a + b, 0)
     .toFixed(2);
 }
 
+// Solution 2:
 /* function SeriesSum(n) {
-  let sum = 1;
+  let sum = 0;
 
-  for (let i = 1; i < n; i++) {
+  for (let i = 0; i < n; i++) {
     sum += 1 / (1 + 3 * i);
   }
   return sum.toFixed(2);
@@ -17,5 +18,4 @@ function SeriesSum(n) {
 /* console.log(SeriesSum(1)); // '1.00'
 console.log(SeriesSum(2)); // '1.25'
 console.log(SeriesSum(3)); // '1.39'
-console.log(SeriesSum(4)); // '1.49'
- */
+console.log(SeriesSum(4)); // '1.49' */
