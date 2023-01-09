b20ccabe6ca03d365279260ce57dbe872a51e966
Spierbal <abdulkadir.f.osman@gmail.com>
2023-01-09 09:54:11 +0000

    T

---
+++ 7kyu/Find the middle element.js
@@ -0,0 +1,17 @@
+function gimme(triplet) {
+  const index = [...triplet].sort((a, b) => a - b);
+
+  return triplet.indexOf(index[1]);
+}
+
+// Solution 1b:
+// const gimme = arr => arr.indexOf([...arr].sort((a, b) => a - b)[1]);
+
+/* console.log(gimme([2, 3, 1])); //  0)
+console.log(gimme([5, 10, 14])); // --> 1;
+console.log(gimme([2.1, 3.2, 1.4])); // --> 0;
+console.log(gimme([5.9, 10.4, 14.2])); // --> 1;
+console.log(gimme([-2, -3, -1])); // --> 0;
+console.log(gimme([-5, -10, -14])); // --> 1;
+console.log(gimme([-2, -3.2, 1])); // --> 0;
+console.log(gimme([-5.2, -10.6, 14])); // --> 0; */
