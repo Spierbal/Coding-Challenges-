c7ec190aa0d167a127c66d6935cee474ffff83e0
Spierbal <abdulkadir.f.osman@gmail.com>
2023-01-11 18:06:36 +0000

    T

---
+++ 7kyu/Get number from string.js
@@ -0,0 +1,9 @@
+const getNumberFromString = s => Number(s.match(/\d/g, "").join(""));
+
+// Solution 1b:
+// const getNumberFromString = s => Number(s.replace(/\D/g, ""));
+
+/* console.log(getNumberFromString("1")); // 1
+console.log(getNumberFromString("123")); // 123
+console.log(getNumberFromString("this is number: 7")); // 7
+console.log(getNumberFromString("hell5o wor6ld")); // 7 */
