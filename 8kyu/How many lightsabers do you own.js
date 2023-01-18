e699b8a4ed5de34dd4896cc124a70307a414be0c
Spierbal <abdulkadir.f.osman@gmail.com>
2023-01-18 00:00:03 +0000

    T

--- 8kyu/How many lightsabers do you own.js
+++ 8kyu/How many lightsabers do you own.js
@@ -1,4 +1,7 @@
-function howManyLightsabersDoYouOwn(name) {}
+function howManyLightsabersDoYouOwn(name) {
+  if (name === "Zach") return 18;
+  else return 0;
+}
 console.log(howManyLightsabersDoYouOwn()); // 0
 console.log(howManyLightsabersDoYouOwn("Adam")); // 0
 console.log(howManyLightsabersDoYouOwn("Zach")); // 0
