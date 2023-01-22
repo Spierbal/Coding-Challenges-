4d8a834cc6f62d586b44086505a8996bc62053d7
Spierbal <abdulkadir.f.osman@gmail.com>
2023-01-22 19:32:27 +0000

    T

--- 8kyu/Check same case.js
+++ 8kyu/Check same case.js
@@ -12,7 +12,7 @@ function sameCase(a, b) {
   else return 0;
 }
 
-console.log(sameCase("C", "B")); // 1
+/* console.log(sameCase("C", "B")); // 1
 console.log(sameCase("b", "a")); // 1
 console.log(sameCase("d", "d")); // 1
 console.log(sameCase("A", "s")); // 0
@@ -20,3 +20,4 @@ console.log(sameCase("c", "B")); // 0
 console.log(sameCase("b", "Z")); // 0
 console.log(sameCase("\t", "Z")); // -1
 console.log(sameCase("H", ":")); // -1
+ */
