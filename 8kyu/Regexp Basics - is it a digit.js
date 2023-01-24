a6661ca3070d5e6e36a6f3d78667e173f84086c1
Spierbal <abdulkadir.f.osman@gmail.com>
2023-01-24 00:20:25 +0000

    T

--- 8kyu/Regexp Basics - is it a digit.js
+++ 8kyu/Regexp Basics - is it a digit.js
@@ -1,3 +1,9 @@
 String.prototype.digit = function () {
   return /^\d$/g.test(this);
 };
+
+/* console.log("".digit()); // false
+console.log("7".digit()); // true
+console.log("a5".digit()); // false
+console.log("14".digit()); // false
+ */
