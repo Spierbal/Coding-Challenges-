5314996598e694f319b684cb8baa7effaa580763
Spierbal <abdulkadir.f.osman@gmail.com>
2023-01-24 22:53:25 +0000

    T

--- 8kyu/Area of a Square.js
+++ 8kyu/Area of a Square.js
@@ -1,10 +1,7 @@
-const squareArea = A => Number((((2 * A) / 3.1416) ** 2).toFixed(2));
+const squareArea = A => Number(Math.pow((2 * A) / 3.1416, 2).toFixed(2));
 
-// Solution 2:
-/* function squareArea(A) {
-  return Number(Math.pow(2 * A / 3.1416, 2).toFixed(2));
-} */
+// const squareArea = A => Number((((2 * A) / 3.1416) ** 2).toFixed(2));
 
-// console.log(squareArea(2)); // 1.62
-// console.log(squareArea(14.05)); // 80
-// console.log(squareArea(20.493049806276403)); // 170.2
+console.log(squareArea(2)); // 1.62
+console.log(squareArea(14.05)); // 80
+console.log(squareArea(20.493049806276403)); // 170.2
