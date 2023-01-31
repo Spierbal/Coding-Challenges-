40898e91694f6887641ff801704d16abfebe2731
Spierbal <abdulkadir.f.osman@gmail.com>
2023-01-31 14:00:28 +0000

    T

---
+++ 8kyu/Type of sum.js
@@ -0,0 +1,12 @@
+const typeOfSum = (a, b) => typeof (a + b);
+
+console.log(typeOfSum(12, 1)); // "number"
+console.log(typeOfSum("d", 1)); // "string"
+console.log(typeOfSum(1, "a")); // "string"
+console.log(typeOfSum(true, 1)); // "string"
+console.log(typeOfSum(null, 1)); // "number"
+console.log(typeOfSum("s", null)); // "string"
+console.log(typeOfSum(null, undefined)); // "number"
+console.log(typeOfSum(undefined, "re")); // "string"
+console.log(typeOfSum(undefined, true)); // "number"
+console.log(typeOfSum(null, false)); // "number"
