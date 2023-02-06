62ef8a513855ab14b9b84c4759bae49ca655ae6b
Spierbal <abdulkadir.f.osman@gmail.com>
2023-02-06 00:07:38 +0000

    T

--- 8kyu/Is it a number.js
+++ 8kyu/Is it a number.js
@@ -1,4 +1,8 @@
 const isDigit = s => Number(s) === parseFloat(s);
 
+// Solution 1b
+// const isDigit = s => s == parseFloat(s);
+
 /* console.log(isDigit("s3")); // false
-console.log(isDigit("-234.4")); // true */
+console.log(isDigit("-234.4")); // true 
+ */
