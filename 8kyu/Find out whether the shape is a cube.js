3991360c0fa92b38b977c7665b8df0f74f66e6fc
Spierbal <abdulkadir.f.osman@gmail.com>
2023-02-21 13:47:34 +0000

    T

---
+++ 8kyu/Find out whether the shape is a cube.js
@@ -0,0 +1,24 @@
+function cubeChecker(volume, side) {
+  return volume === side * side * side && volume > 0;
+}
+
+// Solution 1b:
+// function cubeChecker(volume, side) {
+//   return volume === side ** 3 && volume > 0
+// }
+
+// Solution 1c:
+// function cubeChecker(volume, side) {
+// return volume === Math.pow(side, 3) && volume > 0;
+// }
+
+// Solution 2:
+// const cubeChecker = (volume, side) =>
+//   volume > 0 ? volume === side * side * side : false;
+
+console.log(cubeChecker(56.3, 1)); // false;
+console.log(cubeChecker(-1, 2)); // false
+console.log(cubeChecker(8, 3)); // false
+console.log(cubeChecker(8, 2)); // true
+console.log(cubeChecker(-8, -2)); // false
+console.log(cubeChecker(125, -5)); // false);
