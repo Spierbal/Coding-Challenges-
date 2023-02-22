59a35ba775f437e39f2c8635cfd9dc3b3db43dcd
Spierbal <abdulkadir.f.osman@gmail.com>
2023-02-22 23:28:41 +0000

    T

--- 8kyu/Find out whether the shape is a cube.js
+++ 8kyu/Find out whether the shape is a cube.js
@@ -16,9 +16,9 @@ function cubeChecker(volume, side) {
 // const cubeChecker = (volume, side) =>
 //   volume > 0 ? volume === side * side * side : false;
 
-console.log(cubeChecker(56.3, 1)); // false;
-console.log(cubeChecker(-1, 2)); // false
-console.log(cubeChecker(8, 3)); // false
-console.log(cubeChecker(8, 2)); // true
-console.log(cubeChecker(-8, -2)); // false
-console.log(cubeChecker(125, -5)); // false);
+// console.log(cubeChecker(56.3, 1)); // false;
+// console.log(cubeChecker(-1, 2)); // false
+// console.log(cubeChecker(8, 3)); // false
+// console.log(cubeChecker(8, 2)); // true
+// console.log(cubeChecker(-8, -2)); // false
+// console.log(cubeChecker(125, -5)); // false);
