88522fb46ddae13c134e624408af2c991eda0172
Spierbal <abdulkadir.f.osman@gmail.com>
2023-02-26 00:05:38 +0000

    T

--- 8kyu/Leonardo Dicaprio and Oscars.js
+++ 8kyu/Leonardo Dicaprio and Oscars.js
@@ -5,6 +5,15 @@ function leo(oscar) {
   if (oscar > 88) return "Leo got one already!";
 }
 
+// solution 1b:
+// function leo(oscar) {
+//   if (oscar === 88) return "Leo finally won the oscar! Leo is happy";
+//   if (oscar === 86) return "Not even for Wolf of wallstreet?!";
+//   if (oscar < 88) return "When will you give Leo an Oscar?";
+
+//   return "Leo got one already!";
+// }
+
 // console.log(leo(89)); // "Leo got one already!"
 // console.log(leo(88)); // "Leo finally won the oscar! Leo is happy"
 // console.log(leo(87)); // "When will you give Leo an Oscar?"
