1fc7e0fff38a65305a4724ee8c06a0bf2f4d0689
Spierbal <abdulkadir.f.osman@gmail.com>
2023-02-28 18:13:25 +0000

    T

--- 8kyu/Find Nearest square number.js
+++ 8kyu/Find Nearest square number.js
@@ -1,13 +1,13 @@
-// const nearestSq = n => Math.round(n ** 0.5) ** 2;
+const nearestSq = n => Math.round(n ** 0.5) ** 2;
 
 //Solution 1b:
-const nearestSq = n => Math.round(Math.sqrt(n)) ** 2;
+// const nearestSq = n => Math.round(Math.sqrt(n)) ** 2;
 
 // Solution 1c:
 //const nearestSq = n => Math.pow(Math.round(Math.sqrt(n)), 2);
 
-console.log(nearestSq(1)); //  1 "nearestSq(1) = 1"
-console.log(nearestSq(2)); //  1 "nearestSq(1) = 1"
-console.log(nearestSq(10)); //  9 "nearestSq(1) = 9"
-console.log(nearestSq(111)); //  121 "nearestSq(1) = 121"
-console.log(nearestSq(9999)); //  10000 "nearestSq(1) = 10000
+// console.log(nearestSq(1)); //  1 "nearestSq(1) = 1"
+// console.log(nearestSq(2)); //  1 "nearestSq(1) = 1"
+// console.log(nearestSq(10)); //  9 "nearestSq(1) = 9"
+// console.log(nearestSq(111)); //  121 "nearestSq(1) = 121"
+// console.log(nearestSq(9999)); //  10000 "nearestSq(1) = 10000
