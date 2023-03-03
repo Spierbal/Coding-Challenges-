a5fd7646d92b496f9d7feb0a4136af2ee6861ccc
Spierbal <abdulkadir.f.osman@gmail.com>
2023-03-03 02:37:41 +0000

    T

--- 7kyu/Simple Fun #176 - Reverse Letter.js
+++ 7kyu/Simple Fun #176 - Reverse Letter.js
@@ -1,15 +1,12 @@
-const reverseLetter = s => [...s.replace(/[^a-zA-Z]/g, "")].reverse().join("");
+// const reverseLetter = s => [...s.replace(/[^a-zA-Z]/g, "")].reverse().join("");
 
 //Solution 1b:
-//const reverseLetter = s => [...s.replace(/[^a-z]/gi, "")].reverse().join("");
+// const reverseLetter = s => [...s.replace(/[^a-z]/g, "")].reverse().join("");
 
 // Solution 1c:
-/* function reverseLetter(str) {
-  return str.match(/[a-z]/gi).reverse().join("");
-} */
+// const reverseLetter = str => str.match(/[a-z]/gi).reverse().join("");
 
-/* console.log(reverseLetter("krishan")); //"nahsirk"
-console.log(reverseLetter("ultr53o?n")); //"nahsirk"
-console.log(reverseLetter("ab23c")); //"nahsirk"
+console.log(reverseLetter("krishan")); //"nahsirk"
+console.log(reverseLetter("ultr53o?n")); //"nortlu"
+console.log(reverseLetter("ab23c")); //"cba"
 console.log(reverseLetter("krish21an")); //"nahsirk"
- */
--- 8kyu/Regex count lowercase letters.js
+++ 8kyu/Regex count lowercase letters.js
@@ -1,8 +1,11 @@
 const lowercaseCount = str => str.replace(/[^a-z]/g, "").length;
 
-// console.log(lowercaseCount("abc")); //  3
-// console.log(lowercaseCount("abcABC123")); //  3
-// console.log(lowercaseCount("abcABC123!@€£#$%^&*()_-+=}{[]|':;?/>.<,~")); //  3
-// console.log(lowercaseCount("")); //  0
-// console.log(lowercaseCount("ABC123!@€£#$%^&*()_-+=}{[]|':;?/>.<,~")); //  0
-// console.log(lowercaseCount("abcdefghijklmnopqrstuvwxyz")); //  26
+// Solution 2
+// const lowercaseCount = str => (str.match(/[a-z]/g) || []).length;
+
+/* console.log(lowercaseCount("abc")); //  3
+console.log(lowercaseCount("abcABC123")); //  3
+console.log(lowercaseCount("abcABC123!@€£#$%^&*()_-+=}{[]|':;?/>.<,~")); //  3
+console.log(lowercaseCount("")); //  0
+console.log(lowercaseCount("ABC123!@€£#$%^&*()_-+=}{[]|':;?/>.<,~")); //  0
+console.log(lowercaseCount("abcdefghijklmnopqrstuvwxyz")); //  26 */
