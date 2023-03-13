8721a0ea223246965e8ef683e2f8a61affdd89c1
Spierbal <abdulkadir.f.osman@gmail.com>
2023-03-13 23:49:22 +0000

    T

--- 6kyu/Your order,please.js
+++ 6kyu/Your order,please.js
@@ -1,17 +1,17 @@
-// const order = words => {
-//   return words
-//     .split(" ")
-//     .sort((a, b) => a.match(/\d/) - b.match(/\d/))
-//     .join(" ");
-// };
-
 const order = words => {
   return words
     .split(" ")
-    .reduce((acc, el) => ((acc[el.match(/\d/) - 1] = el), acc), [])
+    .sort((a, b) => a.match(/\d/) - b.match(/\d/))
     .join(" ");
 };
 
-console.log(order("is2 Thi1s T4est 3a")); //, "Thi1s is2 3a T4est")
-console.log(order("4of Fo1r pe6ople g3ood th5e the2")); //, "Fo1r the2 g3ood 4of th5e pe6ople")
-console.log(order("")); //, "", "empty input should return empty string" )) //
+// const order = words => {
+//   return words
+//     .split(" ")
+//     .reduce((acc, el) => ((acc[el.match(/\d/) - 1] = el), acc), [])
+//     .join(" ");
+// };
+
+// console.log(order("is2 Thi1s T4est 3a")); //, "Thi1s is2 3a T4est")
+// console.log(order("4of Fo1r pe6ople g3ood th5e the2")); //, "Fo1r the2 g3ood 4of th5e pe6ople")
+// console.log(order("")); //, "", "empty input should return empty string" ))
