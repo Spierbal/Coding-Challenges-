bde9bfd1fb08d72761077058b8951feb884d7db0
Spierbal <abdulkadir.f.osman@gmail.com>
2023-03-16 02:41:28 +0000

    T

--- 6kyu/Split Strings.js
+++ 6kyu/Split Strings.js
@@ -1,5 +1,8 @@
 const solution = str => `${str}_`.match(/\w\w/g) || [];
 
+// Solution 1b:
+// const solution = str => (s + "").match(/.{2}/g) || [];
+
 // console.log(solution("abcdef")); // ["ab", "cd", "ef"]
 // console.log(solution("abcdefg")); // ["ab", "cd", "ef", "g_"]
 // console.log(solution("")); // []
