0a4030d629ac1f3a00c7ba3ed690148cf3269998
Spierbal <abdulkadir.f.osman@gmail.com>
2023-03-22 23:58:21 +0000

    T

---
+++ 7kyu/Divide and Conquer.js
@@ -0,0 +1,12 @@
+function divCon(x) {
+  const num = x.filter(el => typeof el === "number").reduce((a, b) => a + b, 0);
+  const str = x
+    .filter(el => typeof el === "string")
+    .reduce((a, b) => +a + +b, 0);
+
+  return num - Number(str);
+}
+
+console.log(divCon([9, 3, "7", "3"])); // 2
+console.log(divCon(["5", "0", 9, 3, 2, 1, "9", 6, 7])); // 1
+console.log(divCon(["3", 6, 6, 0, "5", 8, 5, "6", 2, "0"])); // 1
