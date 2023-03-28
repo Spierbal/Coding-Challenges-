f9da99e26a5ce8d59634adbf0a9fac75a3448b26
Spierbal <abdulkadir.f.osman@gmail.com>
2023-03-28 16:41:49 +0100

    T

---
+++ 4kyu/Adding Big Numbers.js
@@ -0,0 +1,20 @@
+function add(a, b) {
+  let res = "",
+    carry = 0;
+  a = a.split("");
+  b = b.split("");
+  while (a.length || b.length || carry) {
+    carry += ~~a.pop() + ~~b.pop();
+    res = (carry % 10) + res;
+    carry = carry > 9;
+  }
+  return res;
+}
+
+// console.log(add("1", "1")); // "2");
+// console.log(add("123", "456")); // "579");
+// console.log(add("888", "222")); // "1110");
+// console.log(add("1372", "69")); // "1441");
+// console.log(add("12", "456")); // "468");
+// console.log(add("101", "100")); // "201");
+// console.log(add("63829983432984289347293874", "90938498237058927340892374089")); // "91002328220491911630239667963")
