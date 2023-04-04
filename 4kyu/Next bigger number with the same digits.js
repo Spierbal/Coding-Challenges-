7adaede72dde5931a8f662a29066ef71d3a93f59
Spierbal <abdulkadir.f.osman@gmail.com>
2023-04-04 02:29:05 +0100

    T

--- 4kyu/Next bigger number with the same digits.js
+++ 4kyu/Next bigger number with the same digits.js
@@ -1,14 +1,26 @@
 function nextBigger(n) {
-  const sortedNum = n => [...String(n)].sort((a, b) => b - a);
-  const max = Number(sortedNum(n).join(""));
-  for (let i = n + 1; i <= max; i++) {
-    if (max === Number(sortedNum(i).join(""))) return i;
-  }
-  return -1;
+  const arr = [...String(n)].map(Number);
+
+  let i = arr.length - 2;
+  while (i >= 0 && arr[i] >= arr[i + 1]) i--;
+
+  if (i < 0) return -1;
+
+  let j = arr.length - 1;
+  while (j > i && arr[j] <= arr[i]) j--;
+
+  [arr[i], arr[j]] = [arr[j], arr[i]];
+  let res = [...arr]
+    .slice(0, i + 1)
+    .concat(arr.slice(i + 1).reverse())
+    .join("");
+
+  return Number(res);
 }
 
-console.log(nextBigger(12)); // 531
-console.log(nextBigger(513)); // 531
-console.log(nextBigger(2017)); // 2071
-console.log(nextBigger(414)); // 441
-console.log(nextBigger(144)); //414
+// console.log(nextBigger(12)); // 531
+// console.log(nextBigger(513)); // 531
+// console.log(nextBigger(2017)); // 2071
+// console.log(nextBigger(414)); // 441
+// console.log(nextBigger(144)); //414
+// console.log(nextBigger(298765422000)); //400022256789
