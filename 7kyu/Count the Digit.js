0355fdd5eae54c45e6d4fc7b4cce6466335734b4
Spierbal <abdulkadir.f.osman@gmail.com>
2023-04-10 00:16:22 +0100

    T

--- 7kyu/Count the Digit.js
+++ 7kyu/Count the Digit.js
@@ -1,11 +1,26 @@
 function nbDig(n, d) {
-  let res = 0;
-  for (let i = 0; i <= n; i++) {
-    let arr = [...String(i * i)];
-    arr.forEach(e => (e === d + "" ? res++ : null));
-  }
-  return res;
+  let res = "";
+  for (let i = 0; i <= n; i++) res += i * i;
+
+  return res.split(d).length - 1;
 }
+
+// Solution 2:
+// const nbDig = (n, d) =>
+//   [...Array(n + 1)]
+//     .map((_, i) => i * i)
+//     .join("")
+//     .split(d).length - 1;
+
+// Solution 3
+// function nbDig(n, d) {
+//   let res = 0;
+//   for (let i = 0; i <= n; i++) {
+//     let arr = [...String(i * i)];
+//     arr.forEach(e => (e === d + "" ? res++ : null));
+//   }
+//   return res;
+// }
 
 // console.log(nbDig(5750, 0)); // 4700, "n = 5750, d = 0"
 // console.log(nbDig(11011, 2)); //  9481, "n = 11011, d = 2"
