78e2d3b417a72125ca5eab7251e4c8da9afbe318
Spierbal <abdulkadir.f.osman@gmail.com>
2023-04-11 06:23:42 +0100

    T

--- 7kyu/Check the exam.js
+++ 7kyu/Check the exam.js
@@ -1,16 +1,29 @@
-function checkExam(array1, array2) {
-  const score = responses.reduce((total, response, index) => {
-    if (!response) return total;
-    if (response === answers[index]) return total + 4;
-    if (response !== answers[index]) return total - 1;
-
-    return total;
+const checkExam = (correctAnswers, studentAnswers) => {
+  const score = studentAnswers.reduce((total, answer, i) => {
+    if (!answer) return total;
+    if (answer === correctAnswers[i]) return total + 4;
+    if (answer !== correctAnswers[i]) return total - 1;
   }, 0);
 
-  return score > 0 ? score : 0;
-}
+  return score < 0 ? 0 : score;
+};
 
-console.log(checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"])); // 6
-console.log(checkExam(["a", "a", "c", "b"], ["a", "a", "b", ""])); // 7
-console.log(checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"])); // 16
-console.log(checkExam(["b", "c", "b", "a"], ["", "a", "a", "c"])); // 0
+// Solution 2:
+/* function checkExam(correctAnswers, studentAnswers) {
+  let score = 0;
+  for (let i = 0; i < correctAnswers.length; i++) {
+    if (correctAnswers[i] === studentAnswers[i]) {
+      score += 4;
+    } else if (studentAnswers[i] === "") {
+      score += 0;
+    } else {
+      score -= 1;
+    }
+  }
+  return score < 0 ? 0 : score;
+} */
+
+// console.log(checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"])); // 6
+// console.log(checkExam(["a", "a", "c", "b"], ["a", "a", "b", ""])); // 7
+// console.log(checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"])); // 16
+// console.log(checkExam(["b", "c", "b", "a"], ["", "a", "a", "c"])); // 0
--- 8kyu/How old will I be in 2099.js
+++ 8kyu/How old will I be in 2099.js
@@ -1,20 +1,16 @@
 function calculateAge(birthYear, currentYear) {
   const age = currentYear - birthYear;
-
-  if (currentYear === birthYear) {
-    return "You were born this very year!";
-  } else if (currentYear === birthYear + 1) {
-    return `You are ${age} year old.`;
-  } else if (currentYear > birthYear) {
-    return `You are ${age} years old.`;
-  } else if (currentYear + 1 === birthYear) {
-    return `You will be born in ${-age} year.`;
-  } else if (currentYear < birthYear) {
-    return `You will be born in ${-age} years.`;
+  switch (true) {
+    case age === 0:
+      return "You were born this very year!";
+    case age === 1:
+      return "You are 1 year old.";
+    case age > 0:
+      return `You are ${age} years old.`;
+    case age < 0:
+      return `You will be born in ${-age} years.`;
   }
 }
-
-//console.log(calculateAge(1971, 2022)) --> You are 51 years old
 
 //** solution 2: *//
 
@@ -33,3 +29,15 @@ function calculateAge(birthYear, currentYear) {
 //       return "You were born this very year!";
 //     }
 //    }
+
+// console.log(calculateAge(3400, 3400)); // You were born this very year!
+// console.log(calculateAge(2021, 2022)); // You are 1 year old.
+// console.log(calculateAge(1971, 2022)); // You are 51 years old.
+// console.log(calculateAge(2012, 2016)); // You are 4 years old.
+// console.log(calculateAge(1989, 2016)); // You are 27 years old.
+// console.log(calculateAge(2000, 2090)); // You are 90 years old.
+// console.log(calculateAge(900, 2900)); // You are 2000 years old.
+// console.log(calculateAge(2000, 1990)); // You will be born in 10 years.
+// console.log(calculateAge(2010, 1990)); // You will be born in 20 years.
+// console.log(calculateAge(2010, 1500)); // You will be born in 510 years.
+// console.log(calculateAge(2000, 1999)); // You will be born in 1 year.
