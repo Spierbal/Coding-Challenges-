6b8ade865bff7c6a9a987d4946d0a7aece7bbacd
Spierbal <abdulkadir.f.osman@gmail.com>
2023-04-12 03:47:15 +0100

    T

--- 7kyu/Check the exam.js
+++ 7kyu/Check the exam.js
@@ -1,14 +1,3 @@
-// const checkExam = (correctAnswers, studentAnswers) => {
-//   const score = studentAnswers.reduce((total, answer, i) => {
-//     if (!answer) return total;
-//     if (answer === correctAnswers[i]) return total + 4;
-//     if (answer !== correctAnswers[i]) return total - 1;
-//   }, 0);
-
-//   return score < 0 ? 0 : score;
-// };
-
-// Solution 2:
 function checkExam(correctAnswers, studentAnswers) {
   let score = 0;
   for (let i = 0; i < correctAnswers.length; i++) {
@@ -19,7 +8,17 @@ function checkExam(correctAnswers, studentAnswers) {
   return score < 0 ? 0 : score;
 }
 
-console.log(checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"])); // 6
-console.log(checkExam(["a", "a", "c", "b"], ["a", "a", "b", ""])); // 7
-console.log(checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"])); // 16
-console.log(checkExam(["b", "c", "b", "a"], ["", "a", "a", "c"])); // 0
+// const checkExam = (correctAnswers, studentAnswers) => {
+//   const score = studentAnswers.reduce((total, answer, i) => {
+//     if (!answer) return total;
+//     if (answer === correctAnswers[i]) return total + 4;
+//     if (answer !== correctAnswers[i]) return total - 1;
+//   }, 0);
+
+//   return score < 0 ? 0 : score;
+// };
+
+// console.log(checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"])); // 6
+// console.log(checkExam(["a", "a", "c", "b"], ["a", "a", "b", ""])); // 7
+// console.log(checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"])); // 16
+// console.log(checkExam(["b", "c", "b", "a"], ["", "a", "a", "c"])); // 0
