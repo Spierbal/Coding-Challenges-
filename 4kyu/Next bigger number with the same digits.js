f7b0f75856ad544f5a64b8c73c41affb3838bba4
Spierbal <abdulkadir.f.osman@gmail.com>
2023-04-14 03:26:15 +0100

    T

--- 4kyu/Next bigger number with the same digits.js
+++ 4kyu/Next bigger number with the same digits.js
@@ -1,21 +1,52 @@
 function nextBigger(n) {
-  const arr = [...String(n)].map(Number);
+  // Convert the number to an array of digits
+  const digits = [...String(n)].map(Number);
 
-  let i = arr.length - 2;
-  while (i >= 0 && arr[i] >= arr[i + 1]) i--;
+  // Find the first digit from the right that is smaller than the digit after it
+  let i = digits.length - 2;
+  while (i >= 0 && digits[i] >= digits[i + 1]) i--;
+
+  // If no such digit is found, return -1
+  if (i < 0) return -1;
+
+  // Find the smallest digit from the right that is bigger than the digit at i
+  let j = digits.length - 1;
+  while (j > i && digits[j] <= digits[i]) j--;
+
+  // Swap the two digits
+  [digits[i], digits[j]] = [digits[j], digits[i]];
+
+  // Reverse the digits after i
+  for (let k = i + 1, l = digits.length - 1; k < l; k++, l--) {
+    [digits[k], digits[l]] = [digits[l], digits[k]];
+  }
+
+  // Convert the array of digits back to a number and return it
+  return Number(digits.join(""));
+}
+
+// Solution 1b: replace for loop with while loop
+function nextBigger(n) {
+  const digits = [...String(n)].map(Number);
+
+  let i = digits.length - 2;
+  while (i >= 0 && digits[i] >= digits[i + 1]) i--;
 
   if (i < 0) return -1;
 
-  let j = arr.length - 1;
-  while (j > i && arr[j] <= arr[i]) j--;
+  let j = digits.length - 1;
+  while (digits[j] <= digits[i]) j--;
 
-  [arr[i], arr[j]] = [arr[j], arr[i]];
-  let res = [...arr]
-    .slice(0, i + 1)
-    .concat(arr.slice(i + 1).reverse())
-    .join("");
+  [digits[i], digits[j]] = [digits[j], digits[i]];
 
-  return Number(res);
+  let k = i + 1;
+  let l = digits.length - 1;
+  while (k < l) {
+    [digits[k], digits[l]] = [digits[l], digits[k]];
+    k++;
+    l--;
+  }
+  return Number(digits.join(""));
 }
 
 // console.log(nextBigger(12)); // 21
