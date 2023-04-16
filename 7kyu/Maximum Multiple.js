4c2b8af712f8e14177dacb6a15ae29fcbe57c824
Spierbal <abdulkadir.f.osman@gmail.com>
2023-04-16 07:31:02 +0100

    T

--- 7kyu/Maximum Multiple.js
+++ 7kyu/Maximum Multiple.js
@@ -1,25 +1,31 @@
-// // Solution 1:
+// Solution 1:
 const maxMultiple = (divisor, bound) => Math.floor(bound / divisor) * divisor;
 
-// // Solution 2:
-// const maxMultiple = (divisor, bound) => bound - (bound % divisor);
+// Solution 2:
+/* const maxMultiple = (divisor, bound) => bound - (bound % divisor); */
+
+// Solution 2b:
+/* const maxMultiple = (divisor, bound) => {
+  let remainder = bound % divisor; // 7 % 2 = 1
+  return bound - remainder; // 7 - 1 = 6
+}; */
 
 // Solution 3:
-// const maxMultiple = (divisor, bound) => {
-//   while (bound % divisor !== 0) {
-//     bound--;
-//   }
-//   return bound;
-// };
+/* const maxMultiple = (divisor, bound) => {
+  while (bound % divisor !== 0) {
+    bound--;
+  }
+  return bound;
+}; */
 
 // Solution 4:
-// const maxMultiple = (divisor, bound) => {
-//   let max = 0;
-//   for (let i = 1; i <= bound; i++) {
-//     if (i % divisor === 0) max = i;
-//   }
-//   return max; // return 0
-// };
+/* const maxMultiple = (divisor, bound) => {
+  let max = 0;
+  for (let i = 1; i <= bound; i++) {
+    if (i % divisor === 0) max = i;
+  }
+  return max;
+}; */
 
 // console.log(maxMultiple(2, 7)); // 6
 // console.log(maxMultiple(3, 10)); // 9
---
+++ 7kyu/Summing a number's digits.js
@@ -0,0 +1,24 @@
+// const sumDigits = number =>
+//   [...String(Math.abs(number))].reduce((a, b) => a + +b, 0);
+
+// Solution 1b
+// const sumDigits = num =>
+//   String(Math.abs(num))
+//     .split("")
+//     .reduce((acc, b) => acc + +b, 0);
+
+// Solution 1c:
+// const sumDigits = num =>
+//   Array.from(String(Math.abs(num)), Number).reduce((a, b) => a + b, 0);
+
+function sumDigits(number) {
+  const absoluteNumber = Math.abs(number); // -32 => 32
+  const digitsArray = Array.from(String(absoluteNumber), Number); // [3, 2]
+  const sum = digitsArray.reduce((sum, cur) => sum + cur, 0); // 5
+
+  return sum;
+}
+
+console.log(sumDigits(10)); // 1
+console.log(sumDigits(99)); // 18
+console.log(sumDigits(-32)); // 5
