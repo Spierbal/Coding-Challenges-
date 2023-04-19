bfc9bdf6830ed57b97ff301a79c018c86ec56943
Spierbal <abdulkadir.f.osman@gmail.com>
2023-04-19 03:09:31 +0100

    T

--- 7kyu/Fix string case.js
+++ 7kyu/Fix string case.js
@@ -1,23 +1,46 @@
+// Solution 1: using regex
 function solve(s) {
   const upperCount = (s.match(/[A-Z]/g) || []).length;
   const lowerCount = (s.match(/[a-z]/g) || []).length;
+
   return upperCount > lowerCount ? s.toUpperCase() : s.toLowerCase();
 }
 
-// Solution 2:
+// Solution 2: using charCodeAt
 /* function solve(s) {
   let upperCount = 0;
   let lowerCount = 0;
+
   for (let i = 0; i < s.length; i++) {
-    if (s[i] === s[i].toUpperCase()) upperCount++;
-    else lowerCount++;
+    const charCode = s.charCodeAt(i);
+    charCode >= 65 && charCode <= 90 ? upperCount++ : lowerCount++; // 65 is the Unicode of A and 90 is the Unicode of Z
   }
   return upperCount > lowerCount ? s.toUpperCase() : s.toLowerCase();
-}
- */
+} */
 
-/* console.log(solve("code")); // "code"
+// Solution 3: using for loop
+/* function solve(s) {
+  let upperCount = 0;
+  let lowerCount = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    s[i] === s[i].toUpperCase() ? upperCount++ : lowerCount++;
+  }
+  return upperCount > lowerCount ? s.toUpperCase() : s.toLowerCase();
+} */
+
+// Solution 4: using for of loop
+/* function solve(s) {
+  let upperCount = 0;
+  let lowerCount = 0;
+
+  for (let char of s) {
+    char === char.toUpperCase() ? upperCount++ : lowerCount++;
+  }
+  return upperCount > lowerCount ? s.toUpperCase() : s.toLowerCase();
+} */
+
+console.log(solve("code")); // "code"
 console.log(solve("CODe")); // "CODE"
 console.log(solve("COde")); // "code"
 console.log(solve("Code")); // "code"
- */
