f371110783b8ee9c2191c2a5a07b9c98a977b6e0
Spierbal <abdulkadir.f.osman@gmail.com>
2023-04-22 23:16:57 +0100

    T

--- 7kyu/Remove the minimum.js
+++ 7kyu/Remove the minimum.js
@@ -1,18 +1,27 @@
-function removeSmallest(numbers) {
-  const result = [...numbers];
-  const minIndex = numbers.indexOf(Math.min(...numbers));
-  result.splice(minIndex, 1);
+const removeSmallest = numbers =>
+  numbers.filter(num => num !== Math.min(...numbers));
 
-  return result;
-}
+// Solution 1b:
+/* const removeSmallest = numbers => {
+  const min = Math.min(...numbers); // Find the smallest value
+  return numbers.filter(num => num !== min); // Remove the smallest value
+}; */
 
-// Solution 2:
+// Solution 1c:
 /* function removeSmallest2(numbers) {
-  let minIdx = numbers.indexOf(Math.min(...numbers));
-  return numbers.filter((_, i) => i !== minIdx);
+  let minIdx = numbers.indexOf(Math.min(...numbers)); // Find index of smallest value
+  return numbers.filter((_, i) => i !== minIdx); // remove the smallest value
 } */
 
-/* console.log(removeSmallest([1, 2, 3, 4, 5])); // --> [2, 3, 4, 5]
-console.log(removeSmallest([2, 2, 1, 2, 1])); // --> [2, 2, 2, 1]
-console.log(removeSmallest([])); // --> [];
- */
+// Solution 2:
+/* function removeSmallest(numbers) {
+  const result = [...numbers]; // copy the array
+  const minIndex = numbers.indexOf(Math.min(...numbers)); // find the index of the smallest element
+  result.splice(minIndex, 1); // remove the element at minIndex
+
+  return result;
+} */
+
+// console.log(removeSmallest([1, 2, 3, 4, 5])); // --> [2, 3, 4, 5]
+// console.log(removeSmallest([2, 2, 1, 2, 1])); // --> [2, 2, 2, 1]
+// console.log(removeSmallest([])); // --> [];
---
+++ 7kyu/Sum of Minimums.js
@@ -0,0 +1,37 @@
+const sumOfMinimums = arr =>
+  arr.reduce((sum, subArr) => sum + Math.min(...subArr), 0);
+
+// Solution 2: for loop
+/* const sumOfMinimums = arr => {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += Math.min(...arr[i]);
+  }
+  return sum;
+}; */
+
+// Solution 3: for of loop
+/* const sumOfMinimums = arr => {
+  let sum = 0;
+  for (let subArr of arr) {
+    sum += Math.min(...subArr);
+  }
+  return sum;
+}; */
+
+/* console.log(
+  sumOfMinimums([
+    [7, 9, 8, 6, 2],
+    [6, 3, 5, 4, 3],
+    [5, 8, 7, 4, 5],
+  ])
+); // 9
+console.log(
+  sumOfMinimums([
+    [11, 12, 14, 54],
+    [67, 89, 90, 56],
+    [7, 9, 4, 3],
+    [9, 8, 6, 7],
+  ])
+); //76
+ */
