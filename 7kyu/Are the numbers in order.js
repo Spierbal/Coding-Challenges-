e825160b223f65f05f37fb3870ca103d2f429535
Spierbal <abdulkadir.f.osman@gmail.com>
2023-04-25 23:10:56 +0100

    T

--- 7kyu/Are the numbers in order.js
+++ 7kyu/Are the numbers in order.js
@@ -19,7 +19,7 @@ const inAscOrder = arr => arr.every((cur, i) => i === 0 || cur > arr[i - 1]);
 /* const inAscOrder = arr =>
   String(arr) === String([...arr].sort((a, b) => a - b)); */
 
-console.log(inAscOrder([1, 2, 4, 7, 19])); // true, 'The list of numbers "1, 2, 4, 7, 19" is in ascending order');
-console.log(inAscOrder([1, 2, 3, 4, 5])); // true, 'The list of numbers "1, 2, 3, 4, 5" is in ascending order');
-console.log(inAscOrder([1, 6, 10, 18, 2, 4, 20])); // false, 'The list of numbers "1, 6, 10, 18, 2, 4, 20" is not in ascending order');
-console.log(inAscOrder([9, 8, 7, 6, 5, 4, 3, 2, 1])); // false, 'The list of numbers "9, 8, 7, 6, 5, 4, 3, 2, 1" is in DESCENDING order not ascending order');
+// console.log(inAscOrder([1, 2, 4, 7, 19])); // true, 'The list of numbers "1, 2, 4, 7, 19" is in ascending order');
+// console.log(inAscOrder([1, 2, 3, 4, 5])); // true, 'The list of numbers "1, 2, 3, 4, 5" is in ascending order');
+// console.log(inAscOrder([1, 6, 10, 18, 2, 4, 20])); // false, 'The list of numbers "1, 6, 10, 18, 2, 4, 20" is not in ascending order');
+// console.log(inAscOrder([9, 8, 7, 6, 5, 4, 3, 2, 1])); // false, 'The list of numbers "9, 8, 7, 6, 5, 4, 3, 2, 1" is in DESCENDING order not ascending order');
