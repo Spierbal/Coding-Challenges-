4e6d8d9673eb9d763824036c9bec916a44ce8b0e
Spierbal <abdulkadir.f.osman@gmail.com>
2023-04-25 22:10:10 +0100

    T

--- 7kyu/Are the numbers in order.js
+++ 7kyu/Are the numbers in order.js
@@ -1,7 +1,7 @@
-// const inAscOrder = arr => arr.every((cur, i) => i === 0 || cur > arr[i - 1]);
+const inAscOrder = arr => arr.every((cur, i) => i === 0 || cur > arr[i - 1]);
 
 // Solution 1b: every() and comparison
-const inAscOrder = arr => arr.every((_, i) => i === 0 || arr[i] > arr[i - 1]);
+// const inAscOrder = arr => arr.every((_, i) => i === 0 || arr[i] > arr[i - 1]);
 
 // Solution 2: for loop
 // function inAscOrder(arr) {
