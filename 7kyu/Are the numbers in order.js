b1c42f472be54e5a392c25eab6e5f4905dd74ad1
Spierbal <abdulkadir.f.osman@gmail.com>
2023-04-26 21:16:10 +0100

    T

--- 7kyu/Are the numbers in order.js
+++ 7kyu/Are the numbers in order.js
@@ -1,15 +1,15 @@
-const inAscOrder = arr => arr.every((cur, i) => i === 0 || cur > arr[i - 1]);
+const inAscOrder = arr => arr.every((cur, i) => i === 0 || cur >= arr[i - 1]);
 
 // Solution 1b: every() and comparison
-// const inAscOrder = arr => arr.every((_, i) => i === 0 || arr[i] > arr[i - 1]);
+/* const inAscOrder = arr => arr.every((_, i) => i === 0 || arr[i] >= arr[i - 1]); */
 
 // Solution 2: for loop
-// function inAscOrder(arr) {
-//   for (let i = 0; i < arr.length - 1; i++) {
-//     if (arr[i] > arr[i + 1]) return false;
-//   }
-//   return true;
-// }
+/* function inAscOrder(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i + 1]) return false;
+  }
+  return true;
+} */
 
 // Solution 3: JSON.stringify and comparison
 /* const inAscOrder = arr =>
---
+++ 7kyu/Sorted yes no how.js
@@ -0,0 +1,30 @@
+function isSortedAndHow(arr) {
+  if (arr.every((e, i) => i === 0 || e >= arr[i - 1])) return "yes, ascending";
+  if (arr.every((e, i) => i === 0 || e <= arr[i - 1])) return "yes, descending";
+  return "no";
+}
+
+// Solution 2: ternary operator
+/* function isSortedAndHow(arr) {
+  return arr.every((cur, i) => i === 0 || cur >= arr[i - 1])
+    ? "yes, ascending"
+    : arr.every((cur, i) => i === 0 || cur <= arr[i - 1])
+    ? "yes, descending"
+    : "no";
+} */
+
+// Solution 3: for loop
+/* function isSortedAndHow(array) {
+  let ascending = true;
+  let descending = true;
+
+  for (let i = 0; i < array.length - 1; i++) {
+    if (array[i] < array[i + 1]) descending = false;
+    if (array[i] > array[i + 1]) ascending = false;
+  }
+  return ascending ? "yes, ascending" : descending ? "yes, descending" : "no";
+} */
+
+console.log(isSortedAndHow([1, 2])); // 'yes, ascending'
+console.log(isSortedAndHow([15, 7, 3, -8])); // 'yes, descending';
+console.log(isSortedAndHow([4, 2, 30])); // 'no';
