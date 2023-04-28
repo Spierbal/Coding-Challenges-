49300ba37952d46d0815a6c9f70a849271a805d3
Spierbal <abdulkadir.f.osman@gmail.com>
2023-04-29 00:01:51 +0100

    T

---
+++ 7kyu/Remove duplicate words.js
@@ -0,0 +1,33 @@
+const removeDuplicateWords = s => [...new Set(s.split(" "))].join(" ");
+
+// Solution 2: filter with indexOf
+/* const removeDuplicateWords = s => {
+  return s
+    .split(" ")
+    .filter((word, idx, arr) => arr.indexOf(word) === idx)
+    .join(" ");
+}; */
+
+// Solution 3: for loop with includes
+/* const removeDuplicateWords = s => {
+  let words = s.split(" ");
+  let uniqueWords = [];
+
+  for (let i = 0; i < words.length; i++) {
+    if (!uniqueWords.includes(words[i])) uniqueWords.push(words[i]);
+  }
+  return uniqueWords.join(" ");
+}; */
+
+// Solution 4: forEach with includes
+// function removeDuplicateWords(s) {
+//   let uniqueWords = [];
+//   s.split(" ").forEach(current => {
+//     if (!uniqueWords.includes(current)) uniqueWords.push(current);
+//   });
+//   return uniqueWords.join(" ");
+// }
+
+// console.log(
+//   removeDuplicateWords("alpha beta beta gamma gamma delta alpha delta")
+// ); // , 'alpha beta gamma delta'
