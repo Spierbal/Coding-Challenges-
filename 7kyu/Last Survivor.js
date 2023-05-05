ea059510509fe3d4c7be38737c022377020fb230
Spierbal <abdulkadir.f.osman@gmail.com>
2023-05-05 01:31:16 +0100

    T

--- 7kyu/Last Survivor.js
+++ 7kyu/Last Survivor.js
@@ -1,14 +1,9 @@
-const lastSurvivor1 = (letters, coords) =>
-  coords.reduce((acc, curr) => {
-    return acc.slice(0, curr) + acc.slice(curr + 1);
-  }, letters);
-
 // Solution 1b: more concise reduce
-/* const lastSurvivor1b = (char, coords) =>
-  coords.reduce((acc, curr) => acc.slice(0, curr) + acc.slice(curr + 1), char); */
+const lastSurvivor = (char, coords) =>
+  coords.reduce((acc, curr) => acc.slice(0, curr) + acc.slice(curr + 1), char);
 
 // Solution 2: For Loop with Slice
-/* function lastSurvivor2(letters, coords) {
+/* function lastSurvivor(letters, coords) {
   let arr = letters;
   for (let i = 0; i < coords.length; i++) {
     arr = arr.slice(0, coords[i]) + arr.slice(coords[i] + 1);
@@ -17,7 +12,7 @@ const lastSurvivor1 = (letters, coords) =>
 } */
 
 // Solution 3: For Loop with Splice
-/* function lastSurvivor3(letters, coords) {
+/* function lastSurvivor(letters, coords) {
   let arr = letters.split("");
   for (let i = 0; i < coords.length; i++) {
     arr.splice(coords[i], 1);
@@ -25,6 +20,6 @@ const lastSurvivor1 = (letters, coords) =>
   return arr.join("");
 } */
 
-/* console.log(lastSurvivor1("abc", [1, 1])); // 'a'
-console.log(lastSurvivor2("c", [])); // 'c'
-console.log(lastSurvivor3("zbk", [2, 1])); // 'z' */
+console.log(lastSurvivor("abc", [1, 1])); // 'a'
+console.log(lastSurvivor("c", [])); // 'c'
+console.log(lastSurvivor("zbk", [2, 1])); // 'z'
