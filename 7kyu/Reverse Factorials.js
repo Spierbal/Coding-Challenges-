2b39bd09f8d2b549f99ff49e117eb265c99f4719
Spierbal <abdulkadir.f.osman@gmail.com>
2023-05-07 21:04:48 +0100

    T

--- 7kyu/Reverse Factorials.js
+++ 7kyu/Reverse Factorials.js
@@ -1,22 +1,22 @@
-/* function reverseFactorial(num) {
+function reverseFactorial(num) {
   let i;
   for (i = 2; num > 1; i++) {
     num /= i;
   }
   return num === 1 ? `${i - 1}` : "None";
-} */
+}
 
 // Solution 2: while loop
-// function reverseFactorial(num) {
-//   let i = 1;
+/* function reverseFactorial(num) {
+  let i = 1;
+  while (num > 1) {
+    i++;
+    num /= i;
+  }
+  return num === 1 ? `${i}` : "None";
+} */
 
-//   while (num > 1) {
-//     i++;
-//     num /= i;
-//   }
-//   return num === 1 ? `${i}` : "None";
-// }
-
-console.log(reverseFactorial(120)); // "5!"
+/* console.log(reverseFactorial(120)); // "5!"
 console.log(reverseFactorial(3628800)); // '10!'
 console.log(reverseFactorial(150)); // "None"
+ */
