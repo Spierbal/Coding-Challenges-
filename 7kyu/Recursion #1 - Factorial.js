2fff30fe5e57419b5a79ffacd6f627a783dc09fa
Spierbal <abdulkadir.f.osman@gmail.com>
2023-05-10 05:17:44 +0100

    T

---
+++ 7kyu/Recursion #1 - Factorial.js
@@ -0,0 +1,9 @@
+const factorial = n => (n === 0 ? 1 : n * factorial(n - 1));
+
+/* console.log(factorial(0)); // 1, (factorial for 0 is 1)
+console.log(factorial(1)); // 1,( factorial(1) returns 1 * 1, which is 1)
+console.log(factorial(2)); // 2, (factorial(2) returns 2 * 1, which is 2)
+console.log(factorial(3)); // 6, (factorial(3) returns 3 * 2, which is 6)
+console.log(factorial(4)); // 24, (factorial(4) returns 4 * 6, which is 24)
+console.log(factorial(5)); // 120, (factorial(5) returns 5 * 24, which is 120)
+ */
