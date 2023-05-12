17c5d60e014760865b4a00716a731b78dcb6c226
Spierbal <abdulkadir.f.osman@gmail.com>
2023-05-12 01:01:55 +0100

    T

--- 7kyu/The Coupon Code.js
+++ 7kyu/The Coupon Code.js
@@ -6,12 +6,12 @@
 // }
 
 // Solution Date Objects
-const checkCoupon = (enteredCode, correctCode, currentDate, expirationDate) => {
+/* const checkCoupon = (enteredCode, correctCode, currentDate, expirationDate) => {
   const currentDateObj = new Date(currentDate);
   const expirationDateObj = new Date(expirationDate);
 
   return enteredCode === correctCode && currentDateObj <= expirationDateObj;
-};
+}; */
 
 // Solution Date.parse()
 /* function checkCoupon(enteredCode, correctCode, currentDate, expirationDate) {
