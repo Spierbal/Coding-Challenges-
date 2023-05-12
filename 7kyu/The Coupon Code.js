2409bba1cd58f9f04a580e3c61aab6f25879cfea
Spierbal <abdulkadir.f.osman@gmail.com>
2023-05-12 01:02:30 +0100

    T

--- 7kyu/The Coupon Code.js
+++ 7kyu/The Coupon Code.js
@@ -21,5 +21,5 @@
   );
 } */
 
-console.log(checkCoupon("123", "123", "September 5, 2014", "October 1, 2014")); // true
-console.log(checkCoupon("123a", "123", "September 5, 2014", "October 1, 2014")); // false
+// console.log(checkCoupon("123", "123", "September 5, 2014", "October 1, 2014")); // true
+// console.log(checkCoupon("123a", "123", "September 5, 2014", "October 1, 2014")); // false
