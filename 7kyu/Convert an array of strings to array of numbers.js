4baf73e710bb873f551a3e473ae2f2ce3902f896
Spierbal <abdulkadir.f.osman@gmail.com>
2023-05-27 23:59:22 +0100

    T

---
+++ 7kyu/Convert an array of strings to array of numbers.js
@@ -0,0 +1,25 @@
+const toNumberArray = stringarray => stringarray.map(Number);
+
+/* 
+// Solution 2: for loop
+function toNumberArray(stringarray) {
+  let numArray = [];
+
+  for (let i = 0; i < stringarray.length; i++) {
+    numArray.push(Number(stringarray[i]));
+  }
+  return numArray;
+} */
+
+/* 
+// Solution 2b: for of loop
+function toNumberArray(stringarray) {
+  let numArray = [];
+
+  for (let char of stringarray) {
+    numArray.push(Number([char]));
+  }
+  return numArray;
+} */
+
+// console.log(toNumberArray(["1.1", "2.2", "3.3"])); //, [1.1,2.2,3.3]);
