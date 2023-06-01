f868fe4c0c02373f413805aeb791f283187499b3
Spierbal <abdulkadir.f.osman@gmail.com>
2023-06-01 17:46:35 +0100

    T

--- 7kyu/Sum of numbers from 0 to N.js
+++ 7kyu/Sum of numbers from 0 to N.js
@@ -1,4 +1,5 @@
-class SequenceSum1 {
+// Solution 1: functional approach
+class SequenceSum {
   static showSequence(count) {
     if (count < 0) return `${count}<0`;
     if (count === 0) return "0=0";
@@ -10,7 +11,9 @@ class SequenceSum1 {
   }
 }
 
-class SequenceSum2 {
+/* 
+// Solution 2: for loop
+class SequenceSum {
   static showSequence(count) {
     let sum = 0;
     let series = "";
@@ -29,6 +32,6 @@ class SequenceSum2 {
 
     return `${series} = ${sum}`;
   }
-}
+} */
 
-console.log(SequenceSum.showSequence(6)); // "0+1+2+3+4+5+6 = 21"
+// console.log(SequenceSum.showSequence(6)); // "0+1+2+3+4+5+6 = 21"
