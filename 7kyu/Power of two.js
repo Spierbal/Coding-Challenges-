bf7795a46e0948db02cda1cd045f2eafc6736da4
Spierbal <abdulkadir.f.osman@gmail.com>
2023-06-04 02:03:26 +0100

    T

--- 7kyu/Power of two.js
+++ 7kyu/Power of two.js
@@ -1,9 +1,28 @@
 // Solution 1:
-const isPowerOfTwo = n => n !== 0 && (n & (n - 1)) === 0;
+const isPowerOfTwo = n => Number.isInteger(Math.log2(n));
 
-// Solution 2:
-const isPowerOfTwo = n => [...Array(n)].map((_, i) => 2 ** i).includes(n);
+/* 
+// Solution 1b:
+const isPowerOfTwo = n => Math.log2(n) % 1 === 0;
+ */
 
-console.log(isPowerOfTwo(2)); // true
-console.log(isPowerOfTwo(4096)); // true
-console.log(isPowerOfTwo(5)); // false
+/* 
+// Solution 2: while loop
+function isPowerOfTwo(n) {
+  while (n % 2 === 0 && n > 1) {
+    n /= 2;
+  }
+  return n === 1;
+} */
+
+/* 
+// Solution 3: recursion
+function isPowerOfTwo(n) {
+  if (n === 1) return true;
+  if (n <= 0 || n % 2 !== 0) return false;
+  return isPowerOfTwo(n / 2);
+} */
+
+// console.log(isPowerOfTwo(2)); // true
+// console.log(isPowerOfTwo(4096)); // true
+// console.log(isPowerOfTwo(5)); // false
