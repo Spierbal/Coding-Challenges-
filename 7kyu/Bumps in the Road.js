8d2858570bff4fe54f8c2ca94561707af21d38b4
Spierbal <abdulkadir.f.osman@gmail.com>
2023-06-05 02:09:33 +0100

    T

--- 7kyu/Bumps in the Road.js
+++ 7kyu/Bumps in the Road.js
@@ -1,8 +1,52 @@
-const bump = x => ((x.match(/n/g) || []).length <= 15 ? "Woohoo!" : "Car Dead");
+// Solution 1: filter()
+const bump = x =>
+  [...x].filter(char => char === "n").length <= 15 ? "Woohoo!" : "Car Dead";
+
+/* 
+// Solution 2: reduce() 
+function bump(x) {
+    const bumps = [...x].reduce((count, char) => {
+        if (char === "n") count++;
+        return count;
+    }, 0);
+    
+    return bumps <= 15 ? "Woohoo!" : "Car Dead";
+}  
+
+// Solution 2b: More concise
+const bump = x =>
+  [...x].reduce((a, b) => a + (b === "n"), 0) <= 15 ? "Woohoo!" : "Car Dead"; */
+
+/*
+// Solution 3: split()
+const bump = x => (x.split("n").length > 16 ? "Car Dead" : "Woohoo!");
+
+// Solution 3b: More readable
+function bump(x) {
+  const bumps = x.split("n").length - 1; // Count the number of bumps
+
+  return bumps <= 15 ? "Woohoo!" : "Car Dead";
+} */
+
+/* 
+// Solution 4: for loop
+function bump(x) {
+  let bumps = 0;
+
+  for (let i = 0; i < x.length; i++) {
+    if (x[i] === "n") bumps++;
+  }
+
+  return bumps <= 15 ? "Woohoo!" : "Car Dead";
+} */
+
+/* 
+// Solution 5: match()
+const bump = x => ((x.match(/n/g) || []).length <= 15 ? "Woohoo!" : "Car Dead"); */
 
 // console.log(bump("__nn_nnnn__n_n___n____nn__nnn")); // "Woohoo!"
-// console.log(bump("nnn_n__n_n___nnnnn___n__nnn__")); //, "Woohoo!");
-// console.log(bump("_"));
-// console.log(bump("nnn_n__n_n___nnnnn___n__nnn__"))
-// console.log(bump("_nnnnnnn_n__n______nn__nn_nnn"))
-// console.log(bump("______n___n_"))
+// console.log(bump("nnn_n__n_n___nnnnn___n__nnn__")); // "Woohoo!"
+// console.log(bump("_")); // "Woohoo!"
+// console.log(bump("nnn_n__n_n___nnnnn___n__nnn__")); // "Woohoo!"
+// console.log(bump("_nnnnnnn_n__n______nn__nn_nnn")); // "Car Dead"
+// console.log(bump("______n___n_")); // "Woohoo!"
