e79c77409454898fd0ef0ebc9de119e7480a0f11
Spierbal <abdulkadir.f.osman@gmail.com>
2023-06-06 00:59:36 +0100

    T

--- 7kyu/Most digits.js
+++ 7kyu/Most digits.js
@@ -1,19 +1,17 @@
 // Solution 1: Reduce
-function findLongest(array) {
-  return array.reduce((a, b) => (`${b}`.length > `${a}`.length ? b : a));
-}
+// function findLongest(array) {
+//   return array.reduce((a, b) => (`${b}`.length > `${a}`.length ? b : a));
+// }
 
-/* 
-// Solution 1b: More readable 
+// Solution 1b: More readable
 function findLongest(array) {
-  return array.reduce((a, b) => {
-    const aLength = String(a).length;
-    const bLength = String(b).length;
-    return aLength > bLength ? a : b;
+  return array.reduce((longest, curr) => {
+    const longestLength = String(longest).length;
+    const currLength = String(curr).length;
+    return currLength > longestLength ? curr : longest;
   });
 }
- */
 
 // console.log(findLongest([1, 10, 100])); // 100
-// console.log(findLongest([9000, 8, 800])); // 9000
+console.log(findLongest([9000, 8, 800])); // 9000
 // console.log(findLongest([8, 900, 500])); // 900
