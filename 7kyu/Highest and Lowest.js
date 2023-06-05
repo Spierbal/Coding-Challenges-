0e7349e9b056a408daf77c044b5c0372c6c818f9
Spierbal <abdulkadir.f.osman@gmail.com>
2023-06-06 00:00:08 +0100

    T

--- 7kyu/Highest and Lowest.js
+++ 7kyu/Highest and Lowest.js
@@ -7,4 +7,5 @@ function highAndLow(numbers) {
 const highAndLow = numbers =>
   `${Math.max(...numbers.split(" "))} ${Math.min(...numbers.split(" "))}`;
  */
-console.log(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4")); // --> 42 -9
+
+// console.log(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4")); // --> 42 -9
