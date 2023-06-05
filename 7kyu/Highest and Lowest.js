5330e48161d6fe533a86d0dbf591c97f4f36309d
Spierbal <abdulkadir.f.osman@gmail.com>
2023-06-06 00:38:44 +0100

    T

--- 7kyu/Highest and Lowest.js
+++ 7kyu/Highest and Lowest.js
@@ -1,11 +1,15 @@
-function highAndLow(numbers) {
-  const n = numbers.split(" ");
-  return `${Math.max(...n)} ${Math.min(...n)}`;
-}
-/* 
-//Solution 2:
+// Solution 1: Using Math.max and Math.min
 const highAndLow = numbers =>
   `${Math.max(...numbers.split(" "))} ${Math.min(...numbers.split(" "))}`;
+
+/* 
+// Solution 1b: More readable
+const highAndLow = numbers => {
+  const numberArray = numbers.split(" ").map(Number);
+  const max = Math.max(...numberArray);
+  const min = Math.min(...numberArray);
+  return `${max} ${min}`;
+};
  */
 
 // console.log(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4")); // --> 42 -9
