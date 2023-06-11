690638a596ceb603f2e155066cb2a1f607c5c0df
Spierbal <abdulkadir.f.osman@gmail.com>
2023-06-11 01:15:09 +0100

    T

--- 7kyu/Number of Decimal Digits.js
+++ 7kyu/Number of Decimal Digits.js
@@ -1,9 +1,13 @@
-// Solution 1: Using +""
-const digits = n => (n + "").length;
+// Solution 1: Using String()
+const digits = n => String(n).length;
+
+/*
+// Solution 2: Using toString()
+const digits = n => n.toString().length; */
 
 /* 
-// Solution 2: Using String()
-const digits = n => String(n).length; */
+// Solution 3: Using +""
+const digits = n => (n + "").length; */
 
 // console.log(digits(5)); // 1
 // console.log(digits(12345)); // 5
