88d43674e2cdd510a8c909772604740c34d65b07
Spierbal <abdulkadir.f.osman@gmail.com>
2023-06-18 02:38:20 +0100

    T

--- 7kyu/Valid Parentheses.js
+++ 7kyu/Valid Parentheses.js
@@ -1,14 +1,44 @@
+// Solution 1: for of loop
 function validParentheses(parenStr) {
   let count = 0;
 
   for (let char of parenStr) {
-    count += char === "(" ? 1 : char === ")" ? -1 : 0;
+    if (char === "(") count++;
+    if (char === ")") count--;
 
     if (count < 0) return false;
   }
-
   return count === 0;
 }
+
+/* 
+// Solution 1b: for loop
+function validParentheses(parentStr) {
+  let counter = 0;
+
+  for (let i = 0; i < parentStr.length; i++) {
+    if (parentStr[i] === "(") counter++;
+    if (parentStr[i] === ")") counter--;
+
+    if (counter < 0) return false;
+  }
+  return counter === 0;
+} */
+
+/* 
+// Solution 2: Using stack with for of loop
+function validParentheses(parenStr) {
+  const stack = [];
+
+  for (let char of parenStr) {
+    if (char === "(") stack.push(char);
+    if (char === ")") {
+      if (stack.length === 0) return false;
+      stack.pop();
+    }
+  }
+  return stack.length === 0;
+} */
 
 // console.log(validParentheses("()")); // true
 // console.log(validParentheses("((()))")); // true
