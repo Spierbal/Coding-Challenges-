10d49cecb149171824301e581154ec3e40c50920
Spierbal <abdulkadir.f.osman@gmail.com>
2023-07-20 22:42:23 +0100

    T

--- 7kyu/Triangular Treasure.js
+++ 7kyu/Triangular Treasure.js
@@ -1,26 +1,22 @@
-//Gauss formula solution
-// const triangular = n => (n <= 0 ? 0 : (n * (n + 1)) / 2);
+// Gauss formula solution
+const triangular = n => (n <= 0 ? 0 : (n * (n + 1)) / 2);
 
 /* 
-// Solution 2: for loop
-function triangular(n) {
-  if (n <= 0) return 0; // Handle out of range values
-
-  let sum = 0;
-  for (let i = 1; i <= n; i++) {
-    sum += i;
-  }
-  return sum;
-} */
+// Solution 1b: uses Math.sign to explicitly check for positive number
+const triangular = n => (Math.sign(n) === 1 ? (n * (n + 1)) / 2 : 0);
+ */
 
 /* 
-// Solution 3: recursion
+// Solution 2: while loop
 function triangular(n) {
-  if (n <= 0) return 0;
-
-  return n + triangular(n - 1);
+    let sum = 0;
+    while (n > 0) {
+        sum += n;
+        n--;
+    }
+    return sum;
 } */
 
-// console.log(triangular(2)); //3
-// console.log(triangular(4)); //10
-// console.log(triangular(-454)); //0
+// console.log(triangular(2)); // 3
+// console.log(triangular(4)); // 10
+// console.log(triangular(-454)); // 0
