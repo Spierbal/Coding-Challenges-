dde56fa3049e961a2fa26a5b984783ab0cdfa90d
Spierbal <abdulkadir.f.osman@gmail.com>
2023-07-23 04:10:51 +0100

    T

--- 7kyu/Deodorant Evaporator.js
+++ 7kyu/Deodorant Evaporator.js
@@ -1,22 +1,22 @@
 function evaporator(content, evap_per_day, threshold) {
   let currContent = content;
   const thresholdContent = (threshold / 100) * content;
-  let days = 0;
+  let numberOfDays = 0;
 
   while (currContent > thresholdContent) {
     currContent -= (evap_per_day / 100) * currContent;
-    days++;
+    numberOfDays++;
   }
-  return days;
+  return numberOfDays;
 }
 
 // Solution 1b: for loop
 /* function evaporator(content, evap_per_day, threshold) {
   let currContent = content;
-  const thresholdContent = (threshold / 100) * content;
+  const remainingContent = (threshold / 100) * content;
   let days;
 
-  for (days = 0; currContent > thresholdContent; days++) {
+  for (days = 0; currContent > remainingContent; days++) {
     currContent -= (evap_per_day / 100) * currContent;
   }
   return days;
