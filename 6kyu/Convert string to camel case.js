da721352bc82997e2500669cc0c5a8da55a2cedc
Spierbal <abdulkadir.f.osman@gmail.com>
2023-07-24 21:46:00 +0100

    T

--- 6kyu/Convert string to camel case.js
+++ 6kyu/Convert string to camel case.js
@@ -1,5 +1,16 @@
+// Solution 1: replace()
 const toCamelCase = str =>
   str.replace(/[-_](\w)/g, (_, char) => char.toUpperCase());
+
+/* 
+// Solution 2: split() & map() & join()
+function toCamelCase(str) {
+  return str
+    .split(/[-_]/)
+    .map((w, i) => (i === 0 ? w : w.charAt(0).toUpperCase() + w.slice(1)))
+    .join("");
+}
+ */
 
 // console.log(toCamelCase("")); // ""
 // console.log(toCamelCase("the_stealth_warrior")); // "theStealthWarrior"
