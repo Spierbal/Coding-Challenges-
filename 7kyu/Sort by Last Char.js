9234edb99b1554020f41878dca4729c6efce40eb
Spierbal <abdulkadir.f.osman@gmail.com>
2023-07-25 02:46:25 +0100

    T

--- 7kyu/Sort by Last Char.js
+++ 7kyu/Sort by Last Char.js
@@ -12,4 +12,3 @@ function last(x) {
 // console.log(last('man i need a taxi up to ubud')) // ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']);
 // console.log(last('what time are we climbing up the volcano'))// ['time', 'are', 'we', 'the', 'climbing', 'volcano', 'up', 'what']);
 // console.log(last("take me to semynak")); // ['take', 'me', 'semynak', 'to']);
-// console.log(last("cherry banana apple")); // ['take', 'me', 'semynak', 'to']);
