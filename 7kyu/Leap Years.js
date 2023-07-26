63302a1df7aad316a22a995a8bd258b12a8254e8
Spierbal <abdulkadir.f.osman@gmail.com>
2023-07-26 23:58:39 +0100

    T

---
+++ 7kyu/Leap Years.js
@@ -0,0 +1,18 @@
+// Solution 1: ternary operator
+const isLeapYear = year =>
+  year % 100 === 0 ? year % 400 === 0 : year % 4 === 0;
+
+/* 
+// Solution 1b: using logical OR
+const isLeapYear = year =>
+  (year % 100 !== 0 && year % 4 === 0) || year % 400 === 0;
+   */
+
+/* 
+// Solution 2: using new Date()
+const isLeapYear = year => new Date(year, 1, 29).getDate() === 29; */
+
+// console.log(isLeapYear(2020)); //, true,
+// console.log(isLeapYear(2000)); //, true,
+// console.log(isLeapYear(2015)); //, false,
+// console.log(isLeapYear(2100)); //, false,
