1376a6b1e1d7cdf250cf2db564760c28d8fb850a
Spierbal <abdulkadir.f.osman@gmail.com>
2023-07-28 21:05:04 +0100

    T

--- 7kyu/Alphabet symmetry.js
+++ 7kyu/Alphabet symmetry.js
@@ -1,22 +1,19 @@
 // Solution 1: map and reduce
-function solve(arr) {
-  return arr.map(word =>
+const solve = words =>
+  words.map(word =>
     word.split("").reduce((count, char, i) => {
       return count + (char.toLowerCase().charCodeAt(0) - 97 === i);
     }, 0)
   );
-}
 
 /* 
 // Solution 2: map and filter
-function solve(arr) {
-  return arr.map(
-    word =>
-      word
-        .split("")
-        .filter((char, i) => char.toLowerCase().charCodeAt(0) - 97 === i).length
-  );
-} */
+const solve = arr =>
+  arr.map(x => {
+    return x.split("").filter((c, i) => c.toLowerCase().charCodeAt() - 97 === i)
+      .length;
+  });
+ */
 
 /* 
 // Solution 3: map and for loop
@@ -30,7 +27,7 @@ function solve(words) {
   });
 } */
 
-// console.log(solve(["abode", "ABc", "xyzD"]));
-// console.log(solve(["abide", "ABc", "xyz"])); // [4,3,1]);
-// console.log(solve(["IAMDEFANDJKL", "thedefgh", "xyzDEFghijabc"])); // [6, 5, 7]);
-// console.log(solve(["encode", "abc", "xyzD", "ABmD"])); // [1, 3, 1, 3]);
+console.log(solve(["abode", "ABc", "xyzD"]));
+console.log(solve(["abide", "ABc", "xyz"])); // [4,3,1]);
+console.log(solve(["IAMDEFANDJKL", "thedefgh", "xyzDEFghijabc"])); // [6, 5, 7]);
+console.log(solve(["encode", "abc", "xyzD", "ABmD"])); // [1, 3, 1, 3]);
