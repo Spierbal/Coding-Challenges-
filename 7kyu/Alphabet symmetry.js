f055f8dda6a30b4273dd4e064ac1b5cf74ae3e16
Spierbal <abdulkadir.f.osman@gmail.com>
2023-07-28 18:04:58 +0100

    T

--- 7kyu/Alphabet symmetry.js
+++ 7kyu/Alphabet symmetry.js
@@ -16,11 +16,21 @@ function solve(arr) {
         .split("")
         .filter((char, i) => char.toLowerCase().charCodeAt(0) - 97 === i).length
   );
-}
- */
+} */
+
+/* 
+// Solution 3: map and for loop
+function solve(words) {
+  return words.map(word => {
+    let count = 0;
+    for (let i = 0; i < word.length; i++) {
+      if (word[i].toLowerCase().charCodeAt(0) - "a".charCodeAt() === i) count++;
+    }
+    return count;
+  });
+} */
 
 // console.log(solve(["abode", "ABc", "xyzD"]));
-// console.log(solve(["abode"])); // [4,3,1]);
-// console.log(solve(["abide", "ABc", "xyz"])); // [4,3,0]);
+// console.log(solve(["abide", "ABc", "xyz"])); // [4,3,1]);
 // console.log(solve(["IAMDEFANDJKL", "thedefgh", "xyzDEFghijabc"])); // [6, 5, 7]);
 // console.log(solve(["encode", "abc", "xyzD", "ABmD"])); // [1, 3, 1, 3]);
