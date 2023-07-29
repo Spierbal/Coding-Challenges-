cf57ec4e020ba9c8beef784ab92a12209509b3f7
Spierbal <abdulkadir.f.osman@gmail.com>
2023-07-29 04:43:05 +0100

    T

---
+++ 7kyu/All unique.js
@@ -0,0 +1,30 @@
+// Solution 1: Concise solution with Set
+const hasUniqueChars1 = str => new Set([...str]).size === str.length;
+
+/* 
+// Solution 1b: Long solution with Set
+function hasUniqueChars(str) {
+  const uniqueCharsSet = new Set();
+  for (const char of str) {
+    if (uniqueCharsSet.has(char)) return false;
+    uniqueCharsSet.add(char);
+  }
+  return true;
+} */
+
+/* 
+// Solution 2: use object as a map
+function hasUniqueChars3(str) {
+  const charMap = {};
+  for (const char of str) {
+    if (charMap[char]) return false;
+    charMap[char] = true;
+  }
+  return true;
+}
+ */
+
+// console.log(hasUniqueChars("  nAa")); // false) // because there are two spaces ' '
+// console.log(hasUniqueChars("abcdef")); // true)
+// console.log(hasUniqueChars("aA")); // true) // case - sensitivity
+// console.log(hasUniqueChars("++-")); // false) // because there are two '+'
