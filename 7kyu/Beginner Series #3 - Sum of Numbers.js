c609e13cb61718d4f49189cea01693811e8b9f20
Spierbal <abdulkadir.f.osman@gmail.com>
2023-08-01 00:25:41 +0100

    T

--- 7kyu/Beginner Series #3 - Sum of Numbers.js
+++ 7kyu/Beginner Series #3 - Sum of Numbers.js
@@ -1,8 +1,8 @@
 // Solution 1: for loop with Math.max and Math.min
 function getSum(a, b) {
   let sum = 0;
-  const start = Math.max(a, b);
-  const end = Math.min(a, b);
+  const end = Math.max(a, b);
+  const start = Math.min(a, b);
 
   for (let i = start; i <= end; i++) sum += i;
   return sum;
@@ -10,7 +10,7 @@ function getSum(a, b) {
 
 /* 
 // Solution 1b: for loop with ternary operator
-function getSum2(a, b) {
+function getSum(a, b) {
   let sum = 0;
   const max = a > b ? a : b;
   const min = a > b ? b : a;
@@ -18,11 +18,11 @@ function getSum2(a, b) {
   for (let i = min; i <= max; i++) sum += i;
   return sum;
 }
- */
+  */
 
 /* 
 // Solution 1c: for loop with array destructuring
-function getSum3(a, b) {
+function getSum(a, b) {
   let sum = 0;
   const [max, min] = a > b ? [a, b] : [b, a];
 
@@ -32,7 +32,7 @@ function getSum3(a, b) {
 
 /* 
 // Solution 2: Recursion
-function getSum4(a, b) {
+function getSum(a, b) {
   if (a === b) return a;
   if (a > b) return a + getSum(a - 1, b);
 
@@ -41,3 +41,5 @@ function getSum4(a, b) {
 
 // console.log(getSum(1, 2)); // --> 3  // 1 + 2 = 3
 // console.log(getSum(-1, 2)); // --> 2
+console.log(getSum(5, 2)); // --> 2
+console.log(getSum(10, 3)); // --> 2
