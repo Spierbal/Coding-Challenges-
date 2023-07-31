27d17b93bb101959a12edea65273504ff5574fb3
Spierbal <abdulkadir.f.osman@gmail.com>
2023-07-31 23:41:46 +0100

    T

--- 7kyu/Beginner Series #3 - Sum of Numbers.js
+++ 7kyu/Beginner Series #3 - Sum of Numbers.js
@@ -1,40 +1,43 @@
+// Solution 1: for loop with Math.max and Math.min
 function getSum(a, b) {
-  let res = 0;
-  const max = Math.max(a, b);
-  const min = Math.min(a, b);
+  let sum = 0;
+  const start = Math.max(a, b);
+  const end = Math.min(a, b);
 
-  for (let i = min; i <= max; i++) res += i;
-  return res;
+  for (let i = start; i <= end; i++) sum += i;
+  return sum;
 }
 
-// Solution 1b:
-/* function getSum(a, b) {
-  let res = 0;    
+/* 
+// Solution 1b: for loop with ternary operator
+function getSum2(a, b) {
+  let sum = 0;
   const max = a > b ? a : b;
   const min = a > b ? b : a;
 
-  for (let i = min; i <= max; i++) res += i;
-  return res;
-} */
-
-// Solution 1c:
-/* function getSum(a, b) {
-  let res = 0;
-  const [max, min] = a > b ? [a, b] : [b, a];
-
-  for (let i = min; i <= max; i++) res += i;
-  return res;
+  for (let i = min; i <= max; i++) sum += i;
+  return sum;
 }
  */
 
-// Solution 2:
-/* function getSum(a, b) {
+/* 
+// Solution 1c: for loop with array destructuring
+function getSum3(a, b) {
+  let sum = 0;
+  const [max, min] = a > b ? [a, b] : [b, a];
+
+  for (let i = min; i <= max; i++) sum += i;
+  return sum;
+} */
+
+/* 
+// Solution 2: Recursion
+function getSum4(a, b) {
   if (a === b) return a;
   if (a > b) return a + getSum(a - 1, b);
 
   return a + getSum(a + 1, b);
-}
- */
+} */
 
-// console.log(getSum(1, 2)); // --> 3
+// console.log(getSum(1, 2)); // --> 3  // 1 + 2 = 3
 // console.log(getSum(-1, 2)); // --> 2
--- 7kyu/Halving Sum.js
+++ 7kyu/Halving Sum.js
@@ -1,8 +1,30 @@
+// Solution 1: while loop
 function halvingSum(n) {
-  for (let i = 0; i <= n; i++) {
-    console.log(n[i]);
+  let sum = 0;
+
+  while (n > 0) {
+    sum += n;
+    n = Math.floor(n / 2);
   }
-  // return +[...String(n)].reduce((a, b, c) =>  );
+  return sum;
 }
-console.log(halvingSum(25)); //47
-console.log(halvingSum(127)); //247
+
+/* 
+// Solution 1b: for loop
+function halvingSum(n) {
+  let sum = 0;
+
+  for (let i = n; i > 0; i = Math.floor(i / 2)) {
+    sum += i;
+  }
+  return sum;
+} */
+
+/* 
+// Solution 2: reduce 
+const halvingSum = n => {
+  return [...Array(n)].reduce((sum, _, i) => sum + Math.floor(n / 2 ** i), 0);
+}; */
+
+// console.log(halvingSum(25)); //47 (25 + 12 + 6 + 3 + 1 = 47)
+// console.log(halvingSum(127)); //247
