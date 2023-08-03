13815d4fb5d7decb0241925a5f4faccfe32c68fe
Spierbal <abdulkadir.f.osman@gmail.com>
2023-08-03 23:01:24 +0100

    T

--- 7kyu/Largest 5 digit number in a series.js
+++ 7kyu/Largest 5 digit number in a series.js
@@ -1,42 +1,30 @@
-// Solution 1: .map() and .slice()
+// Solution 1: map and .slice()
 function solution(digits) {
   const sequences = [...digits].map((_, i) => Number(digits.slice(i, i + 5)));
-
   return Math.max(...sequences);
 }
 
-// Solution 2: Array.from() and .slice()
-/* const solution = n => Math.max(...Array.from(n, (_, i) => +n.slice(i, i + 5))); */
-
-// Solution 3: For loop and .slice()
-/* function solution(digits) {
+/* 
+// Solution 2: For loop and .slice()
+function solution(digits) {
   let maxSequence = 0;
-  const length = digits.length - 4;
 
-  for (let i = 0; i < length; i++) {
-    const seq = +digits.slice(i, i + 5);
-    maxSequence = Math.max(seq, maxSequence);
+  for (let i = 0; i <= digits.length - 5; i++) {
+    const sequence = Number(digits.slice(i, i + 5));
+    maxSequence = Math.max(sequence, maxSequence);
   }
   return maxSequence;
 } */
 
 /* 
-// Solution 3b: For loop and .slice()
-function solution(digits) {
-  let maxSequence = 0;
-  const length = digits.length - 4;
-
-  for (let i = 0; i < length; i++) {
-    const seq = +digits.slice(i, i + 5);
-    if (seq > maxSequence) maxSequence = seq;
-  }
-  return maxSequence;
-}  */
+// Solution 3: Array.from() and .slice()
+const solution = n => Math.max(...Array.from(n, (_, i) => +n.slice(i, i + 5)));
+ */
 
 /* 
-// Solution 4:
+// Solution 4: reduce and .slice()
 function solution(digits) {
-  return digits.split("").reduce((maxSequence, _, i) => {
+  return [...digits].reduce((maxSequence, _, i) => {
     const sequence = Number(digits.slice(i, i + 5));
     return sequence > maxSequence ? sequence : maxSequence;
   }, 0);
