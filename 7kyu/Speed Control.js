accb68fd706a8fb7b0777a98bcca1b3bd3e4cd72
Spierbal <abdulkadir.f.osman@gmail.com>
2023-08-08 23:44:05 +0100

    T

--- 7kyu/Speed Control.js
+++ 7kyu/Speed Control.js
@@ -1,19 +1,46 @@
+// Solution 1: map and Math.max
 function gps(s, x) {
   if (x.length <= 1) return 0;
 
-  let arr = [];
+  const speeds = x.slice(1).map((e, i) => ((e - x[i]) / s) * 3600);
+  const maxSpeed = Math.floor(Math.max(...speeds));
+
+  return maxSpeed;
+}
+
+/*  
+// Solution 2: for loop
+function gps(s, x) {
+  if (x.length <= 1) return 0;
+
+  let maxSpeed = 0;
+
+  for (let i = 1; i < x.length; i++) {
+    const speed = (3600 * (x[i] - x[i - 1])) / s;
+    if (speed > maxSpeed) maxSpeed = speed;
+  }
+  return Math.floor(maxSpeed);
+} 
+ */
+
+/* 
+// Solution 2: for loop and Math.max
+function gps(s, x) {
+  if (x.length <= 1) return 0;
+
+  let speeds = [];
 
   for (let i = 1; i < x.length; i++) {
     const distance = x[i] - x[i - 1];
     const avgSpeed = (3600 * distance) / s;
-    arr.push(avgSpeed);
+    speeds.push(avgSpeed);
   }
-  const maxAvgSpeed = Math.floor(Math.max(...arr));
-  return maxAvgSpeed;
-}
+  const maxSpeed = Math.floor(Math.max(...speeds));
+  return maxSpeed;
+} 
+*/
 
 // console.log(gps(20, [0.0, 0.23, 0.46, 0.69, 0.92, 1.15, 1.38, 1.61])); //41
 // console.log(gps(17, [0.0, 0.02, 0.44, 0.66, 0.88, 1.1, 1.32, 1.54, 1.76])); //88
-// console.log(gps(20, [0.0, 0.23, 0.46, 0.69, 0.92, 1.15, 1.38, 1.61]));
 // console.log(gps(19, [0.0])); // 0
 // console.log(gps(19, [])); //0
