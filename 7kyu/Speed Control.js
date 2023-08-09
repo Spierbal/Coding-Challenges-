8eda1c0608a52f80833b4bba13fe462e2d82b9fe
Spierbal <abdulkadir.f.osman@gmail.com>
2023-08-09 01:36:57 +0100

    T

--- 7kyu/Speed Control.js
+++ 7kyu/Speed Control.js
@@ -38,6 +38,20 @@ function gps(s, x) {
 } 
 */
 
+/* 
+// Solution 3: reduce
+function gps(s, x) {
+  if (x.length <= 1) return 0;
+
+  return Math.floor(
+    x.slice(1).reduce((max, distance, i) => {
+      const speed = (3600 * (distance - x[i])) / s;
+      return speed > max ? speed : max;
+    }, 0)
+  );
+}
+ */
+
 // console.log(gps(20, [0.0, 0.23, 0.46, 0.69, 0.92, 1.15, 1.38, 1.61])); //41
 // console.log(gps(17, [0.0, 0.02, 0.44, 0.66, 0.88, 1.1, 1.32, 1.54, 1.76])); //88
 // console.log(gps(19, [0.0])); // 0
