0492c102e77e0a3c0d094e995a1aa0a86a39ee1a
Spierbal <abdulkadir.f.osman@gmail.com>
2023-08-11 22:37:31 +0100

    T

--- 7kyu/Ordered Count of Characters.js
+++ 7kyu/Ordered Count of Characters.js
@@ -11,19 +11,32 @@ function orderedCount(text) {
 /* 
 // Solution 2: Using Object
 function orderedCount(text) {
-  let count = {};
+  let charCount = {};
   const result = [];
 
   for (const char of text) {
-    count[char] = count[char] + 1 || 1;
+    charCount[char] = charCount[char] + 1 || 1;
   }
 
   for (const char of text) {
-    if (count[char]) result.push([char, count[char]]);
-    count[char] = 0;
+    if (charCount[char]) result.push([char, charCount[char]]);
+    charCount[char] = 0;
   }
 
   return result;
+} */
+
+/* 
+// Solution 3: Using reduce() and Map (most efficient)
+function orderedCount(text) {
+  return [
+    ...[...text]
+      .reduce((charMap, char) => {
+        charMap.set(char, (charMap.get(char) || 0) + 1);
+        return charMap;
+      }, new Map())
+      .entries(),
+  ];
 } */
 
 // console.log(orderedCount("abracadabra")); // [['a', 5], ['b', 2], ['r', 2], ['c', 1], ['d', 1]]]
