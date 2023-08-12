fcef07ed1c3b1aa146fc915dad1be85023a6e111
Spierbal <abdulkadir.f.osman@gmail.com>
2023-08-12 23:59:44 +0100

    T

--- 8kyu/A Needle in the Haystack.js
+++ 8kyu/A Needle in the Haystack.js
@@ -3,9 +3,10 @@ const findNeedle = haystack =>
     needles => needles === "needle"
   )}`;
 
+/* 
 // Solution 2: Shorter Solution
-
-/* const findNeedle = haystack =>
-  `found the needle at position ${haystack.indexOf("needle")}`; */
+const findNeedle = haystack =>
+  `found the needle at position ${haystack.indexOf("needle")}`;
+ */
 
 // console.log(findNeedle(haystack_2)) // --> found the needle at position 5
