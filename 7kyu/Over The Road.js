38d63d708c23a4b5bbac128a17c1e8f3d6f3f0d8
Spierbal <abdulkadir.f.osman@gmail.com>
2023-08-18 00:47:55 +0100

    T

--- 7kyu/Over The Road.js
+++ 7kyu/Over The Road.js
@@ -8,42 +8,32 @@ function overTheRoad(address, n) {
   return oppositeHouse;
 } */
 
-// console.log(overTheRoad(1, 3)); // 6
-// console.log(overTheRoad(3, 3)); // 4
-// console.log(overTheRoad(2, 3)); // 5
-// console.log(overTheRoad(3, 5)); // 8
-// console.log(overTheRoad(7, 11)); // 16
+console.log(overTheRoad(1, 1)); // 6
+console.log(overTheRoad(3, 3)); // 4
+console.log(overTheRoad(2, 3)); // 5
+console.log(overTheRoad(3, 5)); // 8
+console.log(overTheRoad(3, 10)); // 16
 // console.log(overTheRoad(23633656673, 310027696726)); // 596421736780
 
-/*
-n=1
-1| |2     
-  
-n=2 
-1| |4    
-3| |2 
+/* 
+n = 1
 
-n=3
-1| |6     
+1| |2
+
+n = 2
+
+1| |4
+3| |2
+
+n = 3
+
+1| |6
 3| |4
 5| |2
-  
-n=4
-1| |8     
-3| |6
-5| |4
-7| |2
 
-n=5
-1| |10
-3| |8
-5| |6
-7| |4
-9| |2
-
-
-
-
+if, n = 3, address = 1 (3 * 2 - 1 + 1 = 6)
+if, n = 3, address = 3 (3 * 2 - 3 + 1 = 4)
+if, n = 3, address = 5 (3 * 2 - 5 + 1 = 2)
 
 
 */
