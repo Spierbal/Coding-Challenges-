2bbe9c99b3fde2622be4c0e6fe1a8dd147b3f393
Spierbal <abdulkadir.f.osman@gmail.com>
2023-08-18 00:49:06 +0100

    T

--- 7kyu/Over The Road.js
+++ 7kyu/Over The Road.js
@@ -12,7 +12,7 @@ console.log(overTheRoad(1, 1)); // 6
 console.log(overTheRoad(3, 3)); // 4
 console.log(overTheRoad(2, 3)); // 5
 console.log(overTheRoad(3, 5)); // 8
-console.log(overTheRoad(3, 10)); // 16
+console.log(overTheRoad(3, 7)); // 16
 // console.log(overTheRoad(23633656673, 310027696726)); // 596421736780
 
 /* 
@@ -34,6 +34,5 @@ n = 3
 if, n = 3, address = 1 (3 * 2 - 1 + 1 = 6)
 if, n = 3, address = 3 (3 * 2 - 3 + 1 = 4)
 if, n = 3, address = 5 (3 * 2 - 5 + 1 = 2)
-
 
 */
