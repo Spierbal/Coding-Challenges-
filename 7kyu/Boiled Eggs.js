8f682b755679913ec5da853993a3591e85d2b04e
Spierbal <abdulkadir.f.osman@gmail.com>
2023-08-18 17:49:30 +0100

    T

---
+++ 7kyu/Boiled Eggs.js
@@ -0,0 +1,9 @@
+const cookingTime = eggs => Math.ceil(eggs / 8) * 5;
+
+// console.log(cookingTime(0)); // 0, '0 eggs'
+// console.log(cookingTime(5)); // 5, '5 eggs'
+// console.log(cookingTime(10)); // 10, '10 eggs'
+// console.log(cookingTime(20)); // 15, '15 eggs'
+// console.log(cookingTime(45)); // 30, '30 eggs'
+// console.log(cookingTime(8553)); // 5350, '3550 eggs'
+// console.log(cookingTime(829)); // 520, '520 eggs'
