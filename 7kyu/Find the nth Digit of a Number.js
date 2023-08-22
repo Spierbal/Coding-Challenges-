f9eed7a493caf311960ceb7f3b021001f216f94f
Spierbal <abdulkadir.f.osman@gmail.com>
2023-08-22 19:07:19 +0100

    T

--- 7kyu/Find the nth Digit of a Number.js
+++ 7kyu/Find the nth Digit of a Number.js
@@ -11,18 +11,22 @@ function findDigit(num, nth) {
 /* 
 // Solution 2: charAt instead of array indexing
 function findDigit(num, nth) {
-  if (nth <= 0) return -1; // nth is not positive
+  // Check if nth is not positive
+  if (nth <= 0) return -1;
 
-  const numStr = String(Math.abs(num)); // convert to string
-  const index = numStr.length - nth; // get index of nth digit from end
+  // Convert negative num to positive and convert to string
+  const numStr = String(Math.abs(num));
+  // Get index of nth digit from end (from right)
+  const index = numStr.length - nth;
 
-  return index >= 0 ? Number(numStr.charAt(index)) : 0; // return digit or 0 if index is out of range
+  // Return the digit at the calculated index
+  return nth <= numStr.length ? Number(numStr.charAt(index)) : 0;
 } */
 
-console.log(findDigit(5673, 4)); // 5
-console.log(findDigit(129, 2)); // 2
-console.log(findDigit(-2825, 3)); // 8
-console.log(findDigit(-456, 4)); // 0
-console.log(findDigit(0, 20)); // 0)
-console.log(findDigit(65, 0)); // -1
-console.log(findDigit(24, -8)); // -1
+// console.log(findDigit(5673, 4)); // 5
+// console.log(findDigit(129, 2)); // 2
+// console.log(findDigit(-2825, 3)); // 8
+// console.log(findDigit(-456, 4)); // 0
+// console.log(findDigit(0, 20)); // 0)
+// console.log(findDigit(65, 0)); // -1
+// console.log(findDigit(24, -8)); // -1
