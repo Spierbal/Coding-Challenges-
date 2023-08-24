cac9f3a82cb5ee295ceadd0f609588e25f8217b7
Spierbal <abdulkadir.f.osman@gmail.com>
2023-08-25 00:53:58 +0100

    T

--- 7kyu/Anagram Detection.js
+++ 7kyu/Anagram Detection.js
@@ -1,32 +1,56 @@
+// Solution 1: object
 function isAnagram(test, original) {
-  return cleanString(test) === cleanString(original);
+  if (test.length !== original.length) return false;
+
+  const charCount = {};
+
+  for (const char of test.toLowerCase()) {
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
+
+  for (const char of original.toLowerCase()) {
+    if (!charCount[char]) return false;
+    charCount[char]--;
+  }
+
+  return true;
 }
 
-function cleanString(str) {
-  return str.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("");
+/* 
+// Solution 2b: Map
+function isAnagram(test, original) {
+  if (test.length !== original.length) return false;
+
+  const charCount = new Map();
+
+  // Count the frequency of each character in the 'test' string
+  for (const char of test.toLowerCase()) {
+    charCount.set(char, charCount.get(char) + 1 || 1);
+  }
+  // Compare character frequencies in the 'original' string with the map
+  for (const char of original.toLowerCase()) {
+    // If a character isn't present in the map or its frequency is already zero, it's not an anagram
+    if (!charCount.get(char)) return false;
+    charCount.set(char, charCount.get(char) - 1);
+  }
+  return true;
 }
-
-// Solution 1b:
-/* const isAnagram = (test, original) => {
-  return (
-    test.toLowerCase().split("").sort().join("") ===
-    original.toLowerCase().split("").sort().join("")
-  );
-}; */
-
-// Solution 3:
-/* function isAnagram(test, original) {
+ */
+/* 
+// Solution 3: helper function & object
+function isAnagram(test, original) {
+  // Build character map for each string
   const charMapA = buildCharMap(test);
   const charMapB = buildCharMap(original);
 
+  // Compare character maps
   if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
     return false;
   }
 
+  // Compare character frequencies in each map
   for (let char in charMapA) {
-    if (charMapA[char] !== charMapB[char]) {
-      return false;
-    }
+    if (charMapA[char] !== charMapB[char]) return false;
   }
   return true;
 }
@@ -34,13 +58,14 @@ function cleanString(str) {
 function buildCharMap(str) {
   const charMap = {};
 
-  for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+  // Count the frequency of each character in the string & ignore non-alphanumeric characters
+  for (const char of str.replace(/[^\w]/g, "").toLowerCase()) {
     charMap[char] = charMap[char] + 1 || 1;
   }
   return charMap;
 } */
 
-//   console.log(isAnagram("foefet", "toffee")) // --> true, 'The word foefet is an anagram of toffee'
-//   console.log(isAnagram("Buckethead", "DeathCubeK")) // --> true, 'The word Buckethead is an anagram of DeathCubeK'
-//   console.log(isAnagram("apple", "pale")) // --> false, 'Same letters, but different count'
-//   console.log(isAnagram("dumble", "bumble")) // --> false, 'Characters do not match for test case dumble, bumble'
+// console.log(isAnagram("foefet", "toffee")); // --> true, 'The word foefet is an anagram of toffee'
+// console.log(isAnagram("Buckethead", "DeathCubeK")); // --> true, 'The word Buckethead is an anagram of DeathCubeK'
+// console.log(isAnagram("apple", "pale")); // --> false, 'Same letters, but different count'
+// console.log(isAnagram("dumble", "bumble")); // --> false, 'Characters do not match for test case dumble, bumble'
