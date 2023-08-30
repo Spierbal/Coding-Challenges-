1d34f9c985d9c675971623bedf250e1b94e3fc2b
Spierbal <abdulkadir.f.osman@gmail.com>
2023-08-30 04:27:29 +0100

    T

--- 7kyu/Sum of array singles.js
+++ 7kyu/Sum of array singles.js
@@ -1,4 +1,4 @@
-// Solution 1: Set
+// Solution 1: Set & for of loop
 function repeats(arr) {
   let uniqueSet = new Set();
   let sum = 0;
@@ -15,20 +15,7 @@ function repeats(arr) {
 }
 
 /* 
-// Solution 2: object & Set
-function repeats(arr) {
-  const freqObj = {};
-  const uniqueSet = new Set();
-
-  for (const num of arr) {
-    freqObj[num] = freqObj[num] + 1 || 1;
-    freqObj[num] === 1 ? uniqueSet.add(num) : uniqueSet.delete(num);
-  }
-  return [...uniqueSet].reduce((a, b) => a + b, 0);
-} */
-
-/* 
-// Solution 3: Map
+// Solution 2: Map & for of loop
 function repeats(arr) {
   const freqMap = new Map();
   let sum = 0;
@@ -36,14 +23,27 @@ function repeats(arr) {
   for (const num of arr) {
     freqMap.set(num, freqMap.get(num) + 1 || 1);
   }
-  for (const [num, count] of freqMap.entries()) {
+  for (const [num, count] of freqMap) {
     if (count === 1) sum += num;
   }
   return sum;
 } */
 
-// console.log(repeats([4, 5, 7, 5, 4, 8])); // 15
-// console.log(repeats([9, 10, 19, 13, 19, 13])); // 19
-// console.log(repeats([16, 0, 11, 4, 8, 16, 0, 11])); // 12
-// console.log(repeats([5, 17, 18, 11, 13, 18, 11, 13])); // 22
-// console.log(repeats([5, 10, 19, 13, 10, 13])); // 24
+/* 
+// Solution 3: object & for of loop
+function repeats(arr) {
+  const count = {};
+  let sum = 0;
+
+  for (const num of arr) {
+    count[num] = count[num] + 1 || 1;
+    count[num] === 1 ? (sum += num) : (sum -= num);
+  }
+  return sum;
+} */
+
+console.log(repeats([4, 5, 7, 5, 4, 8])); // 15
+console.log(repeats([9, 10, 19, 13, 19, 13])); // 19
+console.log(repeats([16, 0, 11, 4, 8, 16, 0, 11])); // 12
+console.log(repeats([5, 17, 18, 11, 13, 18, 11, 13])); // 22
+console.log(repeats([5, 10, 19, 13, 10, 13])); // 24
