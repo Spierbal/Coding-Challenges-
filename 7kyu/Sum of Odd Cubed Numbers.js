6b8701688149356cf377aba10de2c76c92e17736
Spierbal <abdulkadir.f.osman@gmail.com>
2023-09-06 20:16:50 +0100

    T

--- 7kyu/Sum of Odd Cubed Numbers.js
+++ 7kyu/Sum of Odd Cubed Numbers.js
@@ -1,27 +1,28 @@
-// Solution 1: every & reduce
+// Solution 1: reduce (more efficient)
 function cubeOdd(arr) {
-  return arr.every(element => typeof element === "number")
-    ? arr.reduce((sum, num) => (num % 2 !== 0 ? sum + num ** 3 : sum), 0)
-    : undefined;
-}
-
-/* 
-// Solution 1b: reduce (more efficient)
-function cubeOdd(arr) {
-  const sum = arr.reduce((acc, num) => {
+  const sumOdd = arr.reduce((sum, num) => {
     if (typeof num !== "number") {
       return undefined;
     }
     if (num % 2 !== 0) {
-      return acc + num ** 3;
+      return sum + num ** 3;
     }
-    return acc;
+    return sum;
   }, 0);
-  return isNaN(sum) ? undefined : sum;
+  return isNaN(sumOdd) ? undefined : sumOdd;
+}
+
+/* 
+// Solution 2: every & reduce
+function cubeOdd(arr) {
+  const areAllNumbers = arr.every(element => typeof element === "number");
+  if (!areAllNumbers) return undefined;
+
+  return arr.reduce((sum, num) => (num % 2 !== 0 ? sum + num ** 3 : sum), 0);
 } */
 
 /* 
-// Solution 2: for loop & typeof
+// Solution 3: for loop & typeof
 function cubeOdd(arr) {
   let sum = 0;
 
@@ -39,4 +40,4 @@ function cubeOdd(arr) {
 
 // console.log(cubeOdd([1, 2, 3, 4])); // 28
 // console.log(cubeOdd([-3, -2, 2, 3])); // 0
-// console.log(cubeOdd(["a", 12, 9, "z", 42])); // undefined
+// console.log(cubeOdd(["a", "12", "9", "z", "42"])); // undefined
