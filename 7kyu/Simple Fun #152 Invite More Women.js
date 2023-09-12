e83fd2314b4a7d90b235e0916dad90fb43135c25
Spierbal <abdulkadir.f.osman@gmail.com>
2023-09-12 23:55:47 +0100

    T

--- 7kyu/Simple Fun #152 Invite More Women.js
+++ 7kyu/Simple Fun #152 Invite More Women.js
@@ -1,10 +1,17 @@
+// Solution 1: reduce
+const inviteMoreWomen = L => L.reduce((total, gender) => total + gender, 0) > 0;
+
+/* 
+// Solution 2: for loop
 function inviteMoreWomen(L) {
-  return L.map(e => e);
-}
-console.log(inviteMoreWomen([1, -1, 1])); //true
+  let total = 0;
+  for (const gender of L) {
+    total += gender;
+  }
+  return total > 0;
+} */
 
-console.log(inviteMoreWomen([1, 1, 1])); //true
-
-console.log(inviteMoreWomen([-1, -1, -1])); //false
-
-console.log(inviteMoreWomen([1, -1])); //false
+// console.log(inviteMoreWomen([1, -1, 1])); //true
+// console.log(inviteMoreWomen([1, 1, 1])); //true
+// console.log(inviteMoreWomen([-1, -1, -1])); //false
+// console.log(inviteMoreWomen([1, -1])); //false
