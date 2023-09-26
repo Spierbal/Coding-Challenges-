56cf1268e9fbd4a264cdba64204e3c65c2b6e212
Spierbal <abdulkadir.f.osman@gmail.com>
2023-09-26 10:41:15 +0100

    T

--- 7kyu/Two Oldest Ages.js
+++ 7kyu/Two Oldest Ages.js
@@ -1,29 +1,26 @@
-const twoOldestAges = ages => [...ages].sort((a, b) => a - b).slice(-2);
+// Solution 1: for of loop with destructuring assignment
+// function twoOldestAges(ages) {
+//   let [oldestAge, secondOldestAge] = [-Infinity, -Infinity];
+
+//   for (const age of ages) {
+//     if (age > oldestAge) {
+//       [oldestAge, secondOldestAge] = [age, oldestAge];
+//     } else if (age > secondOldestAge) secondOldestAge = age;
+//   }
+//   return [secondOldestAge, oldestAge];
+// }
 
 /* 
-// Solution 2: Math.max & spread operator & filter
+// Solution 2: sort & slice
+const twoOldestAges2 = ages => [...ages].sort((a, b) => a - b).slice(-2); */
+
+/* 
+// Solution 3: Math.max & spread operator & filter
 const twoOldestAges = ages => {
   const max = Math.max(...ages);
   const secondMax = Math.max(...ages.filter(age => age !== max));
   return [secondMax, max];
 }; */
-
-/* 
-// Solution 3: for loop (most efficient)
-function twoOldestAges(ages) {
-  let oldestAge = -Infinity;
-  let secondOldestAge = -Infinity;
-
-  for (const age of ages) {
-    if (age > oldestAge) {
-      secondOldestAge = oldestAge;
-      oldestAge = age;
-    } else if (age > secondOldestAge) {
-      secondOldestAge = age;
-    }
-  }
-  return [secondOldestAge, oldestAge];
-} */
 
 // console.log(twoOldestAges([1, 5, 87, 45, 8, 8])); // [45, 87];
 // console.log(twoOldestAges([6, 5, 83, 5, 3, 18])); // [18, 83];
