369ac62a0c8982693f31f8ed124d2723bc8e097b
Spierbal <abdulkadir.f.osman@gmail.com>
2023-10-02 19:20:54 +0100

    T

--- 7kyu/Find the divisors.js
+++ 7kyu/Find the divisors.js
@@ -1,3 +1,15 @@
+// Solution  1
+function divisors(integer) {
+  const divisor = [];
+
+  for (let i = 2; i <= integer / 2; i++) {
+    if (integer % i === 0) divisor.push(i);
+  }
+  return divisor.length > 0 ? divisor : `${integer} is prime`;
+}
+
+/* 
+// Solution 1b: for loop (less efficient than solution 1 because it iterates over more numbers)
 function divisors(integer) {
   const divisor = [];
 
@@ -5,7 +17,7 @@ function divisors(integer) {
     if (integer % i === 0) divisor.push(i);
   }
   return divisor.length > 0 ? divisor : `${integer} is prime`;
-}
+} */
 
 // Solution 2:
 /* function divisors(int) {
@@ -16,7 +28,7 @@ function divisors(integer) {
   return arr.length > 0 ? arr : `${int} is prime`;
 } */
 
-/* console.log(divisors(15)); // -->  [3, 5];
-console.log(divisors(12)); // --> [2, 3, 4, 6];
-console.log(divisors(13)); // --> "13 is prime"
-console.log(divisors(25)); // --> "[5]" */
+// console.log(divisors(15)); // -->  [3, 5];
+// console.log(divisors(12)); // --> [2, 3, 4, 6];
+// console.log(divisors(13)); // --> "13 is prime"
+// console.log(divisors(25)); // --> "[5]"
