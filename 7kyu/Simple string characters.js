508ced34792ebf8ae732c90eec2721fbb30a3287
Spierbal <abdulkadir.f.osman@gmail.com>
2023-11-01 01:34:47 +0000

    T

--- 7kyu/Simple string characters.js
+++ 7kyu/Simple string characters.js
@@ -46,6 +46,7 @@ function solve(s) {
   );
 } */
 
+/* 
 // Solution 4: regex & match()
 function solve(s) {
   const uppercaseCount = (s.match(/[A-Z]/g) || []).length;
@@ -53,13 +54,13 @@ function solve(s) {
   const numCount = (s.match(/\d/g) || []).length;
   const specialCharCount = (s.match(/[^A-Za-z0-9]/g) || []).length;
   return [uppercaseCount, lowercaseCount, numCount, specialCharCount];
-}
+} */
 
-console.log(solve("")); // [0,0,0,0]);
-console.log(solve("aAbBcC")); // [3,3,0,0]);
-console.log(solve("Codewars@codewars123.com")); // [1,18,3,2]);
-console.log(solve("bgA5<1d-tOwUZTS8yQ")); // [7,6,3,2]);
-console.log(solve("P*K4%>mQUDaG$h=cx2?.Czt7!Zn16p@5H")); // [9,9,6,9]);
-console.log(solve("RYT'>s&gO-.CM9AKeH?,5317tWGpS<*x2ukXZD")); // [15,8,6,9]);
-console.log(solve("$Cnl)Sr<7bBW-&qLHI!mY41ODe")); // [10,7,3,6])
-console.log(solve("@mw>0=QD-iAx!rp9TaG?o&M%l$34L.nbft")); // [7,13,4,10]);
+// console.log(solve("")); // [0,0,0,0]);
+// console.log(solve("aAbBcC")); // [3,3,0,0]);
+// console.log(solve("Codewars@codewars123.com")); // [1,18,3,2]);
+// console.log(solve("bgA5<1d-tOwUZTS8yQ")); // [7,6,3,2]);
+// console.log(solve("P*K4%>mQUDaG$h=cx2?.Czt7!Zn16p@5H")); // [9,9,6,9]);
+// console.log(solve("RYT'>s&gO-.CM9AKeH?,5317tWGpS<*x2ukXZD")); // [15,8,6,9]);
+// console.log(solve("$Cnl)Sr<7bBW-&qLHI!mY41ODe")); // [10,7,3,6])
+// console.log(solve("@mw>0=QD-iAx!rp9TaG?o&M%l$34L.nbft")); // [7,13,4,10]);
