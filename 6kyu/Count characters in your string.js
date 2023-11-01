f66c47fa4ce8668ba10483da63b66c21a80430b5
Spierbal <abdulkadir.f.osman@gmail.com>
2023-11-01 19:34:14 +0000

    T

--- 6kyu/Count characters in your string.js
+++ 6kyu/Count characters in your string.js
@@ -1,3 +1,4 @@
+// Solution 1: for of loop & object
 function count(str) {
   const counts = {};
 
@@ -7,21 +8,34 @@ function count(str) {
   return counts;
 }
 
-// Solution 2:
-// function count(str) {
-//   const counts = {};
-//   str.split("").forEach(s => {
-//     counts[s] ? counts[s]++ : (counts[s] = 1);
-//   });
-//   return counts;
-// }
+/* 
+// Solution 1b: for of loop & object
+function count(str) {
+  const counts = {};
+  for (const char of str) {
+    if (counts[char]) counts[char]++;
+    else counts[char] = 1;
+  }
+  return counts;
+} */
 
-// Solution 3:
-// function count(str) {
-//   return str.split("").reduce((counts, char) => {
-//     counts[char] = (counts[char] || 0) + 1;
-//     return counts;
-//   }, {});
-// }
+/* 
+// Solution 2: reduce
+function count(str) {
+  return str.split("").reduce((counts, char) => {
+    counts[char] = (counts[char] || 0) + 1;
+    return counts;
+  }, {});
+} */
 
-// console.log(count("aba")); // --> { a: 2, b: 1 }
+/* 
+// Solution 3: forEach
+function count(str) {
+  const counts = {};
+  str.split("").forEach(s => {
+    counts[s] ? counts[s]++ : (counts[s] = 1);
+  });
+  return counts;
+} */
+
+console.log(count("aba")); // --> { a: 2, b: 1 }
