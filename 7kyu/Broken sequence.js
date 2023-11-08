5cfd851e4963491b987332c3270d254c53242515
Spierbal <abdulkadir.f.osman@gmail.com>
2023-11-08 23:44:30 +0000

    T

--- 7kyu/Broken sequence.js
+++ 7kyu/Broken sequence.js
@@ -25,11 +25,11 @@ function findMissingNumber(sequence) {
   return 0;
 } */
 
-console.log(findMissingNumber("1 2 3 5")); // 4 ("It must work for missing middle terms")
-console.log(findMissingNumber("1 3")); // 2 ("It must work for missing middle terms")
-console.log(findMissingNumber("1 5")); // 2 ("It must work for missing more than one number")
-console.log(findMissingNumber("")); // 0 ("It must return 0 for an empty sequence")
-console.log(findMissingNumber("1 2 3 4 5")); // 0 ("It must return 0 if no number is missing")
-console.log(findMissingNumber("2 3 4 5")); // 1 ("It must return 1 for a sequence missing the first element")
-console.log(findMissingNumber("2 6 4 5 3")); // 1 ("It must work for a shuffled input")
-console.log(findMissingNumber("2 1 4 3 a")); // 1 ("It must return 1 for an invalid sequence")
+// console.log(findMissingNumber("1 2 3 5")); // 4 ("It must work for missing middle terms")
+// console.log(findMissingNumber("1 3")); // 2 ("It must work for missing middle terms")
+// console.log(findMissingNumber("1 5")); // 2 ("It must work for missing more than one number")
+// console.log(findMissingNumber("")); // 0 ("It must return 0 for an empty sequence")
+// console.log(findMissingNumber("1 2 3 4 5")); // 0 ("It must return 0 if no number is missing")
+// console.log(findMissingNumber("2 3 4 5")); // 1 ("It must return 1 for a sequence missing the first element")
+// console.log(findMissingNumber("2 6 4 5 3")); // 1 ("It must work for a shuffled input")
+// console.log(findMissingNumber("2 1 4 3 a")); // 1 ("It must return 1 for an invalid sequence")
