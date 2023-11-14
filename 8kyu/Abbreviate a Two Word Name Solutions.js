0517489b4c35cc7bfbf65c70563cf8632e1d6ede
Spierbal <abdulkadir.f.osman@gmail.com>
2023-11-14 01:02:52 +0000

    T

--- 8kyu/Abbreviate a Two Word Name Solutions.js
+++ 8kyu/Abbreviate a Two Word Name Solutions.js
@@ -1,15 +1,30 @@
-// Solution 1: array methods( map, split, join, toUpperCase)
-const abbrevName = name =>
+// Solution 1: map, split
+const abbrevName2 = name =>
   name
     .split(" ")
-    .map(x => x[0].toUpperCase())
+    .map(word => word[0].toUpperCase())
     .join(".");
 
 /* 
-// Solution 2:  
-function abbrevName(name) {
+// Solution 2: split
+function abbrevName1(name) {
+  const [firstName, lastName] = name.split(" ");
+  return `${firstName[0].toUpperCase()}.${lastName[0].toUpperCase()}`;
+} */
+
+/* 
+// Solution 3:
+function abbrevName3(name) {
   const initial = name.toUpperCase().split(" ");
   return `${initial[0][0]}.${initial[1][0]}`;
 } */
 
-// console.log(abbrevName("Ben Gorden")); // "B.G"
+/* 
+// solution 4: regex
+const abbrevName4 = name => name.match(/\b\w/g).join(".").toUpperCase(); */
+
+console.log(abbrevName("Ben Gorden")); // "B.G"
+console.log(abbrevName("Sam Harris")); // "S.H"
+console.log(abbrevName("Evan Cole")); // "E.C"
+console.log(abbrevName("P Favuzzi")); // "P.F"
+console.log(abbrevName("D Mendieta")); // "D.M"
