7a4140a9de2a13a8e10dfedfa863dcd2d4131bbf
Spierbal <abdulkadir.f.osman@gmail.com>
2023-11-16 23:59:03 +0000

    T

--- 8kyu/All Star Code Challenge #18.js
+++ 8kyu/All Star Code Challenge #18.js
@@ -1,21 +1,20 @@
-// const strCount = (str, letter) => [...str].filter(l => l === letter).length;
-
-//Solution 2:
-// const strCount = (str, letter) => str.split(letter).length - 1;
+// Solution !: filter
+const strCount = (str, letter) => [...str].filter(l => l === letter).length;
 
 /* 
-//Solution 3:
-const strCount = (str, letter) =>
-  str.split("").reduce((a, b) => a + letter.includes(b), 0);
+//Solution 2: split
+const strCount = (str, letter) => str.split(letter).length - 1;
  */
 
 /* 
-//Solution 4:
+//Solution 3: reduce
 const strCount = (str, letter) =>
   [...str].reduce((count, char) => (char === letter ? count + 1 : count), 0);
  */
 
+/* 
+//Solution 3b: reduce
 const strCount = (str, letter) =>
-  [...str].reduce((count, char) => count + letter.includes(char), 0);
+  [...str].reduce((count, char) => count + letter.includes(char), 0); */
 
-console.log(strCount("Hello", "l")); //--> 2 (there are two occurences of "l")
+// console.log(strCount("Hello", "l")); //--> 2 (there are two occurences of "l")
