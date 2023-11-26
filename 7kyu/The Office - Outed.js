610e9954ef44e1d7a6258b667b6a32cd52de2e43
Spierbal <abdulkadir.f.osman@gmail.com>
2023-11-26 22:05:11 +0000

    T

--- 7kyu/The Office - Outed.js
+++ 7kyu/The Office - Outed.js
@@ -1,13 +1,27 @@
+// Solution 1: reduce
+function outed(meet, boss) {
+  const totalScore = Object.keys(meet).reduce(
+    (sum, member) => sum + (member === boss ? meet[member] * 2 : meet[member]),
+    0
+  );
+  const avgScore = totalScore / Object.keys(meet).length;
+  return avgScore <= 5 ? "Get Out Now!" : "Nice Work Champ!";
+}
+
+/* 
 function outed(meet, boss) {
   let totalSum = 0;
+  let totalMembers = 0;
 
-  for (let key in meet) {
-    totalSum += meet[key];
-    if (totalSum <= 5) return "Get Out Now!";
-    else return "Get Out Now!";
+  for (const member of Object.keys(meet)) {
+    totalSum += member === boss ? meet[member] * 2 : meet[member];
+    totalMembers++;
   }
-  return totalSum;
-}
+  const avgScore = totalSum / totalMembers;
+
+  return avgScore <= 5 ? "Get Out Now!" : "Nice Work Champ!";
+} */
+/* 
 console.log(
   outed(
     {
@@ -27,56 +41,60 @@ console.log(
   )
 ); // 'Get Out Now!'
 
-// console.log(
-//   outed(
-//     {
-//       tim: 1,
-//       jim: 3,
-//       randy: 9,
-//       sandy: 6,
-//       andy: 7,
-//       katie: 6,
-//       laura: 9,
-//       saajid: 9,
-//       alex: 9,
-//       john: 9,
-//       mr: 8,
-//     },
-//     "katie"
-//   )
-// ); //, 'Nice Work Champ!');
-// console.log(
-//   outed(
-//     {
-//       tim: 2,
-//       jim: 4,
-//       randy: 0,
-//       sandy: 5,
-//       andy: 8,
-//       katie: 6,
-//       laura: 2,
-//       saajid: 2,
-//       alex: 3,
-//       john: 2,
-//       mr: 8,
-//     },
-//     "john"
-//   )
-// ); // 'Get Out Now!'
+console.log(
+  outed(
+    {
+      tim: 1,
+      jim: 3,
+      randy: 9,
+      sandy: 6,
+      andy: 7,
+      katie: 6,
+      laura: 9,
+      saajid: 9,
+      alex: 9,
+      john: 9,
+      mr: 8,
+    },
+    "katie"
+  )
+); //, 'Nice Work Champ!');
 
-// outed(
-//   {
-//     tim: 0,
-//     jim: 2,
-//     randy: 0,
-//     sandy: 7,
-//     andy: 0,
-//     katie: 5,
-//     laura: 1,
-//     saajid: 2,
-//     alex: 3,
-//     john: 2,
-//     mr: 0,
-//   },
-//   "laura"
-// ); // 'Get Out Now!'
+console.log(
+  outed(
+    {
+      tim: 2,
+      jim: 4,
+      randy: 0,
+      sandy: 5,
+      andy: 8,
+      katie: 6,
+      laura: 2,
+      saajid: 2,
+      alex: 3,
+      john: 2,
+      mr: 8,
+    },
+    "john"
+  )
+); // 'Get Out Now!'
+
+console.log(
+  outed(
+    {
+      tim: 0,
+      jim: 2,
+      randy: 0,
+      sandy: 7,
+      andy: 0,
+      katie: 5,
+      laura: 1,
+      saajid: 2,
+      alex: 3,
+      john: 2,
+      mr: 0,
+    },
+    "laura"
+  )
+); // 'Get Out Now!'
+ */
