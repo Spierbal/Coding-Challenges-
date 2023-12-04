da0a546b1814ff7878b32e445d1771ac5f25bbc0
Spierbal <abdulkadir.f.osman@gmail.com>
2023-12-04 12:46:18 +0000

    T

--- 7kyu/A Rule of Divisibility by 7.js
+++ 7kyu/A Rule of Divisibility by 7.js
@@ -1,22 +1,22 @@
-// Solution 1: while loop
-function seven(m) {
-  let counter = 0;
-
-  while (String(m).length > 2) {
-    m = String(m).slice(0, -1) - 2 * String(m).slice(-1);
-    counter++;
-  }
-  return [m, counter];
-}
-
-/* 
-Solution 1b: while loop with Math operations
+// Solution 1: while loop with Math operations
 function seven(m) {
   let counter = 0;
 
   while (m > 99) {
     // Remove last digit and subtract 2 times the last digit
     m = Math.floor(m / 10) - 2 * (m % 10);
+    counter++;
+  }
+  return [m, counter];
+}
+
+/* 
+// Solution 1b: while loop
+function seven(m) {
+  let counter = 0;
+
+  while (String(m).length > 2) {
+    m = String(m).slice(0, -1) - 2 * String(m).slice(-1);
     counter++;
   }
   return [m, counter];
@@ -29,7 +29,6 @@ function seven(m) {
 
   for (; m > 99; counter++) {
     m = Math.floor(m / 10) - 2 * (m % 10);
-    counter++;
   }
   return [m, counter];
 } */
