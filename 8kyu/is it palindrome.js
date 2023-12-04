d054708b27686b25e8807c705ba764573f1911cc
Spierbal <abdulkadir.f.osman@gmail.com>
2023-12-04 23:49:57 +0000

    T

--- 8kyu/is it palindrome.js
+++ 8kyu/is it palindrome.js
@@ -1,17 +1,35 @@
+// Solution 1:
 const isPalindrome = x =>
-  [...x.toLowerCase()].reverse().join("") === x.toLowerCase();
+  x.toLowerCase() === [...x].reverse().join("").toLowerCase();
 
-// Solution 1b:
-// const isPalindrome = x =>
-//   x.toLowerCase() === [...x].reverse().join("").toLowerCase();
+/* 
+//Solution 2:
+const isPalindrome = x =>
+	x.toLowerCase() === x.split("").reverse().join("").toLowerCase(); */
 
-//solution 2:
-// const isPalindrome = x =>
-//   x.split("").reverse().join("").toLowerCase() === x.toLowerCase();
+//Solution 3: regex
+// const isPalindrome = x => {
+//   cleanedStr = x.toLowerCase().replace(/[^a-z0-9]/g, "");
+//   return cleanedStr === [...cleanedStr].reverse().join("");
+// };
 
-//Solution 2b:
-// const isPalindrome = x =>
-// 	x.toLowerCase() === x.split("").reverse().join("").toLowerCase();
+/* 
+// Solution 4: Two-Pointer
+function isPalindrome(x) {
+  const formattedStr = x.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+  let left = 0;
+  let right = formattedStr.length - 1;
+
+  while (left < right) {
+    if (formattedStr[left] !== formattedStr[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
+} */
 
 // console.log(isPalindrome("Aba")); // --> true
 // console.log(isPalindrome("aba")); // --> true
