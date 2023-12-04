2c2763dbf22da559caaeb2494df76b7949ff8306
Spierbal <abdulkadir.f.osman@gmail.com>
2023-12-04 17:47:56 +0000

    T

--- 8kyu/Find Maximum and Minimum Values of a List.js
+++ 8kyu/Find Maximum and Minimum Values of a List.js
@@ -1,10 +1,19 @@
-// Solution 1:
-const min = list => list.reduce((a, b) => Math.min(a, b));
-const max = list => list.reduce((a, b) => Math.max(a, b));
+// Solution 1: Math.min/Math.max
+const min = list => Math.min(...list);
+const max = list => Math.max(...list);
 
-// Solution 2:
-// const min = list => Math.min(...list);
-// const max = list => Math.max(...list);
+/* 
+// Solution 2: reduce
+const min = list => list.reduce((a, b) => Math.min(a, b));
+const max = list => list.reduce((a, b) => Math.max(a, b)); 
+*/
 
 // console.log(min([-52, 56, 30, 29, -54, 0, -110])); // --> -110
+// console.log(min([42, 54, 65, 87, 0])); // --> 0
+// console.log(min([1, 2, 3, 4, 5, 10])); // --> 1
+// console.log(min([-1, -2, -3, -4, -5, -10])); // --> -10
+
 // console.log(max([4, 6, 2, 1, 9, 63, -134, 566])); // --> 566
+// console.log(max([-52, 56, 30, 29, -54, 0, -110])); //  -->56
+// console.log(max([5])); // --> 5
+// console.log(max([534, 43, 2, 1, 3, 4, 5, 5, 443, 443, 555, 555])); //  --> 555
--- 8kyu/Sum without highest and lowest number.js
+++ 8kyu/Sum without highest and lowest number.js
@@ -1,3 +1,49 @@
+// Solution 1: reduce, Math.max, Math.min
+function sumArray(array) {
+  if (!Array.isArray(array)) return 0;
+
+  const max = Math.max(...array);
+  const min = Math.min(...array);
+
+  return array.reduce((a, b) => a + b, 0) - max - min;
+}
+
+/* 
+// Solution 2: for loop with Math.max, Math.min
+function sumArray(array) {
+  if (!Array.isArray(array) || array.length < 3) return 0;
+
+  let sum = 0;
+  let [max, min] = [array[0], array[0]];
+
+  for (let num of array) {
+    sum += num;
+    max = Math.max(max, num);
+    min = Math.min(min, num);
+  }
+
+  return sum - min - max;
+} */
+
+/* 
+// Solution 3: for of loop 
+function sumArray1(array) {
+  if (!Array.isArray(array) || array.length < 3) return 0;
+
+  let sum = 0;
+  let [max, min] = [array[0], array[0]];
+
+  for (const num of array) {
+    sum += num;
+    if (num > max) max = num;
+    if (num < min) min = num;
+  }
+  return sum - max - min;
+}
+ */
+
+/* 
+// Solution 4: sort & reduce
 function sumArray(array) {
   if (!Array.isArray(array)) return 0;
 
@@ -5,27 +51,13 @@ function sumArray(array) {
     .sort((a, b) => a - b)
     .slice(1, array.length - 1)
     .reduce((a, b) => a + b, 0);
-}
+} */
 
-//////// Solution 2://////////
-
-// function sumArray(array) {
-//   if (!Array.isArray(array)) return 0;
-
-//   const max = Math.max(...array);
-//   const min = Math.min(...array);
-
-//   return array.reduce((a, b) => a + b, 0) - max - min;
-// }
-
-///////// Solution 3:///////////
-
-// function sumArray(array) {
-//   const sum = (a, b) => a + b;
-
-//   return Array.isArray(array) && array.length > 1
-//     ? array.reduce(sum, 0) - Math.min(...array) - Math.max(...array)
-//     : 0;
-// }
-
-// console.log(sumArray([0, 1, 6, 10, 10])) // --> 17
+console.log(sumArray(null)); //  --> 0
+console.log(sumArray([])); // --> 0)
+console.log(sumArray([3])); // --> 0)
+console.log(sumArray([3, 5])); // --> 0
+console.log(sumArray([6, 2, 1, 8, 10])); // --> 16
+console.log(sumArray([0, 1, 6, 10, 10])); // --> 17
+console.log(sumArray([-6, -20, -1, -10, -12])); // --> -28
+console.log(sumArray([-6, 20, -1, 10, -12])); // --> 3
