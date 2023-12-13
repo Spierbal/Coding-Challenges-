90d0011ebdd890f53949dbc2788ffc141ea2bb6c
Spierbal <abdulkadir.f.osman@gmail.com>
2023-12-13 23:59:36 +0000

    T

---
+++ 7kyu/Array element parity.js
@@ -0,0 +1,42 @@
+// Solution 1: Set
+function solve(arr) {
+  const uniqueValues = new Set(arr);
+
+  for (const num of uniqueValues) {
+    if (!uniqueValues.has(-num)) return num;
+  }
+}
+
+/* 
+// Solution 2: for of loop
+function solve(arr) {
+  for (const num of arr) {
+    if (!arr.includes(-num)) return num;
+  }
+  return num;
+} */
+
+/* 
+// Solution 2b: for loop
+function solve(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (!arr.includes(-arr[i])) return arr[i];
+  }
+} */
+
+// Solution 3: find()
+// const solve = array => array.find(num => !array.includes(-num));
+
+// Solution 4: filter()
+// const solve4 = array => array.filter(num => !array.includes(-num))[0];
+
+/* 
+// Solution 5: reduce()
+const solve5 = array =>
+  array.reduce((acc, num) => (array.includes(-num) ? acc : num), 0); */
+
+// console.log(solve([1, -1, 2, -2, 3])); // 3
+// console.log(solve([-3, 1, 2, 3, -1, -4, -2])); // -4
+// console.log(solve([1, -1, 2, -2, 3, 3])); // 3
+// console.log(solve([-110, 110, -38, -38, -62, 62, -38, -38, -38])); // -38
+// console.log(solve([-9, -105, -9, -9, -9, -9, 105])); // -9
--- 7kyu/Find Count of Most Frequent Item in an Array.js
+++ 7kyu/Find Count of Most Frequent Item in an Array.js
@@ -10,31 +10,9 @@ function mostFrequentItemCount(collection) {
   return Math.max(...count.values());
 }
 
-// Solution 2: Reduce()
-/* function mostFrequentItemCount(collection) {
-  if (!collection.length) return 0;
-
-  const count = collection.reduce((acc, item) => {
-    acc[item] = acc[item] + 1 || 1;
-    return acc;
-  }, {});
-  return Math.max(...Object.values(count));
-} */
-
-// Solution 3: forEach()
-/* function mostFrequentItemCount(collection) {
-  if (!collection.length) return 0;
-
-  const count = {};
-
-  collection.forEach(item => {
-    count[item] = count[item] + 1 || 1;
-  });
-  return Math.max(...Object.values(count));
-} */
-
-// Solution 4: for of loop
-/* function mostFrequentItemCount(collection) {
+/* 
+// Solution 2: Object and for of loop
+function mostFrequentItemCount(collection) {
   if (!collection.length) return 0;
   const count = {};
 
@@ -44,7 +22,31 @@ function mostFrequentItemCount(collection) {
   return Math.max(...Object.values(count));
 } */
 
+/* 
+// Solution 3: Reduce()
+function mostFrequentItemCount(collection) {
+  if (!collection.length) return 0;
+
+  const count = collection.reduce((acc, item) => {
+    acc[item] = acc[item] + 1 || 1;
+    return acc;
+  }, {});
+  return Math.max(...Object.values(count));
+}  */
+
+/* 
+// Solution 4: Object & forEach()
+function mostFrequentItemCount(collection) {
+  if (!collection.length) return 0;
+
+  const count = {};
+
+  collection.forEach(item => {
+    count[item] = count[item] + 1 || 1;
+  });
+  return Math.max(...Object.values(count));
+} 
+ */
+
 // console.log(mostFrequentItemCount([3, -1, -1])); // 2 (there are two occurrences of -1)
-// console.log(
-//   mostFrequentItemCount([3, -1, -1, -1, 2, 3, -1, 3, -1, 2, 4, 9, 3])
-// ); // 5 (there are 5 occurrences of -1)
+// console.log(mostFrequentItemCount([3, -1, -1, -1, 3, -1, 3, -1, 9, 3])); // 5 (there are 5 occurrences of -1)
