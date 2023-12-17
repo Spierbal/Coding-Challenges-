d213c83f31b91de6ff951d96a97a49d0fae0f640
Spierbal <abdulkadir.f.osman@gmail.com>
2023-12-17 01:34:36 +0000

    T

--- 6kyu/Find the unique number.js
+++ 6kyu/Find the unique number.js
@@ -2,13 +2,15 @@
 const findUniq = arr =>
   arr.find(num => arr.indexOf(num) === arr.lastIndexOf(num));
 
+/* 
 // Solution 2: for of loop
 function findUniq(arr) {
   for (const num of arr) {
     if (arr.indexOf(num) === arr.lastIndexOf(num)) return num;
   }
-}
+} */
 
+/* 
 // Solution 2b: for loop
 function findUniq(arr) {
   for (let i = 0; i < arr.length; i++) {
@@ -16,15 +18,17 @@ function findUniq(arr) {
       return arr[i];
     }
   }
-}
+} */
 
-// Solution 2: filter()
+/* 
+// Solution 3: filter()
 const findUniq = arr =>
   arr.filter(num => arr.indexOf(num) === arr.lastIndexOf(num))[0];
+ */
 
-console.log(findUniq([1, 0, 0])); // 1
-console.log(findUniq([0, 1, 0])); // 1
-console.log(findUniq([0, 0, 1])); // 1
-console.log(findUniq([1, 1, 1, 2, 1, 1])); // 2
-console.log(findUniq([1, 1, 2, 1, 1])); // 2
-console.log(findUniq([3, 10, 3, 3, 3])); // 10
+// console.log(findUniq([1, 0, 0])); // 1
+// console.log(findUniq([0, 1, 0])); // 1
+// console.log(findUniq([0, 0, 1])); // 1
+// console.log(findUniq([1, 1, 1, 2, 1, 1])); // 2
+// console.log(findUniq([1, 1, 2, 1, 1])); // 2
+// console.log(findUniq([3, 10, 3, 3, 3])); // 10
