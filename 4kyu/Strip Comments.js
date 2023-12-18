7a72877ee6fdbfe2c8c9339807355dbd1300878a
Spierbal <abdulkadir.f.osman@gmail.com>
2023-12-18 21:31:51 +0000

    T

--- 4kyu/Strip Comments.js
+++ 4kyu/Strip Comments.js
@@ -1,25 +1,21 @@
 const solution = (input, markers) =>
   input.replace(new RegExp(`\\s*[${markers.join("")}].+`, "g"), "");
 
-// Solution 2:
-// function solution(input, markers) {
-//   return input
-//     .split("\n")
-//     .map(s => markers.reduce((a, b) => a.split(b)[0].trim(), s))
-//     .join("\n");
-// }
+/* 
+// Solution 2: 
+function solution(input, markers) {
+  return input
+    .split("\n")
+    .map(s => markers.reduce((a, b) => a.split(b)[0].trim(), s))
+    .join("\n");
+} */
 
-// console.log(solution("Q @b\nu\ne -e f g", ["@", "-"]));
-// //"Q
-// //u
-// //e"
+// console.log(solution("Q @b\nu\ne -e f g", ["@", "-"])); // "Q u e"
 
-// console.log(
-//   solution("apples, plums % and bananas\npears\noranges !applesauce", [
-//     "%",
-//     "!",
-//   ])
-// );
-/* "apples, plums
-pears
-oranges" */
+/* 
+console.log(
+  solution("apples, plums % and bananas\npears\noranges !applesauce", [
+    "%",
+    "!",
+  ])
+); // "apples, plums pears oranges" */
