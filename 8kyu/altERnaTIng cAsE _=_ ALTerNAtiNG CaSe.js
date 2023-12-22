9cd237930c291aadc87776e301670e1c5beadf1c
Spierbal <abdulkadir.f.osman@gmail.com>
2023-12-22 21:45:18 +0000

    T

--- 8kyu/altERnaTIng cAsE _=_ ALTerNAtiNG CaSe.js
+++ 8kyu/altERnaTIng cAsE _=_ ALTerNAtiNG CaSe.js
@@ -1,17 +1,27 @@
+// Solution 1:
 String.prototype.toAlternatingCase = function () {
   return [...this]
     .map(c => (c === c.toUpperCase() ? c.toLowerCase() : c.toUpperCase()))
     .join("");
 };
 
-// Solution 2:
-// String.prototype.toAlternatingCase = function () {
-//   return this.replace(/[A-z]/g, char =>
-//     /[a-z]/.test(char) ? char.toUpperCase() : char.toLowerCase()
-//   );
-// };
+/* 
+// Solution 2: regex
+String.prototype.toAlternatingCase = function () {
+  return this.replace(/[A-z]/g, c =>
+    c === c.toUpperCase() ? c.toLowerCase() : c.toUpperCase()
+  );
+}; */
 
-//  console.log("hello world".toAlternatingCase()) // --> HELLO WORLD
-//  console.log("HELLO WORLD".toAlternatingCase()) // --> hello world
-//  console.log("hello WORLD".toAlternatingCase()) // --> HELLO world
-//  console.log("HeLLo WoRLD".toAlternatingCase()) // --> hEllO wOrld
+/* 
+// Solution 2b: regex
+String.prototype.toAlternatingCase = function () {
+  return this.replace(/[A-z]/g, char =>
+    /[a-z]/.test(char) ? char.toUpperCase() : char.toLowerCase()
+  );
+}; */
+
+// console.log("hello world".toAlternatingCase()); // --> HELLO WORLD
+// console.log("HELLO WORLD".toAlternatingCase()); // --> hello world
+// console.log("hello WORLD".toAlternatingCase()); // --> HELLO world
+// console.log("HeLLo WoRLD".toAlternatingCase()); // --> hEllO wOrld
