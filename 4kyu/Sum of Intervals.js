ac498cd25f39685334909f8c918aa3e9bef1dbb7
Spierbal <abdulkadir.f.osman@gmail.com>
2023-12-27 23:59:35 +0000

    T

---
+++ 4kyu/Sum of Intervals.js
@@ -0,0 +1,48 @@
+function sumIntervals(intervals) {
+  intervals.sort((a, b) => a[1] - b[1]);
+
+  let result = 0,
+    current = intervals[0];
+
+  for (let i = 1; i < intervals.length; i++) {
+    const next = intervals[i];
+
+    if (current[1] >= next[0]) {
+      current[1] = Math.max(current[1], next[1]);
+    } else {
+      result += current[1] - current[0];
+      current = next;
+    }
+  }
+
+  return result + current[1] - current[0];
+}
+
+/* 
+// non-overlapping intervals
+// console.log(sumIntervals([[1, 5]])); // 4
+console.log(
+  sumIntervals([
+    [1, 5],
+    [6, 10],
+  ])
+); // 8  (5-1) + (10-6) = 4 + 4 = 8
+ */
+
+/* 
+// overlapping intervals
+console.log(
+  sumIntervals([
+    [1, 5],
+    [1, 5],
+  ])
+); // 4 (5-1) = 4
+
+console.log(
+  sumIntervals([
+    [1, 4],
+    [7, 10],
+    [3, 5],
+  ])
+); // 7 (4-1) + (10-7) = 3 + 4 = 7
+ */
