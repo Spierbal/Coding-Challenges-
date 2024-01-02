60fbea69173fdbec73142a8e53ea465794b0974c
Spierbal <abdulkadir.f.osman@gmail.com>
2024-01-02 21:55:52 +0000

    T

--- 6kyu/Tribonacci Sequence.js
+++ 6kyu/Tribonacci Sequence.js
@@ -3,7 +3,8 @@ function tribonacci(signature, n) {
   let sequence = [...signature];
 
   for (let i = 3; i < n; i++) {
-    sequence.push(sequence[i - 1] + sequence[i - 2] + sequence[i - 3]);
+    const nextNumber = sequence[i - 1] + sequence[i - 2] + sequence[i - 3];
+    sequence.push(nextNumber);
   }
   return n > 3 ? sequence : sequence.slice(0, n);
 }
@@ -23,13 +24,13 @@ function tribonacci(signature, n) {
   return arr;
 } */
 
-console.log(tribonacci([1, 1, 1], 10)); // [1,1,1,3,5,9,17,31,57,105]
-console.log(tribonacci([0, 0, 1], 10)); // [0,0,1,1,2,4,7,13,24,44]
-console.log(tribonacci([0, 1, 1], 10)); // [0,1,1,2,4,7,13,24,44,81]
-console.log(tribonacci([1, 0, 0], 10)); // [1,0,0,1,1,2,4,7,13,24]
-console.log(tribonacci([0, 0, 0], 10)); // [0,0,0,0,0,0,0,0,0,0]
-console.log(tribonacci([1, 2, 3], 10)); // [1,2,3,6,11,20,37,68,125,230]
-console.log(tribonacci([3, 2, 1], 10)); // [3,2,1,6,9,16,31,56,103,190]
-console.log(tribonacci([1, 1, 1], 1)); // [1]
-console.log(tribonacci([3, 1], 2)); // [3,1]
-console.log(tribonacci([300, 200, 100], 0)); // []
+// console.log(tribonacci([1, 1, 1], 10)); // [1,1,1,3,5,9,17,31,57,105]
+// console.log(tribonacci([0, 0, 1], 10)); // [0,0,1,1,2,4,7,13,24,44]
+// console.log(tribonacci([0, 1, 1], 10)); // [0,1,1,2,4,7,13,24,44,81]
+// console.log(tribonacci([1, 0, 0], 10)); // [1,0,0,1,1,2,4,7,13,24]
+// console.log(tribonacci([0, 0, 0], 10)); // [0,0,0,0,0,0,0,0,0,0]
+// console.log(tribonacci([1, 2, 3], 10)); // [1,2,3,6,11,20,37,68,125,230]
+// console.log(tribonacci([3, 2, 1], 10)); // [3,2,1,6,9,16,31,56,103,190]
+// console.log(tribonacci([1, 1, 1], 1)); // [1]
+// console.log(tribonacci([3, 1], 2)); // [3,1]
+// console.log(tribonacci([300, 200, 100], 0)); // []
