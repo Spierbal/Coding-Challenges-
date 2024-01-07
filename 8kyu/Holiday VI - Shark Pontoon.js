626045c80be20d3756382fdf5a9bbb97d7dca503
Spierbal <abdulkadir.f.osman@gmail.com>
2024-01-07 23:59:42 +0000

    T

---
+++ 8kyu/Holiday VI - Shark Pontoon.js
@@ -0,0 +1,12 @@
+function shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin) {
+  if (dolphin) sharkSpeed / 2;
+
+  const pontoonTime = pontoonDistance / youSpeed;
+  const sharkTime = sharkDistance / sharkSpeed;
+
+  return pontoonTime < sharkTime ? "Alive!" : "Shark Bait!";
+}
+
+console.log(shark(12, 50, 4, 8, true)); // "Alive!"
+console.log(shark(7, 55, 4, 16, true)); // "Alive!"
+console.log(shark(24, 0, 4, 8, true)); // "Shark Bait!"
