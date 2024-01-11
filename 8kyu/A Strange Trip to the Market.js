63075c601941151d1d54e4f33693b3201a558999
Spierbal <abdulkadir.f.osman@gmail.com>
2024-01-11 00:10:43 +0000

    T

--- 8kyu/A Strange Trip to the Market.js
+++ 8kyu/A Strange Trip to the Market.js
@@ -5,12 +5,18 @@ const isLockNessMonster = s => /tree fiddy|3\.50|three fifty/.test(s);
 // Solution 2: some & includes
 const isLockNessMonster = s =>
   ["tree fiddy", "3.50", "three fifty"].some(el => s.includes(el));
- */
+   */
 
 /* 
-// Solution 3: includes
+// Solution 3: some & indexOf
+const isLockNessMonster = s =>
+  ["tree fiddy", "3.50", "three fifty"].some(el => s.indexOf(el) !== -1); */
+
+/* 
+// Solution 4: includes (less efficient)
 const isLockNessMonster1 = s =>
-  s.includes("tree fiddy") || s.includes("3.50") || s.includes("three fifty"); */
+  s.includes("tree fiddy") || s.includes("3.50") || s.includes("three fifty");
+ */
 
 /* 
 console.log(
@@ -18,9 +24,11 @@ console.log(
     "Your girlscout cookies are ready to ship. Your total comes to tree fiddy"
   )
 ); // true
+
 console.log(
   isLockNessMonster(
     "Yo, I heard you were on the lookout for Nessie. Let me know if you need assistance."
   )
 ); // false
+ 
  */
