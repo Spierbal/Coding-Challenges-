acb57db011468a18ef0407b35b91ece5d83283a8
Spierbal <abdulkadir.f.osman@gmail.com>
2024-01-11 00:12:35 +0000

    T

--- 8kyu/A Strange Trip to the Market.js
+++ 8kyu/A Strange Trip to the Market.js
@@ -5,18 +5,18 @@ const isLockNessMonster = s => /tree fiddy|3\.50|three fifty/.test(s);
 // Solution 2: some & includes
 const isLockNessMonster = s =>
   ["tree fiddy", "3.50", "three fifty"].some(el => s.includes(el));
-   */
+*/
 
 /* 
 // Solution 3: some & indexOf
 const isLockNessMonster = s =>
   ["tree fiddy", "3.50", "three fifty"].some(el => s.indexOf(el) !== -1); */
-
 /* 
+
 // Solution 4: includes (less efficient)
 const isLockNessMonster1 = s =>
   s.includes("tree fiddy") || s.includes("3.50") || s.includes("three fifty");
- */
+*/
 
 /* 
 console.log(
