ea8e2e321f3434f8ae5bc1bd6d83dc9a0cda3474
Spierbal <abdulkadir.f.osman@gmail.com>
2024-01-12 20:42:34 +0000

    T

---
+++ 8kyu/get ascii value of character.js
@@ -0,0 +1,10 @@
+// Solution 1: charCodeAt
+const getASCII = c => c.charCodeAt(0);
+
+/* 
+// Solution 2: codePointAt
+const getASCII = c => c.codePointAt(0); */
+
+// console.log(getASCII("A")); // 65
+// console.log(getASCII(" ")); // 32
+// console.log(getASCII("!")); // 33
