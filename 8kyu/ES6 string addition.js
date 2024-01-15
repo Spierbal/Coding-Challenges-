5b14b7e6adf6408456dda46f456ad3b9d879f32b
Spierbal <abdulkadir.f.osman@gmail.com>
2024-01-15 20:37:23 +0000

    T

---
+++ 8kyu/ES6 string addition.js
@@ -0,0 +1,5 @@
+const joinStrings = (string1, string2) => `${string1} ${string2}`;
+
+// console.log(joinStrings("string1", "string2")); // 'string1 string2'
+// console.log(joinStrings("testing", "testing")); // 'testing testing'
+// console.log(joinStrings(134, 134)); // '134 134'
