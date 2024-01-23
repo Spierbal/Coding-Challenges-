fa07cd5fd57f45acdf5bc484d6fcf6d92947b833
Spierbal <abdulkadir.f.osman@gmail.com>
2024-01-23 09:20:28 +0000

    T

--- 8kyu/Power.js
+++ 8kyu/Power.js
@@ -10,16 +10,16 @@ function numberToPower(number, power) {
 }
 
 /* 
-// Solution 2: Recursion
+// Soluton 2: Reduce
+const numberToPower = (base, power) =>
+  Array.from({ length: power }, () => base).reduce((acc, val) => acc * val, 1);
+*/
+
+/* 
+// Solution 3: Recursion
 const numberToPower = (base, power) => {
     return power === 0 ? 1 : base * numberToPower(base, power - 1)
 }; */
-
-/* 
-// Soluton 3: Reduce
-const numberToPower = (base, power) =>
-  Array.from({ length: power }, () => base).reduce((acc, val) => acc * val, 1);
- */
 
 // console.log(numberToPower(4, 2)); // 16);
 // console.log(numberToPower(10, 4)); // 10000);
