5a684438ad3059f6b457137ddb1b77a6171969a6
Spierbal <abdulkadir.f.osman@gmail.com>
2024-02-01 23:57:32 +0000

    T

---
+++ 8kyu/app.js
@@ -0,0 +1,13 @@
+function uefaEuro2016(teams, scores) {
+  if (scores[0] > scores[1]) {
+    return `At match ${teams[0]} - ${teams[1]}, ${teams[0]} won!`;
+  } else if (scores[0] < scores[1]) {
+    return `At match ${teams[0]} - ${teams[1]}, ${teams[1]} won!`;
+  } else {
+    return `At match ${teams[0]} - ${teams[1]}, teams played draw.`;
+  }
+}
+
+// console.log(uefaEuro2016(["Germany", "Ukraine"], [2, 0])); //, "At match Germany - Ukraine, Germany won!"
+// console.log(uefaEuro2016(["Belgium", "Italy"], [0, 2])); //, "At match Belgium - Italy, Italy won!"
+// console.log(uefaEuro2016(["Portugal", "Iceland"], [1, 1])); //, "At match Portugal - Iceland, teams played draw."
