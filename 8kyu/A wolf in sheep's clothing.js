827259166d581a618a5d8cab0721180d4f1ce8bf
Spierbal <abdulkadir.f.osman@gmail.com>
2024-02-09 23:06:29 +0000

    T

--- 8kyu/A wolf in sheep's clothing.js
+++ 8kyu/A wolf in sheep's clothing.js
@@ -10,18 +10,28 @@ function warnTheSheep(queue) {
 }
 
 /* 
-// Solution 2: indexOf() and reverse()
+// Solution 2: indexOf()
 function warnTheSheep(queue) {
-  const wolfIndex = [...queue].reverse().indexOf("wolf");
-
-  return wolfIndex === 0
+  const index = queue.indexOf("wolf");
+  return index === queue.length - 1
     ? "Pls go away and stop eating my sheep"
-    : `Oi! Sheep number ${wolfIndex}! You are about to be eaten by a wolf!`;
+    : `Oi! Sheep number ${
+        queue.length - 1 - index
+      }! You are about to be eaten by a wolf!`;
 } */
 
 /* 
-console.log(warnTheSheep(["sheep", "sheep", "wolf"])); // -->"Pls go away and stop eating my sheep"
-console.log(
-  warnTheSheep(["sheep", "sheep", "sheep", "wolf", "sheep", "sheep"])
-); // "Oi! Sheep number 2! You are about to be eaten by a wolf!"
- */
+// Solution 3: indexOf() and reverse()
+function warnTheSheep(queue) {
+  const index = [...queue].reverse().indexOf("wolf");
+
+  return index === 0
+    ? "Pls go away and stop eating my sheep"
+    : `Oi! Sheep number ${index}! You are about to be eaten by a wolf!`;
+}
+  */
+
+// console.log(warnTheSheep(["sheep", "sheep", "wolf"])); // -->"Pls go away and stop eating my sheep"
+// console.log(
+//   warnTheSheep(["sheep", "sheep", "sheep", "wolf", "sheep", "sheep"])
+// ); // "Oi! Sheep number 2! You are about to be eaten by a wolf!"
