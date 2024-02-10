7554077589dc180c2ff0016abad25a1e00e23d55
Spierbal <abdulkadir.f.osman@gmail.com>
2024-02-10 23:59:08 +0000

    T

--- 8kyu/Abbreviate a Two Word Name Solutions.js
+++ 8kyu/Abbreviate a Two Word Name Solutions.js
@@ -1,27 +1,29 @@
-// Solution 1: map, split
+// Solution 1: split() and destructuring
+function abbrevName(name) {
+  const [firstName, lastName] = name.split(" ");
+  return `${firstName[0].toUpperCase()}.${lastName[0].toUpperCase()}`;
+}
+
+/* 
+// Solution 2: map, split() and join()
 const abbrevName = name =>
   name
     .split(" ")
     .map(word => word[0].toUpperCase())
-    .join(".");
+    .join("."); */
 
 /* 
-// Solution 2: split
+// solution 3: regex
+const abbrevName = name => name.match(/\b\w/g).join(".").toUpperCase();
+ */
+
+/* 
+// Solution 4: charAt() & toUpperCase()
 function abbrevName(name) {
-  const [firstName, lastName] = name.split(" ");
-  return `${firstName[0].toUpperCase()}.${lastName[0].toUpperCase()}`;
+  return `${name.charAt(0).toUpperCase()}.${name
+    .charAt(name.indexOf(" ") + 1)
+    .toUpperCase()}`;
 } */
-
-/* 
-// Solution 3:
-function abbrevName(name) {
-  const initial = name.toUpperCase().split(" ");
-  return `${initial[0][0]}.${initial[1][0]}`;
-} */
-
-/* 
-// solution 4: regex
-const abbrevName = name => name.match(/\b\w/g).join(".").toUpperCase(); */
 
 // console.log(abbrevName("Ben Gorden")); // "B.G"
 // console.log(abbrevName("Sam Harris")); // "S.H"
