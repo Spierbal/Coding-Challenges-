589530c94d1acbbb97e879b085416bf5d4d622d5
Spierbal <abdulkadir.f.osman@gmail.com>
2024-02-14 17:22:58 +0000

    T

--- 8kyu/All Star Code Challenge #18.js
+++ 8kyu/All Star Code Challenge #18.js
@@ -5,12 +5,12 @@ const strCount = (str, letter) => str.split(letter).length - 1;
 // const strCount = (str, letter) => [...str].filter(l => l === letter).length;
 
 /* 
-// Solution 3: reduce 
+// Solution 3: reduce() 
 const strCount = (str, letter) =>
   [...str].reduce((count, char) => (char === letter ? count + 1 : count), 0); */
 
 /* 
-// Solution 3b: reduce & includes
+// Solution 3b: reduce() & includes()
 const strCount = (str, letter) =>
   [...str].reduce((count, char) => count + letter.includes(char), 0); */
 
--- 8kyu/Are arrow functions odd.js
+++ 8kyu/Are arrow functions odd.js
@@ -1,4 +1,40 @@
-const odds = values => values.filter(v => v % 2);
+// Solution 1: filter()
+const odds = values => values.filter(value => value % 2);
+
+/* 
+// Solution 2: for of loop
+function odds(values) {
+  let arr = [];
+  for (let value of values) {
+    if (value % 2 !== 0) {
+      arr.push(value);
+    }
+  }
+  return arr;
+} */
+
+/* 
+// Solution 3: for loop
+function odds(values) {
+  let arr = [];
+  for (let i = 0; i < values.length; i++) {
+    if (values[i] % 2 !== 0) {
+      arr.push(values[i]);
+    }
+  }
+  return arr;
+} */
+
+/* 
+// Solution 4: reduce()
+function odds(values) {
+  return values.reduce((arr, value) => {
+    if (value % 2 !== 0) {
+      arr.push(value);
+    }
+    return arr;
+  }, []);
+} */
 
 // console.log(odds([2, 4, 6])); // --> []
 // console.log(odds([1, 3, 5])); // --> [1, 3, 5]
