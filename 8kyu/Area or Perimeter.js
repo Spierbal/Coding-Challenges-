cd1a7b1a5c92298a0b3bd27e2658330a796ac93e
Spierbal <abdulkadir.f.osman@gmail.com>
2024-02-15 22:58:34 +0000

    T

--- 8kyu/Area or Perimeter.js
+++ 8kyu/Area or Perimeter.js
@@ -1,15 +1,15 @@
+// Solution 1: ternary operator
 const areaOrPerimeter = (l, w) => (l === w ? l * w : 2 * (l + w));
 
-//const areaOrPerimeter = (l, w) => (l === w ? l * w : l * 2 + w * 2);
+/* 
+// Solution 2: if else statement
+const areaOrPerimeter = function (l, w) {
+  if (l === w) {
+    return l * w;
+  } else if (l !== w) {
+    return 2 * (l + w);
+  }
+}; */
 
-// Solution 2:
-// const areaOrPerimeter = function (l, w) {
-//   if (l === w) {
-//     return l * w;
-//   } else if (l !== w) {
-//     return 2 * (l + w);
-//   }
-// };
-
-//console.log(areaOrPerimeter(4, 4)); // --16
-//console.log(areaOrPerimeter(6, 10)); // --> 32
+// console.log(areaOrPerimeter(4, 4)); // --16
+// console.log(areaOrPerimeter(6, 10)); // --> 32
