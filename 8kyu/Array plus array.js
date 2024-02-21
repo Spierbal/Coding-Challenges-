a2e26033e3ae6bf753cfa61ad331a26a034eadd7
Spierbal <abdulkadir.f.osman@gmail.com>
2024-02-21 23:46:50 +0000

    T

--- 8kyu/Array plus array.js
+++ 8kyu/Array plus array.js
@@ -1,8 +1,15 @@
+// Solution 1: spread operator with reduce()
 const arrayPlusArray = (arr1, arr2) =>
   [...arr1, ...arr2].reduce((a, b) => a + b, 0);
 
 /* 
-// Solution 2: for of loop
+// Solution 2: concat with reduce()
+function arrayPlusArray(arr1, arr2) {
+  return arr1.concat(arr2).reduce((a, b) => a + b, 0);
+} */
+
+/* 
+// Solution 3: for of loop
 function arrayPlusArray (arr1, arr2) => {
   let sum = 0;
 
