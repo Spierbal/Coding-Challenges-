66d3249707cf03c417c6c11d9953a5e83f11c4ee
Spierbal <abdulkadir.f.osman@gmail.com>
2024-02-27 23:41:37 +0000

    T

--- 8kyu/BASIC - Making Six Toast.js
+++ 8kyu/BASIC - Making Six Toast.js
@@ -1,8 +1,14 @@
+// Solution 1: Math.abs()
 const sixToast = num => Math.abs(num - 6);
 
-// Solution 2:
-// const sixToast = num => (num < 6 ? num - 6 : 6 - num);
+/* 
+// Solution 2: Ternary operator
+const sixToast = num => (num > 6 ? num - 6 : 6 - num); */
 
-// console.log(sixToast(6)); // 0
-// console.log(sixToast(17)); // 11
-// console.log(sixToast(3)); // 3
+/* 
+// Solution 3: Math.max() & Math.min()
+const sixToast = num => Math.max(num, 6) - Math.min(num, 6); */
+
+console.log(sixToast(6)); // 0
+console.log(sixToast(17)); // 11
+console.log(sixToast(3)); // 3
