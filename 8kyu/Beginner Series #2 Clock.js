5889990beb1a466a53aee57dad665f14f63614cd
Spierbal <abdulkadir.f.osman@gmail.com>
2024-03-06 00:24:54 +0000

    T

--- 8kyu/Beginner Series #2 Clock.js
+++ 8kyu/Beginner Series #2 Clock.js
@@ -1,13 +1,14 @@
-//Shortest Solution 1:
+// Solution 1: concise
 const past = (h, m, s) => (h * 3600 + m * 60 + s) * 1000;
 
-// Solution 2:
-// function past(h, m, s) {
-//   const hrsInMs = h * 60 * 60 * 1000;
-//   const minInMs = m * 60 * 1000;
-//   const secInMs = s * 1000;
-//   return hrsInMs + minInMs + secInMs;
-// }
+/* 
+// Solution 2: readable
+function past(h, m, s) {
+  const hoursInMilliseconds = h * 60 * 60 * 1000;
+  const minutesToMilliseconds = m * 60 * 1000;
+  const secondsToMilliseconds = s * 1000;
+  return hoursToMilliseconds + minutesToMilliseconds + secondsToMilliseconds;
+} */
 
 // console.log(past(0, 1, 1)); // 61000
 // console.log(past(1, 1, 1)); // 3661000
