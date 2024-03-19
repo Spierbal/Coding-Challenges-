d4bcf926fd9dc32977752d72328df02a26b965f3
Spierbal <abdulkadir.f.osman@gmail.com>
2024-03-19 23:59:54 +0000

    T

--- 8kyu/Calculate average.js
+++ 8kyu/Calculate average.js
@@ -1,5 +1,16 @@
+// Solution 1: reduce (concise)
 const find_average = arr =>
   arr.length ? arr.reduce((a, b) => a + b, 0) / arr.length : 0;
 
-// console.log(find_average([10, 20])) // --> 15
-// console.log(find_average([])) // --> 0
+/* 
+// Solution 2: for of loop
+function find_average(arr) {
+  let sum = 0;
+  for (const num of arr) {
+    sum += num;
+  }
+  return sum / arr.length;
+} */
+
+// console.log(find_average([10, 20])); // --> 15
+// console.log(find_average([])); // --> 0
