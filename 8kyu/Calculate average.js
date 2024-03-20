9ce0cd77344ff1220bf4dde1a5cafba3884e041b
Spierbal <abdulkadir.f.osman@gmail.com>
2024-03-20 00:25:31 +0000

    T

--- 8kyu/Calculate average.js
+++ 8kyu/Calculate average.js
@@ -1,10 +1,12 @@
 // Solution 1: reduce (concise)
 const find_average = arr =>
-  arr.length ? arr.reduce((a, b) => a + b, 0) / arr.length : 0;
+  arr.length ? arr.reduce((acc, num) => acc + num, 0) / arr.length : 0;
 
 /* 
 // Solution 2: for of loop
 function find_average(arr) {
+  if (arr.length === 0) return 0;
+
   let sum = 0;
   for (const num of arr) {
     sum += num;
