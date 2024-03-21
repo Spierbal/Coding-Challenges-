20531f2b23e37b9d50fc295cf7d712cfcc6d005f
Spierbal <abdulkadir.f.osman@gmail.com>
2024-03-21 23:58:35 +0000

    T

--- 8kyu/Calculate BMI.js
+++ 8kyu/Calculate BMI.js
@@ -1,3 +1,4 @@
+// Solution 1: if else statements
 function bmi(weight, height) {
   const bmi = weight / height ** 2;
 
@@ -8,17 +9,35 @@ function bmi(weight, height) {
   return "Obese";
 }
 
-// Solution 2:
-// function bmi(weight, height) {
-//   const bmi = weight / height ** 2;
+/* 
+// Solution 2: switch statement
+function bmi(weight, height) {
+  const bmi = weight / (height * height);
 
-//   return bmi < 18.5
-//     ? "Underweight"
-//     : bmi <= 25
-//     ? "Normal"
-//     : bmi <= 30
-//     ? "Overweight"
-//     : "Obese";
-// }
+  switch (true) {
+    case bmi <= 18.5:
+      return "Underweight";
+    case bmi <= 25:
+      return "Normal";
+    case bmi <= 30:
+      return "Overweight";
+    default:
+      return "Obese";
+  }
+} */
 
-//console.log(bmi(80, 1.8)); // --> "Normal"
+/* 
+// Solution 3: ternary operator
+function bmi(weight, height) {
+  const bmi = weight / height ** 2;
+
+  return bmi < 18.5
+    ? "Underweight"
+    : bmi <= 25
+    ? "Normal"
+    : bmi <= 30
+    ? "Overweight"
+    : "Obese";
+} */
+
+console.log(bmi(80, 1.8)); // --> "Normal"
