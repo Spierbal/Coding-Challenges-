54b2d4a97e9efc4097817bfbcc9fe3726504226e
Spierbal <abdulkadir.f.osman@gmail.com>
2024-03-25 23:18:37 +0000

    T

--- 8kyu/Capitalization and Mutability.js
+++ 8kyu/Capitalization and Mutability.js
@@ -1,4 +1,13 @@
+// Solution 1: slice()
 const capitalizeWord = word => word[0].toUpperCase() + word.slice(1);
 
-// console.log(capitalizeWord('word')) // --> "Word"
-// console.log(capitalizeWord('glassWear')) // --> "Glasswear"
+/* 
+// Solution 2: charAt()
+const capitalizeWord = word => word.charAt(0).toUpperCase() + word.slice(1);  */
+
+/* 
+// Solution 3: replace() (regex)
+const capitalizeWord = word => word.replace(/^\w/, char => char.toUpperCase());  */
+
+// console.log(capitalizeWord("word")); // --> "Word"
+// console.log(capitalizeWord("glassWear")); // --> "Glasswear"
