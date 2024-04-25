676f7a8e4e4d0381a828a88c4fee19ea1d8cd0db
Spierbal <abdulkadir.f.osman@gmail.com>
2024-04-26 00:02:02 +0100

    T

--- 8kyu/Cat years, Dog years.js
+++ 8kyu/Cat years, Dog years.js
@@ -1,4 +1,4 @@
-function humanYearsCatYearsDogYears1(humanYears) {
+function humanYearsCatYearsDogYears(humanYears) {
   let dogYears = 0;
   let catYears = 0;
 
@@ -13,7 +13,8 @@ function humanYearsCatYearsDogYears1(humanYears) {
   return [humanYears, catYears, dogYears];
 }
 
-function humanYearsCatYearsDogYears2(humanYears) {
+/* 
+function humanYearsCatYearsDogYears(humanYears) {
   let catYears = 0;
   let dogYears = 0;
 
@@ -31,43 +32,30 @@ function humanYearsCatYearsDogYears2(humanYears) {
   }
 
   return [humanYears, catYears, dogYears];
-}
+} */
 
-function humanYearsCatYearsDogYears1(humanYears) {
+/* 
+const humanYearsCatYearsDogYears = humanYears => {
+  if (humanYears === 1) return [1, 15, 15];
+  if (humanYears === 2) return [2, 24, 24];
+
+  return [humanYears, 24 + (humanYears - 2) * 4, 24 + (humanYears - 2) * 5];
+};
+ */
+
+/* 
+function humanYearsCatYearsDogYears(humanYears) {
   const catYears =
     humanYears < 2 ? 15 : humanYears < 3 ? 24 : 24 + (humanYears - 2) * 4;
   const dogYears =
     humanYears < 2 ? 15 : humanYears < 3 ? 24 : 24 + (humanYears - 2) * 5;
 
   return [humanYears, catYears, dogYears];
-}
+} */
 
-const humanYearsCatYearsDogYears3 = humanYears => {
-  if (humanYears === 1) return [1, 15, 15];
-  if (humanYears === 2) return [2, 24, 24];
-
-  return [humanYears, 24 + (humanYears - 2) * 4, 24 + (humanYears - 2) * 5];
-};
-
-const humanYearsCatYearsDogYears4 = humanYears => {
-  switch (humanYears) {
-    case 1:
-      return [1, 15, 15];
-    case 2:
-      return [2, 24, 24];
-  }
-  return [humanYears, 24 + (humanYears - 2) * 4, 24 + (humanYears - 2) * 5];
-};
-
-const humanYearsCatYearsDogYears5 = h => {
-  const c = h === 1 ? 15 : h === 2 ? 24 : 24 + (h - 2) * 4;
-  const d = h === 1 ? 15 : h === 2 ? 24 : 24 + (h - 2) * 5;
-  return [h, c, d];
-};
-
-console.log(humanYearsCatYearsDogYears(1)); // --> [1, 15, 15]
-console.log(humanYearsCatYearsDogYears(2)); // --> [2, 24, 24]
-console.log(humanYearsCatYearsDogYears(3)); // --> [3, 28, 29]
-console.log(humanYearsCatYearsDogYears(4)); // --> [4, 32, 34]
-console.log(humanYearsCatYearsDogYears(5)); // --> [5, 36, 39]
-console.log(humanYearsCatYearsDogYears(10)); // --> [10, 56, 64]
+// console.log(humanYearsCatYearsDogYears(1)); // --> [1, 15, 15]
+// console.log(humanYearsCatYearsDogYears(2)); // --> [2, 24, 24]
+// console.log(humanYearsCatYearsDogYears(3)); // --> [3, 28, 29]
+// console.log(humanYearsCatYearsDogYears(4)); // --> [4, 32, 34]
+// console.log(humanYearsCatYearsDogYears(5)); // --> [5, 36, 39]
+// console.log(humanYearsCatYearsDogYears(10)); // --> [10, 56, 64]
