6cdd9bf151f1073321689d52f89768e7eca4dbba
Spierbal <abdulkadir.f.osman@gmail.com>
2024-04-30 23:58:24 +0100

    T

--- 8kyu/Century From Year.js
+++ 8kyu/Century From Year.js
@@ -1,6 +1,11 @@
+// Solution 1: Math.ceil
 const century = year => Math.ceil(year / 100);
 
-//console.log(century(1705)) --> 18
-//console.log(century(1900)) --> 19
-//console.log(century(1601)) --> 17
-//console.log(century(2000)) --> 20
+/* 
+// Solution 2: Math.floor
+// const century = year => Math.floor((year - 1) / 100) + 1; */
+
+// console.log(century(1705)); // --> 18
+// console.log(century(1900)); //--> 19
+// console.log(century(1601)); //--> 17
+// console.log(century(2000)); //--> 20
